bd71366d688bd1d48e6231208c212e1eeb547d89
Kyle P. Johnson <kpj204.kyle@gmail.com>
2014-01-18 21:45:03 -0500

    working json cumps

--- examples/compiler_ex_3.py
+++ examples/compiler_ex_3.py
@@ -1,6 +1,7 @@
-from cltk.corpus_compiler import Compile
+from cltk.corpus_compiler_multiline_insert_2 import Compile
 
 #c = Compile()
+#c = Compile('/home/kyle/Downloads/project_dir/corps', '.')
 c = Compile('/home/kyle/Downloads/cltk_dl_test', '.')
 #c.dump_txts_phi5()
 #c.dump_txts_phi7()
