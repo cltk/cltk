9fedc1cfccdae774400a9eacfdc97e252ccedacb
Kyle P. Johnson <kpj204.kyle@gmail.com>
2014-01-21 07:11:02 -0500

    Latin stops and lemmas added
    Former-commit-id: c210bd316e04017b56efa330f9d0a2e433a799c8

---
+++ corpora/Classical Latin/linguistic_data/lemma/lemmatizer.py
@@ -0,0 +1,11 @@
+import re
+from lemmata_list import replacement_patterns
+
+class LemmaReplacer(object):
+    def __init__(self, patterns=replacement_patterns):
+        self.patterns = [(re.compile(regex), repl) for (regex, repl) in patterns]
+    def replace(self, text):
+        s = text
+        for (pattern, repl) in self.patterns:
+            (s, count) = re.subn(pattern, repl, s)
+        return s
