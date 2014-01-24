9fe5d9c1b98b68e8a35d0e9a55f1c5175a97b661
Kyle P. Johnson <kpj204.kyle@gmail.com>
2014-01-23 21:49:30 -0500

    phi and tlg compile exs edited
    Former-commit-id: d6d7648624b5892134598234e656a882a6fb326e

--- example/classical_greek/compile_tlg.py
+++ example/classical_greek/compile_tlg.py
@@ -1,5 +1,6 @@
 from cltk.corpus.common.compiler import Compile
 
 c = Compile('/home/kyle/Downloads/project_dir/corps', '/home/kyle/Downloads/project_dir')
-c.dump_txts_phi5()
-c.dump_txts_phi7()
+
+#c.dump_txts_phi7()
+c.dump_txts_tlg()
