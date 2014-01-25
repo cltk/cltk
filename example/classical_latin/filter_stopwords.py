9f71e310045e53adf486bb9c50416880c89cdf69
Kyle P. Johnson <kpj204.kyle@gmail.com>
2014-01-25 12:48:17 -0500

    latin stop filtering example

---
+++ example/classical_latin/filter_stopwords.py
@@ -0,0 +1,14 @@
+import nltk
+
+from cltk.stop.classical_latin.stops import STOPS_LIST
+
+SENTENCE = 'Quo usque tandem abutere, Catilina, patientia nostra?'
+
+lowered = SENTENCE.lower()
+
+tokens = nltk.word_tokenize(lowered)
+
+filtered = [w for w in tokens if not w in STOPS_LIST]
+
+print(tokens)
+print(filtered)
