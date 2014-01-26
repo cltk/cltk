8ad0aeef461659e3bc37af6e3eb3d80038d7d973
Kyle P. Johnson <kpj204.kyle@gmail.com>
2014-01-26 17:25:09 -0500

    bleach added to travis
    Former-commit-id: a5616f3af72015346880e55ee5cacd4746b57073

--- cltk/corpus/classical_greek/perseus_compiler.py
+++ cltk/corpus/classical_greek/perseus_compiler.py
@@ -7,12 +7,29 @@ perseus_root = root + '/plaintext/perseus_greek/'
 
 authors = os.listdir(perseus_root)
 
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
 for author in authors:
-        texts = os.listdir(perseus_root + author + '/opensource')
-        for text in texts:
-                text_match = re.match(r'.*_gk.xml', text)
-                if text_match:
-                        gk_file = text_match.group()
-                        txt_file = perseus_root + author + '/opensource/' + gk_file
-                        with open(txt_file) as gk:
-                             print(gk.read())
+    texts = os.listdir(perseus_root + author + '/opensource')
+    for text in texts:
+        text_match = re.match(r'.*_gk.xml', text)
+        if text_match:
+            gk_file = text_match.group()
+            txt_file = perseus_root + author + '/opensource/' + gk_file
+            with open(txt_file) as gk:
+                html = gk.read()
+                print(strip_tags(html))
