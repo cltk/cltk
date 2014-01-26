be626fedad85fb6cca97c78ad3fec5f7a54645e2
Kyle P. Johnson <kpj204.kyle@gmail.com>
2014-01-26 00:48:08 -0500

    removed examples dir

--- example/classical_greek/compile_tlg.py
+++
@@ -1,6 +0,0 @@
-from cltk.corpus.common.compiler import Compile
-
-c = Compile('/home/kyle/Downloads/project_dir/corps', '/home/kyle/Downloads/project_dir')
-
-#c.dump_txts_phi7()
-c.dump_txts_tlg()
--- example/classical_latin/jv_convert.py
+++
@@ -1,5 +0,0 @@
-from cltk.stem.classical_latin.j_and_v_converter import JVReplacer
-
-j = JVReplacer()
-
-j.replace('vem jam')
