bcd1d7d3209ad59799a0ec9e3e835bfba43ba33c
Kyle P. Johnson <kpj204.kyle@gmail.com>
2014-01-26 17:41:01 -0500

    Perseus beta translating to unicode
    Former-commit-id: 2ae516e0640217320545fe4da7284c5ef3c15d95

--- cltk/corpus/classical_greek/perseus_compiler.py
+++ cltk/corpus/classical_greek/perseus_compiler.py
@@ -1,27 +1,11 @@
 import os
 import re
+import bleach
+from cltk.corpus.classical_greek.replacer import Replacer
 
 root = os.getcwd()
-
 perseus_root = root + '/plaintext/perseus_greek/'
-
 authors = os.listdir(perseus_root)
-
-from HTMLParser import HTMLParser
-
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-        return ''.join(self.fed)
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
 
 for author in authors:
     texts = os.listdir(perseus_root + author + '/opensource')
@@ -32,4 +16,7 @@ for author in authors:
             txt_file = perseus_root + author + '/opensource/' + gk_file
             with open(txt_file) as gk:
                 html = gk.read()
-                print(strip_tags(html))
+                beta_code = bleach.clean(html, strip=True).upper()
+                a_replacer = Replacer()
+                unicode_converted = a_replacer.beta_code(beta_code)
+                print(unicode_converted)
