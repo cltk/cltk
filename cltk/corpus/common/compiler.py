e1e6ea29d0a19a0462a88811b8b0a079536eab09
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-04-12 11:56:42 -0400

    add pos_latin dl
    Former-commit-id: eb2356802f871b01cdf944d7a65469d2b3efe5e9

--- cltk/corpus/common/compiler.py
+++ cltk/corpus/common/compiler.py
@@ -126,6 +126,14 @@ class Compile(object):
                 os.mkdir(orig_files_dir_treebank_perseus_latin)
                 logging.info('Made new directory "%s" at "%s"', corpus_name, orig_files_dir_treebank_perseus_latin)
             self.get_treebank_perseus_latin_tar()
+        elif corpus_name == 'pos_latin':
+            orig_files_dir_pos_latin = os.path.join(self.orig_files_dir, 'pos_latin')
+            if os.path.isdir(orig_files_dir_pos_latin) is True:
+                pass
+            else:
+                os.mkdir(orig_files_dir_pos_latin)
+                logging.info('Made new directory "%s" at "%s"', corpus_name, orig_files_dir_pos_latin)
+            self.get_pos_latin_tar()
         else:
             logging.error('Unrecognized corpus name. Choose one of the following: "tlg", "phi7", "phi5", "latin_library", "perseus_latin", "perseus_greek", "lacus_curtius_latin".')
 
@@ -684,6 +692,33 @@ class Compile(object):
         except IOError:
             logging.info('Failed to unpack %s.', treebank_perseus_latin_file_name)
 
+    def get_pos_latin_tar(self):
+        orig_files_dir_pos_latin = os.path.join(self.orig_files_dir, 'pos_latin')
+        #make compiled files dir for pos_latin
+        '''
+        compiled_files_dir_pos_latin = os.path.join(self.compiled_files_dir, 'pos_latin')
+        if os.path.isdir(compiled_files_dir_pos_latin) is True:
+            pass
+        else:
+            os.mkdir(compiled_files_dir_pos_latin)
+        '''
+        pg_url = 'https://raw.githubusercontent.com/kylepjohnson/pos_latin/master/pos_latin.tar.gz'
+        s = requests.Session()
+        s.mount(pg_url, SSLAdapter(ssl.PROTOCOL_TLSv1))
+        pg_tar = s.get(pg_url, stream=True)
+        pos_latin_file_name = urlsplit(pg_url).path.split('/')[-1]
+        pos_latin_file_path = os.path.join(orig_files_dir_pos_latin, pos_latin_file_name)
+        try:
+            with open(pos_latin_file_path, 'wb') as new_file:
+                new_file.write(pg_tar.content)
+                logging.info('Finished writing %s.', pos_latin_file_name)
+        except IOError:
+            logging.error('Failed to write file %s', pos_latin_file_name)
+        try:
+            shutil.unpack_archive(pos_latin_file_path, self.compiled_files_dir)
+            logging.info('Finished unpacking %s', pos_latin_file_name)
+        except IOError:
+            logging.info('Failed to unpack %s.', pos_latin_file_name)
 
 def remove_non_ascii(input_string):
     """remove non-ascii: http://stackoverflow.com/a/1342373"""
