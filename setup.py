81baff40111dd9a8991cbb7f76e9956746911443
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-06-14 13:05:48 -0400

    nltk version bump
    Former-commit-id: 5bb717e529939039d46f675cfe0592194413abf3

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ and Latin languages. It is developed in Python 3.4.""",
     name='cltk',
     packages=find_packages(),
     url='https://github.com/kylepjohnson/cltk',
-    version='0.0.0.14',
+    version='0.0.0.15',
     zip_safe=True,
 )
