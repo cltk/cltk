16fbb1d47a3157bf5427d39f3ea32ffbe0eb9b60
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-07-04 10:33:33 -0400

    betacode

--- cltk/tests/test_ex.py
+++ cltk/tests/test_ex.py
@@ -1,8 +1,9 @@
 """unit tests for CLTK
 """
 
-from cltk.stop.classical_latin.stops import STOPS_LIST
+from cltk.corpus.classical_greek.beta_to_unicode import Replacer
 from cltk.stem.classical_latin.j_and_v_converter import JVReplacer
+from cltk.stop.classical_latin.stops import STOPS_LIST
 import nltk.tokenize
 import random
 import unittest
@@ -45,6 +46,16 @@ class TestSequenceFunctions(unittest.TestCase):
         no_stops = [w for w in tokens if not w in STOPS_LIST]
         target_list = ['usque', 'tandem', 'abutere', ',', 'catilina', ',', 'patientia', 'nostra', '?']
         self.assertEqual(no_stops, target_list)
+
+    def test_greek_betacode_to_unicode(self):
+        """test conversion of betacode to unicode
+        note: assertEqual appears to not be correctly comparing certain characters (ά and ί, at least)
+        """
+        BETA_EXAMPLE = r"""O(/PWS OU)=N MH\ TAU)TO\ """
+        r = Replacer()
+        unicode = r.beta_code(BETA_EXAMPLE)
+        target_unicode = 'ὅπωσ οὖν μὴ ταὐτὸ '
+        self.assertEqual(unicode, target_unicode)
 
 
 if __name__ == '__main__':
