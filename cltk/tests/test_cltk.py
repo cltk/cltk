2dbe1404e2edc4e7a56388982e2162f9186f034a
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-07-04 11:34:20 -0400

    cleanup tests
    Former-commit-id: e8a377d6f3bb7c05fdce35c1ff0a55d6ef02fcb7

--- cltk/tests/test_cltk.py
+++ cltk/tests/test_cltk.py
@@ -5,32 +5,9 @@ from cltk.corpus.classical_greek.beta_to_unicode import Replacer
 from cltk.stem.classical_latin.j_and_v_converter import JVReplacer
 from cltk.stop.classical_latin.stops import STOPS_LIST
 from nltk.tokenize.punkt import PunktWordTokenizer
-import random
 import unittest
 
 class TestSequenceFunctions(unittest.TestCase):
-
-    def setUp(self):
-        self.seq = list(range(10))
-
-    def test_shuffle(self):
-        # make sure the shuffled sequence does not lose any elements
-        random.shuffle(self.seq)
-        self.seq.sort()
-        self.assertEqual(self.seq, list(range(10)))
-
-        # should raise an exception for an immutable sequence
-        self.assertRaises(TypeError, random.shuffle, (1,2,3))
-
-    def test_choice(self):
-        element = random.choice(self.seq)
-        self.assertTrue(element in self.seq)
-
-    def test_sample(self):
-        with self.assertRaises(ValueError):
-            random.sample(self.seq, 20)
-        for element in random.sample(self.seq, 5):
-            self.assertTrue(element in self.seq)
 
     def test_latin_i_u_transform(self):
         """Test conversion of j to i and v to u"""
@@ -56,7 +33,6 @@ class TestSequenceFunctions(unittest.TestCase):
         unicode = r.beta_code(BETA_EXAMPLE)
         target_unicode = 'ὅπωσ οὖν μὴ ταὐτὸ '
         self.assertEqual(unicode, target_unicode)
-
 
 if __name__ == '__main__':
     unittest.main()
