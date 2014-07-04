3861af463bb925337d82a2e9b4d81ed8234e2daa
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-07-04 17:09:45 -0400

    pylint + pep8 fixes

--- cltk/tests/test_cltk.py
+++ cltk/tests/test_cltk.py
@@ -6,7 +6,9 @@ from cltk.stem.classical_latin.j_and_v_converter import JVReplacer
 from nltk.tokenize.punkt import PunktWordTokenizer
 import unittest
 
-class TestSequenceFunctions(unittest.TestCase):
+
+class TestSequenceFunctions(unittest.TestCase):  # pylint: disable=R0904
+    """Class for unittest"""
 
     def test_latin_i_u_transform(self):
         """Test conversion of j to i and v to u"""
@@ -17,30 +19,36 @@ class TestSequenceFunctions(unittest.TestCase):
     def test_latin_stopwords(self):
         """Filter Latin stopwords"""
         from cltk.stop.classical_latin.stops import STOPS_LIST
-        SENTENCE = 'Quo usque tandem abutere, Catilina, patientia nostra?'
-        lowered = SENTENCE.lower()
+        sentence = 'Quo usque tandem abutere, Catilina, patientia nostra?'
+        lowered = sentence.lower()
         tokens = PunktWordTokenizer().tokenize(lowered)
-        no_stops = [w for w in tokens if not w in STOPS_LIST]
-        target_list = ['usque', 'tandem', 'abutere', ',', 'catilina', ',', 'patientia', 'nostra', '?']
+        no_stops = [w for w in tokens if w not in STOPS_LIST]
+        target_list = ['usque', 'tandem', 'abutere', ',', 'catilina', ',',
+                       'patientia', 'nostra', '?']
         self.assertEqual(no_stops, target_list)
 
     def test_greek_stopwords(self):
         """Filter Greek stopwords"""
         from cltk.stop.classical_greek.stops_unicode import STOPS_LIST
-        sentence = """Ἅρπαγος δὲ καταστρεψάμενος Ἰωνίην ἐποιέετο στρατηίην ἐπὶ Κᾶρας καὶ Καυνίους καὶ Λυκίους, ἅμα ἀγόμενος καὶ Ἴωνας καὶ Αἰολέας."""
+        sentence = """Ἅρπαγος δὲ καταστρεψάμενος Ἰωνίην ἐποιέετο στρατηίην
+        ἐπὶ Κᾶρας καὶ Καυνίους καὶ Λυκίους, ἅμα ἀγόμενος καὶ Ἴωνας καὶ
+        Αἰολέας."""
         lowered = sentence.lower()
         tokens = PunktWordTokenizer().tokenize(lowered)
-        no_stops = [w for w in tokens if not w in STOPS_LIST]
-        target_list = ['ἅρπαγος', 'καταστρεψάμενος', 'ἰωνίην', 'ἐποιέετο', 'στρατηίην', 'κᾶρας', 'καυνίους', 'λυκίους', ',', 'ἅμα', 'ἀγόμενος', 'ἴωνας', 'αἰολέας.']
+        no_stops = [w for w in tokens if w not in STOPS_LIST]
+        target_list = ['ἅρπαγος', 'καταστρεψάμενος', 'ἰωνίην', 'ἐποιέετο',
+                       'στρατηίην', 'κᾶρας', 'καυνίους', 'λυκίους', ',',
+                       'ἅμα', 'ἀγόμενος', 'ἴωνας', 'αἰολέας.']
         self.assertEqual(no_stops, target_list)
 
     def test_greek_betacode_to_unicode(self):
         """Test conversion of betacode to unicode
-        note: assertEqual appears to not be correctly comparing certain characters (ά and ί, at least)
+        note: assertEqual appears to not be correctly comparing certain
+        characters (ά and ί, at least)
         """
-        BETA_EXAMPLE = r"""O(/PWS OU)=N MH\ TAU)TO\ """
-        r = Replacer()
-        unicode = r.beta_code(BETA_EXAMPLE)
+        beta_example = r"""O(/PWS OU)=N MH\ TAU)TO\ """
+        replacer = Replacer()
+        unicode = replacer.beta_code(beta_example)
         target_unicode = 'ὅπωσ οὖν μὴ ταὐτὸ '
         self.assertEqual(unicode, target_unicode)
 
