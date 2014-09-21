51dce81df1e043a86909181068d8deb200718897
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-09-21 00:02:41 -0400

    fix tes
    Former-commit-id: 3fbb13a801708d9d7bdac474b6fe2c1a3a2567bf

--- cltk/tests/test_cltk.py
+++ cltk/tests/test_cltk.py
@@ -5,7 +5,8 @@ __license__ = 'MIT License. See LICENSE.'
 
 from cltk.corpus.classical_greek.beta_to_unicode import Replacer
 from cltk.stem.classical_latin.j_and_v_converter import JVReplacer
-from cltk.tokenize.sentence_tokenizer_greek import tokenize_latin_sentences
+from cltk.tokenize.sentence_tokenizer_greek import tokenize_greek_sentences
+from cltk.tokenize.sentence_tokenizer_latin import tokenize_latin_sentences
 from nltk.tokenize.punkt import PunktWordTokenizer
 import os
 import unittest
@@ -71,7 +72,7 @@ class TestSequenceFunctions(unittest.TestCase):  # pylint: disable=R0904
                 os.path.exists(pickle_file_path)
                 sentences = 'εἰ δὲ καὶ τῷ ἡγεμόνι πιστεύσομεν ὃν ἂν Κῦρος διδῷ, τί κωλύει καὶ τὰ ἄκρα ἡμῖν κελεύειν Κῦρον προκαταλαβεῖν; ἐγὼ γὰρ ὀκνοίην μὲν ἂν εἰς τὰ πλοῖα ἐμβαίνειν ἃ ἡμῖν δοίη, μὴ ἡμᾶς ταῖς τριήρεσι καταδύσῃ, φοβοίμην δ᾽ ἂν τῷ ἡγεμόνι ὃν δοίη ἕπεσθαι, μὴ ἡμᾶς ἀγάγῃ ὅθεν οὐκ ἔσται ἐξελθεῖν· βουλοίμην δ᾽ ἂν ἄκοντος ἀπιὼν Κύρου λαθεῖν αὐτὸν ἀπελθών· ὃ οὐ δυνατόν ἐστιν.'  # pylint: disable=C0301
                 good_tokenized_sentences = ['εἰ δὲ καὶ τῷ ἡγεμόνι πιστεύσομεν ὃν ἂν Κῦρος διδῷ, τί κωλύει καὶ τὰ ἄκρα ἡμῖν κελεύειν Κῦρον προκαταλαβεῖν;', 'ἐγὼ γὰρ ὀκνοίην μὲν ἂν εἰς τὰ πλοῖα ἐμβαίνειν ἃ ἡμῖν δοίη, μὴ ἡμᾶς ταῖς τριήρεσι καταδύσῃ, φοβοίμην δ᾽ ἂν τῷ ἡγεμόνι ὃν δοίη ἕπεσθαι, μὴ ἡμᾶς ἀγάγῃ ὅθεν οὐκ ἔσται ἐξελθεῖν· βουλοίμην δ᾽ ἂν ἄκοντος ἀπιὼν Κύρου λαθεῖν αὐτὸν ἀπελθών· ὃ οὐ δυνατόν ἐστιν.']  # pylint: disable=C0301
-                tokenized_sentences = tokenize_latin_sentences(sentences)
+                tokenized_sentences = tokenize_greek_sentences(sentences)
                 self.assertEqual(tokenized_sentences, good_tokenized_sentences)
             except:
                 pass
