f526fab6a4f84abc9ebc39616c9ba98803c84ce5
Stephen Margheim <stephen.margheim@gmail.com>
2014-10-05 12:44:09 -0400

    add config script

---
+++ config.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+CLTK_DATA = "/Users/smargh/Code/cltk/cltk_data"
+
+CORPORA = {
+#    'tlg': TLG,
+#    'phi7': PHI7,
+#    'phi5': PHI5,
+#    'latin_library': LatinLibrary,
+#    'perseus_latin': PerseusLatin,
+#    'perseus_greek': PerseusGreek,
+#    'lacus_curtius_latin': LacusCurtiusLatin,
+#    'treebank_perseus_greek': TreebankPerseusGreek,
+#    'treebank_perseus_latin': TreebankPerseusLatin,
+#    'pos_latin': POSLatin,
+#    'sentence_tokens_latin': SentenceTokensLatin,
+#    'sentence_tokens_greek': SentenceTokensGreek
+}
