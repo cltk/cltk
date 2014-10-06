71e3c3966c2b760cda8a642799fe90333f501116
Stephen Margheim <stephen.margheim@gmail.com>
2014-10-05 21:04:03 -0400

    convert LL to RemoteCorpus sub-class

--- corpora/latin_library.py
+++ corpora/latin_library.py
@@ -3,21 +3,22 @@
 __author__ = 'Stephen Margheim <stephen.margheim@gmail.com>'
 __license__ = 'MIT License. See LICENSE.'
 
-from cltk.corpus import Corpus
+from cltk.corpus import RemoteCorpus
 
 
-class LatinLibrary(Corpus):
+class LatinLibrary(RemoteCorpus):
     def __init__(self):
         self.name = 'latin_library'
-        Corpus.__init__(self, self.name)
         self.tar_url = ('https://raw.githubusercontent.com/kylepjohnson/'
                         'corpus_latin_library/master/latin_library.tar.gz')
+        RemoteCorpus.__init__(self, self.name, self.tar_url)
 
     def retrieve(self):
-        self.retrieve(url=self.tar_url)
+        self.get_tar()
 
 
 class LatinLibraryDoc(object):
     def __init__(self, path):
         self.path = path
 
+LatinLibrary().retrieve()
