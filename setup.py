f31cc39700bec4eaa3042efef90009e66ff46b63
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-10-16 23:12:53 -0400

    verison bump

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     name='cltk',
     packages=find_packages(),
     url='https://github.com/kylepjohnson/cltk',
-    version='0.0.0.38',
+    version='0.0.0.39',
     zip_safe=True,
     test_suite='cltk.tests.test_cltk',
 )
