192ec30584d1a88d2ef44411ad1c6a3ac991091a
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-10-18 12:17:08 -0400

    update path

--- cltk/tag/pos/pos_tagger.py
+++ cltk/tag/pos/pos_tagger.py
@@ -18,9 +18,9 @@ class POSTag(object):
     def unigram_tagger(self, untagged_string, language):
         """Reads language .pickle for right language"""
         if language == 'greek':
-            pickle_path = os.path.expanduser('~/cltk_data/greek/taggers/pos/unigram.pickle')
+            pickle_path = os.path.expanduser('~/cltk_data/greek/cltk_linguistic_data/taggers/pos/unigram.pickle')
         elif language == 'latin':
-            pickle_path = os.path.expanduser('~/cltk_data/latin/taggers/pos/unigram.pickle')
+            pickle_path = os.path.expanduser('~/cltk_data/latin/cltk_linguistic_data/taggers/pos/unigram.pickle')
         else:
             print('No unigram tagger for this language available.')
         with open(pickle_path, 'rb') as open_pickle:
