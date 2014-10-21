708ccb8e298f4cb2378915ebda316c713c2f2e5b
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-10-20 20:25:58 -0400

    add 123 backoff tagger

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     name='cltk',
     packages=find_packages(),
     url='https://github.com/kylepjohnson/cltk',
-    version='0.0.0.44',
+    version='0.0.0.45',
     zip_safe=True,
     test_suite='cltk.tests.test_cltk',
 )
