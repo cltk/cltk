1f0aaf4379719936f71c6585fd0323f09f444570
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-11-16 13:32:18 -0500

    add starter greek corpora list

---
+++ cltk/corpus/greek/corpora.py
@@ -0,0 +1,42 @@
+"""Greek language corpora available for download or loading locally."""
+
+GREEK_CORPORA = [
+    {'encoding': 'utf-8',
+     'homepage': 'https://github.com/cltk/greek_corpus_perseus',
+     'markup': 'tei_xml',
+     'name': 'greek_text_perseus',
+     'location': 'remote',
+     'type': 'text',
+     'path': 'https://raw.githubusercontent.com/cltk/greek_corpus_perseus/master/greek_corpus_perseus_new.tar.gz'},  # pylint: disable=C0301
+    {'encoding': 'latin-1',
+     'homepage': 'http://epigraphy.packhum.org/inscriptions/',
+     'markup': 'beta_code',
+     'name': 'phi7',
+     'location': 'local',
+     'type': 'text',
+     'path': ''},
+    {'encoding': 'latin-1',
+     'homepage': '',
+     'markup': 'beta_code',
+     'name': 'tlg',
+     'location': 'local',
+     'type': 'text',
+     'path': ''},
+    {'encoding': 'utf-8',
+     'homepage': 'https://github.com/cltk/greek_proper_names',
+     'markup': 'plaintext',
+     'name': 'greek_proper_names',
+     'location': 'remote',
+     'type': 'dictionary',
+     'path': 'https://raw.githubusercontent.com/cltk/greek_proper_names/master/greek_names.tar.gz'},  # pylint: disable=C0301
+    {'encoding': 'utf-8',
+     'homepage': 'https://github.com/cltk/cltk_greek_linguistic_data',
+     'markup': 'pickle',
+     'name': 'cltk_linguistic_data',
+     'location': 'remote',
+     'type': 'trained_model',
+     'path': 'https://raw.githubusercontent.com/cltk/cltk_greek_linguistic_data/master/greek.tar.gz'},  # pylint: disable=C0301
+]
+
+
+
