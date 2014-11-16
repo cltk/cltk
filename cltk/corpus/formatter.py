54a987ba83bcdd581377488f4b88b7d5424b932c
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2014-11-16 15:45:21 -0500

    add simple text cleanup functions

--- cltk/corpus/formatter.py
+++ cltk/corpus/formatter.py
@@ -13,3 +13,24 @@ __license__ = 'MIT License. See LICENSE.'
 
 
 from cltk.corpus.greek.tlgu import tlgu
+
+
+def remove_non_ascii(input_string):
+    """remove non-ascii: http://stackoverflow.com/a/1342373"""
+    no_ascii = "".join(i for i in input_string if ord(i) < 128)
+    return no_ascii
+
+def cleanup_tlg_txt(tlg_str):
+    """"Remove all non–Greek characters from a TLG corpus."""
+    # fix beta code transliteration problems
+    tlg_str = re.sub(r'ι\+', 'ϊ', tlg_str)
+    tlg_str = re.sub(r'ί\+', 'ΐ', tlg_str)
+    tlg_str = re.sub(r'\\.', '.', tlg_str)
+    # fix tlg markup
+    tlg_str = re.sub(r'@1 \{1.+?\}1 @', '', tlg_str) #  rm book titles
+    tlg_str = re.sub(r'\[.+?\]', '', tlg_str) #  rm words in square brackets
+    tlg_str = re.sub(r'[0-9]', '', tlg_str)
+    tlg_str = re.sub(r'@|%|\x00', '', tlg_str)
+    tlg_str = re.sub('—', ' — ', tlg_str)
+    return tlg_str
+
