5372fdfa18048546841b26424550cb30f10a88e1
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-01-03 11:27:42 -0500

    rm unused dir copy function

--- cltk/corpus/utils/file_operations.py
+++ cltk/corpus/utils/file_operations.py
@@ -7,21 +7,6 @@ import shutil
 import sys
 
 
-def copy_dir_contents(src_path: str, destination_path: str):
-    """Copy contents of one directory to another. ``destination_path`` will
-    be the name of the new dir and cannot exist.
-    :type src_path: str
-    :type destination_path: str
-    :param : src_path: Path of dir to be copied.
-    :param : destination_path: Path of new dir.
-    """
-    src_files = os.listdir(src_path)
-    for file_name in src_files:
-        full_file_name = os.path.join(src_path, file_name)
-        if os.path.isfile(full_file_name):
-            shutil.copy(full_file_name, destination_path)
-
-
 def open_pickle(path: str):
     """Open a pickle and return loaded pickle object.
     :type path: str
