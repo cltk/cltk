22c705409ead9bbbcb6e9e0879013fedefb83078
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-03-19 12:52:01 -0400

    bump vers for new text cleaners

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     name='cltk',
     packages=find_packages(),
     url='https://github.com/kylepjohnson/cltk',
-    version='0.0.1.10',
+    version='0.0.1.11',
     zip_safe=True,
     test_suite='cltk.tests.test_cltk',
 )
