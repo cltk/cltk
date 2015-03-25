e635af6b2364623e411ff3f34a0bd8bd7d0ab90a
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-03-24 23:49:11 -0400

    bump vers for git importer

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     name='cltk',
     packages=find_packages(),
     url='https://github.com/kylepjohnson/cltk',
-    version='0.0.1.11',
+    version='0.0.1.12',
     zip_safe=True,
     test_suite='cltk.tests.test_cltk',
 )
