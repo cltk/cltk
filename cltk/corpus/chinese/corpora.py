be6be63571722f76afd7084ffa6915f90a77a8b3
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-03-29 15:14:39 -0400

    add chinese and pali corpora.py

---
+++ cltk/corpus/chinese/corpora.py
@@ -0,0 +1,22 @@
+"""Chinese language corpora available for download or loading locally.
+All remote corpora hosted by github on the cltk organization account, eg:
+'http://github.com/cltk' + name
+"""
+
+CHINESE_CORPORA = [
+    {'encoding': 'utf-8',
+     'markup': 'xml',
+     'location': 'remote',
+     'type': 'text',
+     'name': 'chinese_text_cbeta_01'},
+    {'encoding': 'utf-8',
+     'markup': 'xml',
+     'location': 'remote',
+     'type': 'text',
+     'name': 'chinese_text_cbeta_02'},
+    {'encoding': 'utf-8',
+     'markup': 'xml',
+     'location': 'remote',
+     'type': 'text',
+     'name': 'chinese_text_cbeta_indices'},
+]
