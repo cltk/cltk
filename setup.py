3e5c02f2b1c8ac902dc4c21cb82c644f6af52ae0
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-04-10 23:35:38 -0400

    bump vers, ch to beta

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(
     author='Kyle P. Johnson',
     author_email='kyle@kyle-p-johnson.com',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
+        'Natural Language :: Chinese (Traditional)'
         'Natural Language :: English',
         'Natural Language :: Greek',
         'Natural Language :: Latin',
@@ -35,7 +36,7 @@ setup(
     name='cltk',
     packages=find_packages(),
     url='https://github.com/kylepjohnson/cltk',
-    version='0.0.1.13',
+    version='0.0.1.14',
     zip_safe=True,
     test_suite='cltk.tests.test_cltk',
 )
