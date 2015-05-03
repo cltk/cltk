a6082dc8c5476434108fcf2e8ac098600c7d8399
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-05-03 12:30:30 -0400

    add half working new lemmatizer

--- cltk/stem/lemma.py
+++ cltk/stem/lemma.py
@@ -5,10 +5,11 @@ __license__ = 'MIT License. See LICENSE.'
 
 from cltk.utils.cltk_logger import logger
 import importlib.machinery
+from nltk.tokenize.punkt import PunktLanguageVars
 import os
 import re
 
-AVAILABLE_LANGUAGES = ['latin']
+AVAILABLE_LANGUAGES = ['greek', 'latin']
 
 
 class LemmaReplacer(object):  # pylint: disable=R0903
@@ -18,25 +19,37 @@ class LemmaReplacer(object):  # pylint: disable=R0903
 
     def __init__(self, language):
         """Import replacement patterns into a list."""
-        self.language = language
-        self._patterns = self._setup_language_variables()
-
-    def _setup_language_variables(self):
-        """Check for availability of lemmatizer for a language.
-        TODO: Turn 'lemma_list' file a simple csv and importing on the fly.
-        """
+        self.language = language.lower()
         assert self.language in AVAILABLE_LANGUAGES, \
-            'Corpora not available for {0} language.'.format(self.language)
+            "Lemmatizer not available for language '{0}'.".format(self.language)
+        self._raw_patterns = self._load_replacement_patterns()
+        self._patterns = self._compile_patterns(self._raw_patterns)
+
+    def _load_replacement_patterns(self):
+        """Check for availability of lemmatizer for a language."""
         if self.language == 'latin':
             rel_path = os.path.join('~/cltk_data',
                                     self.language,
-                                    'model/latin_models_cltk/lemmata/lemma_list.py')  # pylint: disable=C0301
+                                    'model/latin_models_cltk/lemmata/latin_lemmata_cltk.py')  # pylint: disable=C0301
             path = os.path.expanduser(rel_path)
             logger.info('Loading lemmata. This may take a minute.')
-            loader = importlib.machinery.SourceFileLoader('lemma_list', path)
+            loader = importlib.machinery.SourceFileLoader('latin_lemmata_cltk', path)
             module = loader.load_module()
-            patterns = module.REPLACEMENT_PATTERNS
-        return [(re.compile(regex), repl) for (regex, repl) in patterns]
+            lemmata_set = module.LEMMATA
+            #patterns = []
+            for inflection, headword in lemmata_set.items():
+                #patterns.append((inflection, list(headword)[0]))  # just take first headword val
+
+                yield inflection, list(headword)[0]
+
+
+    def _compile_patterns(self, raw_patterns):
+        """Take incoming generator and compile regex replacements."""
+
+        #return [(re.compile(regex), repl) for (regex, repl) in patterns]
+        #yield [(re.compile(regex), repl) for (regex, repl) in raw_patterns]
+        for (regex, repl) in raw_patterns:
+            yield re.compile(regex), repl
 
     def lemmatize(self, text):
         """Replacer of text via the dict.
@@ -45,5 +58,27 @@ class LemmaReplacer(object):  # pylint: disable=R0903
         :rtype : str
         """
         for (pattern, repl) in self._patterns:
-            text = re.subn(pattern, repl, text)[0]
-        return text
+            new_token, count = re.subn(pattern, repl, text)
+            if count == 1:
+                yield new_token
+        #return text
+
+    def lemmatize_tokens(self, in_tokens):
+        """This isn't good."""
+        for token in in_tokens:
+            for (pattern, repl) in self._patterns:
+                new_token, did_replace = re.subn(pattern, repl, token, count=1)
+                if did_replace == 1:
+                    yield new_token
+
+if __name__ == '__main__':
+    print('***')
+    r = LemmaReplacer('latin')
+    p = PunktLanguageVars()
+    #s = 'Est interdum praestare mercaturis rem quaerere, nisi tam periculosum sit, et item foenerari, si tam honestum. Maiores nostri sic habuerunt et ita in legibus posiuerunt: furem dupli condemnari, foeneratorem quadrupli. Quanto peiorem ciuem existimarint foeneratorem quam furem, hinc licet existimare. Et uirum bonum quom laudabant, ita laudabant: bonum agricolam bonumque colonum; amplissime laudari existimabatur qui ita laudabatur. Mercatorem autem strenuum studiosumque rei quaerendae existimo, uerum, ut supra dixi, periculosum et calamitosum. At ex agricolis et uiri fortissimi et milites strenuissimi gignuntur, maximeque pius quaestus stabilissimusque consequitur minimeque inuidiosus, minimeque male cogitantes sunt qui in eo studio occupati sunt. Nunc, ut ad rem redeam, quod promisi institutum principium hoc erit.'
+    s = 'Est'
+    tokens = p.word_tokenize(s.lower())
+    new_tokens = r.lemmatize_tokens(tokens)
+    print(new_tokens)
+    for x in new_tokens:
+        print(x)
