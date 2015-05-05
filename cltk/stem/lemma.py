b9b05ede97bcb4bf7d29b6aa96a0d73819dfaf61
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-05-04 21:07:18 -0400

    add working brute force match tokens

--- cltk/stem/lemma.py
+++ cltk/stem/lemma.py
@@ -22,8 +22,8 @@ class LemmaReplacer(object):  # pylint: disable=R0903
         self.language = language.lower()
         assert self.language in AVAILABLE_LANGUAGES, \
             "Lemmatizer not available for language '{0}'.".format(self.language)
-        self._raw_patterns = self._load_replacement_patterns()
-        self._patterns = self._compile_patterns(self._raw_patterns)
+        self.lemmata = self._load_replacement_patterns()
+        #self._patterns = self._compile_patterns(self._raw_patterns)
 
     def _load_replacement_patterns(self):
         """Check for availability of lemmatizer for a language."""
@@ -35,12 +35,10 @@ class LemmaReplacer(object):  # pylint: disable=R0903
             logger.info('Loading lemmata. This may take a minute.')
             loader = importlib.machinery.SourceFileLoader('latin_lemmata_cltk', path)
             module = loader.load_module()
-            lemmata_set = module.LEMMATA
-            #patterns = []
-            for inflection, headword in lemmata_set.items():
-                #patterns.append((inflection, list(headword)[0]))  # just take first headword val
-
-                yield inflection, list(headword)[0]
+            lemmata = module.LEMMATA
+            return lemmata
+            #for inflection, headword in lemmata.items():
+            #    yield inflection, headword
 
 
     def _compile_patterns(self, raw_patterns):
@@ -58,27 +56,46 @@ class LemmaReplacer(object):  # pylint: disable=R0903
         :rtype : str
         """
         for (pattern, repl) in self._patterns:
-            new_token, count = re.subn(pattern, repl, text)
+            text, count = re.subn(pattern, repl, text)
+            '''
             if count == 1:
                 yield new_token
-        #return text
+            '''
+        return text
 
-    def lemmatize_tokens(self, in_tokens):
-        """This isn't good."""
+
+
+    def lemmatize_tokens(self, in_tokens, testing=False):
+        """Simple. Try equal match of tokens. Watch out for periods which ``PunktLanguageVars()`` sticks on."""
+        lemmatized_tokens = []
         for token in in_tokens:
-            for (pattern, repl) in self._patterns:
-                new_token, did_replace = re.subn(pattern, repl, token, count=1)
-                if did_replace == 1:
-                    yield new_token
+            final_period = False
+            if token[-1] == '.':
+                final_period = True
+                token = token[:-1]
+            for k, v in self.lemmata.items():
+                if token == k:
+                    if final_period == True:
+                        token = token + '.'
+                        v = v + '.'
+                    if not testing:
+                        lemmatized_tokens.append(v)
+                    else:
+                        print(token, '-->', v)
+                    break
+            else:
+                if not testing:
+                    lemmatized_tokens.append(token)
+                else:
+                    print(token, '-->', token)
+        return lemmatized_tokens
+
 
 if __name__ == '__main__':
     print('***')
     r = LemmaReplacer('latin')
     p = PunktLanguageVars()
-    #s = 'Est interdum praestare mercaturis rem quaerere, nisi tam periculosum sit, et item foenerari, si tam honestum. Maiores nostri sic habuerunt et ita in legibus posiuerunt: furem dupli condemnari, foeneratorem quadrupli. Quanto peiorem ciuem existimarint foeneratorem quam furem, hinc licet existimare. Et uirum bonum quom laudabant, ita laudabant: bonum agricolam bonumque colonum; amplissime laudari existimabatur qui ita laudabatur. Mercatorem autem strenuum studiosumque rei quaerendae existimo, uerum, ut supra dixi, periculosum et calamitosum. At ex agricolis et uiri fortissimi et milites strenuissimi gignuntur, maximeque pius quaestus stabilissimusque consequitur minimeque inuidiosus, minimeque male cogitantes sunt qui in eo studio occupati sunt. Nunc, ut ad rem redeam, quod promisi institutum principium hoc erit.'
-    s = 'Est'
+    s = 'Est interdum praestare mercaturis rem quaerere, nisi tam periculosum sit, et item foenerari, si tam honestum. Maiores nostri sic habuerunt et ita in legibus posiuerunt: furem dupli condemnari, foeneratorem quadrupli. Quanto peiorem ciuem existimarint foeneratorem quam furem, hinc licet existimare. Et uirum bonum quom laudabant, ita laudabant: bonum agricolam bonumque colonum; amplissime laudari existimabatur qui ita laudabatur. Mercatorem autem strenuum studiosumque rei quaerendae existimo, uerum, ut supra dixi, periculosum et calamitosum. At ex agricolis et uiri fortissimi et milites strenuissimi gignuntur, maximeque pius quaestus stabilissimusque consequitur minimeque inuidiosus, minimeque male cogitantes sunt qui in eo studio occupati sunt. Nunc, ut ad rem redeam, quod promisi institutum principium hoc erit.'
     tokens = p.word_tokenize(s.lower())
-    new_tokens = r.lemmatize_tokens(tokens)
-    print(new_tokens)
-    for x in new_tokens:
-        print(x)
+    lemmatized = r.lemmatize_tokens(tokens)
+    print(lemmatized)
