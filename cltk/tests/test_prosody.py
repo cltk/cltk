6b65a2b5b3002f39ee910805c71c79755ecf276f
TylerKirby <Tyler@Josephs-MacBook-Pro.local>
2015-08-06 13:12:33 -0400

    Greek Prosody Module

--- cltk/tests/test_prosody.py
+++ cltk/tests/test_prosody.py
@@ -5,6 +5,7 @@ __license__ = 'MIT License. See LICENSE.'
 
 from cltk.corpus.utils.importer import CorpusImporter
 from cltk.prosody.latin.scanner import Scansion
+from cltk.prosody.greek.scanner import Scansion as ScansionGreek
 import os
 import unittest
 
@@ -49,6 +50,42 @@ class TestSequenceFunctions(unittest.TestCase):  # pylint: disable=R0904
         scansion = Scansion()
         elidable_end = scansion._elidable_end(['fi', 'li', 'ae'])
         self.assertTrue(elidable_end)
+
+    # Test string for Greek prosody module unit testing
+    test = "νέος μὲν καὶ ἄπειρος, δικῶν ἔγωγε ἔτι. μὲν καὶ ἄπειρος."
+
+    def test_clean_text_greek(self):
+        """Test _clean_text method."""
+        correct = "νέος μὲν καὶ ἄπειρος δικῶν ἔγωγε ἔτι. μὲν καὶ ἄπειρος."
+        current = ScansionGreek()._clean_text(self.test)
+        self.assertEqual(current, correct)
+
+    def test_clean_accents_greek(self):
+        """Test _clean_accents method."""
+        correct = "νεος μεν και απειρος δικων εγωγε ετι. μεν και απειρος."
+        current = ScansionGreek()._clean_accents(self.test)
+        self.assertEqual(current, correct)
+
+    def test_tokenize_greek(self):
+        """Test _tokenize method."""
+        correct = [['νεος', 'μεν', 'και', 'απειρος', 'δικων', 'εγωγε', 'ετι.'],
+                   ['μεν', 'και', 'απειρος.']]
+        current = ScansionGreek()._tokenize(self.test)
+        self.assertEqual(current, correct)
+
+    def test_make_syllables_greek(self):
+        """Test _make_syllables method."""
+        correct = [[['νε', 'ος'], ['μεν'], ['και'], ['α', 'πει', 'ρος'],
+                   ['δι', 'κων'], ['ε', 'γω', 'γε'], ['ε', 'τι']], [['μεν'],
+                   ['και'], ['α', 'πει', 'ρος']]]
+        current = ScansionGreek()._make_syllables(self.test)
+        self.assertEqual(current, correct)
+
+    def text_scan_text_greek(self):
+        """Test scan_text method."""
+        correct = ['˘¯¯¯˘¯¯˘¯˘¯˘˘x', '¯¯˘¯x']
+        current = ScansionGreek().scan_text(self.test)
+        self.assertEqual(current, correct)
 
 if __name__ == '__main__':
     unittest.main()
