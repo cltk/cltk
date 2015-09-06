d579caf6820f0ee6743ea53655b9e7d29ddc4c5b
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-09-06 10:55:17 -0700

    develop index access functions

--- cltk/corpus/greek/tlg/parse_tlg_indices.py
+++ cltk/corpus/greek/tlg/parse_tlg_indices.py
@@ -16,62 +16,132 @@ import sys
 THIS_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 
+def open_json(file_name):
+    """Opens json file, returns contents."""
+    json_path = os.path.join(THIS_DIR, file_name)
+    with open(json_path) as file_open:
+        return json.load(file_open)
+
+
+# Gender
 def get_female_authors():
-    """Open female authors index and return list of author ids."""
-    female_path = os.path.join(THIS_DIR, 'author_female.json')
-    with open(female_path) as file_open:
-        female_dict = json.load(file_open)
-        return female_dict['Femina']
+    """Open female authors index and return ordered set of author ids."""
+    _path = os.path.join(THIS_DIR, 'author_female.json')
+    _dict = open_json(_path)
+    return set(_dict['Femina'])
 
 
-def get_author_geo():
-    """"""
-    _path = os.path.join(THIS_DIR, 'author_geo.json')
-    with open(_path) as file_open:
-        _dict = json.load(file_open)
-    return _dict
+# Epithet
+def get_epithet_index():
+    """Return dict of epithets (key) to a set of all author ids of that
+    epithet (value).
+    """
+    _path = os.path.join(THIS_DIR, 'author_epithet.json')
+    _dict = open_json(_path)
 
-
-def get_date_author():
-    """"""
-    _path = os.path.join(THIS_DIR, 'author_date.json')
-    with open(_path) as file_open:
-        _dict = json.load(file_open)
+    for k, v in _dict.items():
+        _dict[k] = set(v)
     return _dict
 
 
 def get_epithets():
+    """Return a list of all the epithet labels."""
     _path = os.path.join(THIS_DIR, 'author_epithet.json')
-    with open(_path) as file_open:
-        _dict = json.load(file_open)
+    _dict = open_json(_path)
     return sorted(_dict.keys())
 
 
-def get_author_epithet():
-    """"""
+def select_authors_by_epithet(query):
+    """Pass exact name (case insensitive) of epithet name, return ordered set
+    of author ids.
+    """
+
     _path = os.path.join(THIS_DIR, 'author_epithet.json')
-    with open(_path) as file_open:
-        _dict = json.load(file_open)
+    _dict = open_json(_path)
+
+    for epithet, ids in _dict.items():
+        if epithet.casefold() == query.casefold():
+            return set(ids)
+
+
+def get_epithet_of_author(_id):
+    """Pass author id and return the name of its associated epithet."""
+    _path = os.path.join(THIS_DIR, 'author_epithet.json')
+
+    _id = str(_id)
+
+    _dict = open_json(_path)
+
+    for epithet, ids in _dict.items():
+        if _id in ids:
+            return epithet
+
+
+# Geography
+def get_geo_index():
+    """Get entire index of geographic name (key) and set of associated authors
+    (value).
+    """
+    _path = os.path.join(THIS_DIR, 'author_geo.json')
+    _dict = open_json(_path)
+
+    for k, v in _dict.items():
+        _dict[k] = set(v)
+
     return _dict
 
 
+def get_geographies():
+    """Return a list of all the epithet labels."""
+    _path = os.path.join(THIS_DIR, 'author_geo.json')
+    _dict = open_json(_path)
+    return sorted(_dict.keys())
+
+
+def select_authors_by_geo(query):
+    """Pass exact name (case insensitive) of geography name, return ordered set
+    of author ids.
+    """
+
+    _path = os.path.join(THIS_DIR, 'author_geo.json')
+    _dict = open_json(_path)
+
+    for geo, ids in _dict.items():
+        if geo.casefold() == query.casefold():
+            return set(ids)
+
+
+def get_geo_of_author(_id):
+    """Pass author id and return the name of its associated epithet."""
+    _path = os.path.join(THIS_DIR, 'author_geo.json')
+
+    _id = str(_id)
+
+    _dict = open_json(_path)
+
+    for geo, ids in _dict.items():
+        if _id in ids:
+            return geo
+
+
+# List of TLG indices
 def get_lists():
-    """"""
+    """A list of the TLG's lists."""
     _path = os.path.join(THIS_DIR, 'index_lists.json')
-    with open(_path) as file_open:
-        _dict = json.load(file_open)
+    _dict = open_json(_path)
     return _dict
 
 
+# Master author index (`id_author.json`)
 def get_id_author():
-    """"""
+    """Returns entirety of id-author TLG index."""
     _path = os.path.join(THIS_DIR, 'id_author.json')
-    with open(_path) as file_open:
-        _dict = json.load(file_open)
+    _dict = open_json(_path)
     return _dict
 
 
 def select_author_by_name(query):
+    """Do a case-insensitive regex match on author name, returns TLG id."""
     id_author = get_id_author()
     comp = re.compile(r'{}'.format(query.casefold()))
     matches = []
@@ -82,30 +152,12 @@ def select_author_by_name(query):
     return matches
 
 
-def select_authors_by_epithet(query):
-    """Pass exact name (case insensitive) of epithet name, return list of author ids."""
-
-    _path = os.path.join(THIS_DIR, 'author_epithet.json')
-    with open(_path) as file_open:
-        _dict = json.load(file_open)
-
-    for epithet,ids in _dict.items():
-        if epithet.casefold() == query.casefold():
-            return sorted(ids)
-
-
-def get_epithet_of_author(_id):
-    """Takes author id, returns TLG's epithet"""
-    _path = os.path.join(THIS_DIR, 'author_epithet.json')
-
-    _id = str(_id)  # the loaded json expects a str
-
-    with open(_path) as file_open:
-        _dict = json.load(file_open)
-
-    for epithet, ids in _dict.items():
-        if _id in ids:
-            return epithet
+# Dates
+def get_date_author():
+    """Returns entirety of date-author index."""
+    _path = os.path.join(THIS_DIR, 'author_date.json')
+    _dict = open_json(_path)
+    return _dict
 
 if __name__ == "__main__":
-    print(get_epithet_of_author('0016'))
+    print(get_date_author())
