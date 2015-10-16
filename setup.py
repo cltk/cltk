6bcfd437bde7caa6ac18b97591ad54373abb09ba
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-10-15 20:15:47 -0700

    test upgrade to Python 3.5

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     name='cltk',
     packages=find_packages(),
     url='https://github.com/kylepjohnson/cltk',
-    version='0.1.26',
+    version='0.1.27',
     zip_safe=True,
     test_suite='cltk.tests.test_cltk',
 )
