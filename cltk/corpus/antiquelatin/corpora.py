a29d53798992d0f1b964fe4fecd51adea1639a4f
yash14123 <yash14123@iiitd.ac.in>
2016-03-01 10:37:50 +0530

    Late Antique Latin corpus
    Fixes https://github.com/cltk/cltk/issues/121
    I will make the repo on my account so you can fork it. :)

--- cltk/corpus/antiquelatin/corpora.py
+++ cltk/corpus/antiquelatin/corpora.py
@@ -2,3 +2,11 @@
 All remote corpora hosted by github on the cltk organization account, eg:
 'http://github.com/cltk' + name
 """
+
+ANTIQUELATIN_CORPORA = [
+    {'encoding': 'utf-8',
+     'markup': 'tei_xml',
+     'location': 'remote',
+     'type': 'text',
+     'name': 'digiliblt_antiquelatin_texts'}
+]
