907e4c59a413b7ffa57ba8cbbeab2936b16f7cf8
yash14123 <yash14123@iiitd.ac.in>
2016-03-01 10:25:07 +0530

    AntiqueLatin repo

---
+++ cltk/corpus/antiquelatin/corpora.py
@@ -0,0 +1,4 @@
+"""Coptic language corpora available for download or loading locally.
+All remote corpora hosted by github on the cltk organization account, eg:
+'http://github.com/cltk' + name
+"""
