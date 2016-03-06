0c2d70d4b8295572db4c8e961006359d31d899dd
Nishant Nikhil <nishantiam@gmail.com>
2016-03-06 17:16:56 +0530

    Added Sanskrit Corpus from DCS

--- cltk/corpus/sanskrit/corpora.py
+++ cltk/corpus/sanskrit/corpora.py
@@ -2,4 +2,8 @@ SANSKRIT_CORPORA = [
         {'name':'sanskrit_text_jnu',
         'location':'remote',
         'type':'text'},
+        {'name':'sanskrit_corpus_DCS',
+        'markup': 'xml',
+        'location':'remote',
+        'type':'text'}
         ]
