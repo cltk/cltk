93d9e66a056959a246d6d1126139f8c09847af84
Md. Sabbir Alam <sabbiralam.cse@gmail.com>
2016-03-09 01:57:41 +0600

    Added entry for sanskrit_text_dcs in cltk/cltk/corpus/sanskrit/corpora.py

--- cltk/corpus/sanskrit/corpora.py
+++ cltk/corpus/sanskrit/corpora.py
@@ -2,4 +2,8 @@ SANSKRIT_CORPORA = [
         {'name':'sanskrit_text_jnu',
         'location':'remote',
         'type':'text'},
+        {'name':'sanskrit_text_dcs',
+        'location':'remote',
+        'type':'text'
+            },
         ]
