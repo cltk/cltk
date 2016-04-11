f82243208fc8d1b7c969704b7d41fd96e77fa8aa
Chaitanya Sai Alaparthi <achaitanyasai@gmail.com>
2016-04-11 23:29:07 +0530

    added sanskrit_parallel_gitasupersite (#238)

--- cltk/corpus/sanskrit/corpora.py
+++ cltk/corpus/sanskrit/corpora.py
@@ -11,4 +11,7 @@ SANSKRIT_CORPORA = [
         {'name':'sanskrit_text_sacred_texts',
         'location':'remote',
          'type':'text'},
+        {'name':'sanskrit_parallel_gitasupersite',
+         'location':'remote',
+         'type':'parallel'},
 ]
