8446fdec672650c87ea565b51ab73fb17aae44a7
Shai Wilson <sjwilson2@usfca.edu>
2016-04-18 16:59:42 -0700

    remove changes from syllabifier

--- cltk/stem/latin/syllabifier.py
+++ cltk/stem/latin/syllabifier.py
@@ -173,38 +173,6 @@ class Syllabifier(object):
                     # Determine if the syllable is complete
                     if char_is_vowel:
 
-                        # And it doesn't compose a dipthong 
-                        # with the current character
-                        notDipthong = self._is_diphthong(char, next_char)
-
-                        # And the current character isn't preceded by a q, 
-                        # unless followed by a u
-                        hasQ = has_prev_char and prev_char == "q"
-
-                        isCharU = char == "u"
-
-                        isVowel = self._is_vowel(word[i + 2])
-
-                        # if ((self._is_vowel(next_char) 
-                        #     and not notDipthong
-                        #     and not hasQ
-                        #     and isCharU and next_char != "u") 
-                        #     or (i < word_len - 2 
-                        #     and hasQ
-                        #     and isCharU
-                        #     and isVowel)
-                        #     or (not has_prev_char
-                        #     and isCharU
-                        #     and isVowel
-                        #     and not notDipthong)
-                        #     or (char != "u"
-                        #     and isVowel
-                        #     and not notDipthong)
-                        #     or (self._is_mute_consonant_or_f(next_char)
-                        #     and self._is_liquid_consonant(word[i + 2]))):
-
-                        #         syllable_complete = True
-
                         if (
                                     (  # If the next character's a vowel
                                        self._is_vowel(
