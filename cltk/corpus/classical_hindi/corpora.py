e71b44c27382fa70befc6f741529a7bdca19daa0
Nurendra Choudhary <nurendra.choudhary@research.iiit.ac.in>
2016-04-20 22:57:27 +0530

    Added Classical Hindi Corpora (#256)
    Add 1 hindi corpus

---
+++ cltk/corpus/classical_hindi/corpora.py
@@ -0,0 +1,11 @@
+"""Hindi language corpora available for download or loading locally.
+All remote corpora hosted by github on the cltk organization account, eg:
+'http://github.com/cltk' + name
+"""
+
+CLASSICAL_HINDI_CORPORA = [
+    {'encoding': 'utf-8',
+     'location': 'remote',
+     'type': 'text',
+     'name': 'hindi_text_ltrc'}
+]
