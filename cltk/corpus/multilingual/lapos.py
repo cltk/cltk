a5f6571401fdce235e5239ea60484ae48be7848c
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2016-05-14 13:02:37 -0700

    develop input sentence tagging

--- cltk/corpus/multilingual/lapos.py
+++ cltk/corpus/multilingual/lapos.py
@@ -14,8 +14,9 @@ from cltk.utils.cltk_logger import logger
 class Lapos:
     """Class for everything pertaining to installing Lapos."""
 
-    def __init__(self):
+    def __init__(self, language):
         """Constructor."""
+        self.language = language
         self.operating_system = self._what_os()
         self._is_cloned_get()  # get software if not present
         self.commands = {'evaluate': 'lapos-evaluate',
@@ -68,7 +69,7 @@ class Lapos:
             print('Lapos did not build successfully.')
             logger.error('Lapos did not build successfully.')
 
-    def train(self):
+    def train_example(self):
         """Train new Lapos model."""
         fp = os.path.expanduser('~/cltk_data/multilingual/software/lapos')
 
@@ -81,6 +82,25 @@ class Lapos:
             logger.info('Lapos built model successfully.')
         else:
             print('Lapos did not build successfully.')
+            logger.error('Lapos did not build model successfully.')
+
+    def train(self, language, pos_file):
+        """Train new Lapos model."""
+        fp_lapos = os.path.expanduser('~/cltk_data/multilingual/software/lapos')
+        fp_model = os.path.expanduser('~/{}_models_cltk/taggers/pos'.format(language))
+        fp_pos = os.path.expanduser(pos_file)
+
+        call = 'cd {0} && ./lapos-learn -m {1} {2}'.format(fp_lapos, fp_model, fp_pos)
+        print(call)
+        p_out = subprocess.call(call,
+                                shell=True,
+                                stdout=subprocess.DEVNULL)
+
+        if p_out == 0:
+            print('Lapos built model successfully.')
+            logger.info('Lapos built model successfully.')
+        else:
+            print('Lapos did not build model successfully.')
             logger.error('Lapos did not build model successfully.')
 
     def tag_file_example(self):
@@ -117,20 +137,26 @@ class Lapos:
 
         return tagged_sentences
 
-    def tag_sentence(self, sentence):
-        """Tag using Lapos model."""
-        fp = os.path.expanduser('~/cltk_data/multilingual/software/lapos')
-        try:
-            p_out = subprocess.check_output('cd {0} && echo {1}| ./lapos -t -m .'.format(fp, sentence),
-                                            shell=True,
-                                            stderr=subprocess.STDOUT,
-                                            universal_newlines=True)
-        except subprocess.CalledProcessError as cp_err:
-            logger.error('Lapos call failed.')
-            print(cp_err)
-            raise
+    def tag_sentence_example(self, sentence):
+        """Tag using Lapos model.
 
-        # Parse output from Laops
+        TODO: Figure out how to pre-load model (loading is slow). Or force users to bulk-convert files or strings.
+        """
+        fp = os.path.expanduser('~/cltk_data/multilingual/software/lapos')
+        if self.language == 'latin':
+            try:
+                p_out = subprocess.check_output('cd {0} && echo "{1}" | ./lapos -t -m ~/cltk_data/latin/model/latin_models_cltk/taggers/pos/'.format(fp, sentence),
+                                                shell=True,
+                                                stderr=subprocess.STDOUT,
+                                                universal_newlines=True)
+            except subprocess.CalledProcessError as cp_err:
+                logger.error('Lapos call failed.')
+                logger.error(sentence)
+                print(cp_err)
+                print(sentence)
+                raise
+
+        # Parse output from Lapos
         # TODO: Make this cleaner/faster
         output_list = p_out.split('\n')
         output_list_filtered = [l for l in output_list if not l.startswith('loading the models')]
@@ -149,5 +175,6 @@ class Lapos:
 
 
 if __name__ == '__main__':
-    l = Lapos()
-    print(l.tag_sentence('The involvement of ion channels in B and T lymphocyte activation'))
+    l = Lapos('latin')
+    # print(l.tag_sentence_example('The involvement of ion channels in B and T lymphocyte activation'))
+    print(l.tag_sentence_example('Gallia est omnis divisa in partes tres'))
