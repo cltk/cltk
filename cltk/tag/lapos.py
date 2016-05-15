7a7accfbdcc42e4ecd3f64b70beb4f835bfcad05
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2016-05-14 22:34:59 -0700

    Finish first iteration of Latin Lapos

---
+++ cltk/tag/lapos.py
@@ -0,0 +1,108 @@
+"""Install the Lapos POS tagger."""
+
+import os
+import subprocess
+from sys import platform
+
+
+from cltk.corpus.utils.importer import CorpusImporter
+from cltk.corpus.utils.importer import CorpusImportError
+from cltk.utils.cltk_logger import logger
+
+
+class Lapos:
+    """Class for everything pertaining to installing Lapos."""
+
+    def __init__(self, language):
+        """Constructor."""
+        self.language = language
+        self.operating_system = self._what_os()
+        self._is_cloned_get_make()  # get software if not present
+
+    def _is_cloned_get_make(self):
+        """Check if installed, if not, install it.
+        TODO: This could be 3 functions.
+        """
+        # ! add check here for mac or unix
+        if self.operating_system == 'mac':
+            branch = 'clang'
+        else:
+            branch = 'master'
+        fp = os.path.expanduser('~/cltk_data/multilingual/software/lapos/README.md')
+        if os.path.isfile(fp):
+            return True
+        else:
+            importer = CorpusImporter('multilingual')
+            importer.import_corpus('lapos', branch=branch)
+        if os.path.isfile(fp):
+            print('Cloned Lapos successfully.')
+            self.make()
+            return True
+        else:
+            logger.error("Something went wrong with importing the Lapos tagger on the '{}' branch.".format(branch))
+            raise CorpusImportError
+
+    def _what_os(self):
+        """Get operating system."""
+        if platform == "linux" or platform == "linux2":
+            _platform = 'linux'
+        elif platform == "darwin":
+            _platform = 'mac'
+        elif platform == "win32":
+            _platform = 'windows'
+        logger.info("Detected '{}' operating system.".format(_platform))
+
+        return _platform
+
+    def make(self):
+        """Build program."""
+        fp = os.path.expanduser('~/cltk_data/multilingual/software/lapos')
+        p_out = subprocess.call('cd {} && make'.format(fp), shell=True, stdout=subprocess.DEVNULL)
+
+        if p_out == 0:
+            print('Lapos built successfully.')
+            logger.info('Lapos build successfully.')
+        else:
+            print('Lapos did not build successfully.')
+            logger.error('Lapos did not build successfully.')
+
+    def tag_sentence(self, sentence):
+        """Tag using Lapos model.
+
+        TODO: Figure out how to pre-load model (loading is slow). Or force users to bulk-convert files or strings.
+        """
+        fp_lapos = os.path.expanduser('~/cltk_data/multilingual/software/lapos')
+        fp_model = os.path.expanduser('~/cltk_data/{0}/model/{1}_models_cltk/taggers/pos'.format(self.language, self.language))  # rel from Lapos dir
+        try:
+            p_out = subprocess.check_output('cd {0} && echo "{1}" | ./lapos -t -m {2}'.format(fp_lapos, sentence, fp_model),
+                                            shell=True,
+                                            stderr=subprocess.STDOUT,
+                                            universal_newlines=True)
+        except subprocess.CalledProcessError as cp_err:
+            logger.error('Lapos call failed. Check installation.')
+            logger.error(sentence)
+            print(cp_err)
+            raise
+
+        # Parse output from Lapos
+        # TODO: Make this cleaner/faster
+        output_list = p_out.split('\n')
+        output_list_filtered = [l for l in output_list if not l.startswith('loading the models')]
+        output_list_filtered = [l for l in output_list_filtered if not l == 'done']
+        output_list_filtered = [l for l in output_list_filtered if l]
+
+        for line in output_list_filtered:
+            word_tags = line.split(' ')
+            tagged_sentence = []
+            for word_tag in word_tags:
+                word, tag = word_tag.split('/')
+                word_tag_tuple = (word, tag)
+                tagged_sentence.append(word_tag_tuple)
+
+            return tagged_sentence
+
+
+if __name__ == '__main__':
+    l = Lapos('latin')
+    s = "Quis novus hic nostris successit sedibus hospes.".lower()
+    print(l.tag_sentence(s))
--- docs/latin.rst
+++ docs/latin.rst
@@ -308,7 +308,9 @@ TnT tagger
 CRF tagger
 ``````````
 
-.. warning:: This tagger's accuracy has not yet been tested.
+.. warning:: This tagger's accuracy has not yet been evaluated.
+
+.. note:: As designed, Lapos is slow to load a pre-trained module into memory, so tagging sentence-by-sentence can be slow.
 
 We use the NLTK's CRF tagger. For information on it, see `the NLTK docs <http://www.nltk.org/_modules/nltk/tag/crf.html>`_.
 
@@ -323,6 +325,35 @@ We use the NLTK's CRF tagger. For information on it, see `the NLTK docs <http://
     ('in', 'R--------'),
     ('partes', 'N-P---FA-'),
     ('tres', 'M--------')]
+
+
+Lapos tagger
+
+.. warning:: This tagger's accuracy has not yet been evaluated.
+
+The Lapos tagger is C++ code which must be compiled before use, so you'll need a C compiler (normally GCC Linux, Clang on Mac and the *BSDs, and Cygwin on Windows).
+
+When you first instantiate the `Lapos()` class, it will check if it is already present. If not the Lapos source will will be downloaded and a `make` command attempted.
+
+.. code-block:: python
+
+   In [1]: from cltk.tag.lapos import Lapos
+
+   In [2]: lapos_tagger = Lapos('latin')
+   Cloned Lapos successfully.
+   Lapos built successfully.
+
+   In [3]: sentence = 'Gallia est omnis divisa in partes tres'
+
+   In [4]: lapos_tagger.tag_sentence(sentence)
+   Out[4]:
+   [('Gallia', 'n-p---nn-'),
+    ('est', 'v3spia---'),
+    ('omnis', 'n-p---fa-'),
+    ('divisa', 't-prppnn-'),
+    ('in', 'p-p---fd-'),
+    ('partes', 'n-p---fa-'),
+    ('tres', 'a-p---nbc')]
 
 
 Prosody Scanning
