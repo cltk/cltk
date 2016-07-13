8d5290cc381582dbed8afae097827b57aa78a17a
Tyler Kirby <joseph.kirby12@ncf.edu>
2016-07-13 09:43:05 -0400

    Fixed punctuation and None Type Handeling

--- cltk/prosody/latin/macronizer/macronizer.py
+++ cltk/prosody/latin/macronizer/macronizer.py
@@ -1,5 +1,6 @@
 """
 Macronizes Latin word by matching its POS tag to its entry in the Morpehus database.
+
 All unfound and ambiguous entries are logged in cltk_log.
 
 Example:
@@ -19,17 +20,21 @@ Out    : [('n-s---fb-', 'Gallia', 'Galliā')
 
 from cltk.tag.pos import POSTag
 from cltk.utils.cltk_logger import logger
-from cltk.prosody.latin.macronizer import macrons
+import macrons
 import random
+import re
 
 AVAILABLE_TAGGERS = ['tag_ngram_123_backoff', 'tag_tnt', 'tag_crf']
 
 
 class Macronizer(object):
     """
+    Macronize Latin words.
+
     Macronize text by using the POS tag to find the macronized form within the
     morpheus database.
     """
+
     def __init__(self, tagger):
         """Initialize class with chosen tagger."""
         self.tagger = tagger.lower()
@@ -39,6 +44,7 @@ class Macronizer(object):
     def retrieve_tag(self, text):
         """
         Tag text with chosen tagger and clean tags.
+
         Tag format: [('word', 'tag')]
 
         :param text: string
@@ -47,17 +53,20 @@ class Macronizer(object):
         """
         if self.tagger == 'tag_ngram_123_backoff': # Data format: Perseus Style (see https://github.com/cltk/latin_treebank_perseus)
             tags = POSTag('latin').tag_ngram_123_backoff(text.lower())
-            return [(tag[0], tag[1].lower()) for tag in tags]
+            return [(tag[0], tag[1]) for tag in tags]
         elif self.tagger == 'tag_tnt':
             tags = POSTag('latin').tag_tnt(text.lower())
-            return [(tag[0], tag[1].lower()) for tag in tags]
+            return [(tag[0], tag[1]) for tag in tags]
         elif self.tagger == 'tag_crf':
             tags = POSTag('latin').tag_crf(text.lower())
-            return [(tag[0], tag[1].lower()) for tag in tags]
+            return [(tag[0], tag[1]) for tag in tags]
+
+
 
     def retrieve_morpheus_entry(self, text):
         """
         Return morpheus entry.
+
         :param text: string
         :return: list of list of tuples from morpheus db
         :rtype : list
@@ -65,9 +74,16 @@ class Macronizer(object):
         tags = Macronizer(self.tagger).retrieve_tag(text)
         entries = []
         for tag in tags:
-            pos_tag = tag[1]
+            if tag[1] is not None:
+                pos_tag = tag[1].lower()
+            else:
+                continue
             entry = macrons.vowel_len_map.get(tag[0])
-            matching_entries = [entries for entries in entry if pos_tag in entries]
+            if entry is not None:
+                matching_entries = [entries for entries in entry if pos_tag in entries]
+            else:
+                logger.info('Entry {} not found in Morpheus.'.format(tag[0]))
+                continue
             if len(entry) == 0 or len(matching_entries) == 0:
                 entries.append([tag + (None,)])
                 logger.info('No entry found for "{}".'.format(tag))
@@ -80,6 +96,7 @@ class Macronizer(object):
     def macronize(self, text):
         """
         Return list of tuples containing the POS tag, token, and macronized token.
+
         :param text: string
         :return: tuples with POS tag, token, and macornized token
         :rtype : list
@@ -96,6 +113,12 @@ class Macronizer(object):
 
 
 if __name__ == "__main__":
-    test = "gallia est omnis divisa partes tres"
-    for tuple in (Macronizer('tag_tnt').macronize(test)):
+    def clean_text(text):
+        return re.sub('[^a-zA-Z\s]+', '', text)
+    test = "/Users/Tyler1/cltk_data/latin/text/latin_text_latin_library/caesar/gall1.txt"
+    with open(test) as file:
+        caesar = file.read()
+    for tuple in (Macronizer('tag_ngram_123_backoff').macronize(clean_text(caesar))):
         print(tuple)
+    # test = "[1] gallia est divisa omnis tres partes."
+    # print(Macronizer("tag_ngram_123_backoff").retrieve_morpheus_entry(test))
