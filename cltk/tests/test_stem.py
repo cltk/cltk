a91b1284ca291c287fac7b0b342b4736cbe9740b
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2016-07-16 11:49:13 -0700

    disambiguate indian syllab class in test

--- cltk/tests/test_stem.py
+++ cltk/tests/test_stem.py
@@ -7,7 +7,7 @@ from cltk.corpus.utils.importer import CorpusImporter
 from cltk.stem.latin.j_v import JVReplacer
 from cltk.stem.latin.stem import Stemmer
 from cltk.stem.lemma import LemmaReplacer
-from cltk.stem.latin.syllabifier import Syllabifier
+from cltk.stem.latin.syllabifier import IndianSyllabifier
 from cltk.stem.sanskrit.indian_syllabifier import Syllabifier
 
 import os
@@ -197,20 +197,20 @@ class TestSequenceFunctions(unittest.TestCase):  # pylint: disable=R0904
     def test_syllabify(self):
         """Test Indic Syllabifier method"""
         correct = ['न', 'म', 'स्ते']
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         current = syllabifier.orthographic_syllabify('नमस्ते')
         self.assertEqual(current, correct)
 
     def test_get_offset(self):
         """Test Indic Syllabifier get_offset method"""
         correct = 40
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         current = syllabifier.get_offset('न', 'hi')
         self.assertEqual(current, correct)
 
     def test_coordinated_range(self):
         """Test Indic Syllabifier in_coordinated_range method"""
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         current = syllabifier.get_offset('न', 'hi')
         current1 = syllabifier.in_coordinated_range_offset(current)
         self.assertTrue(current1)
@@ -220,56 +220,56 @@ class TestSequenceFunctions(unittest.TestCase):  # pylint: disable=R0904
     def test_phonetic_vector(self):
         cor = [0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1,1,0,1,0,0,0,0,0,0,0,0,0,0]
         correct = bytearray(cor)
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         current = syllabifier.get_phonetic_feature_vector('न', 'hi')
         # self.assertTrue(current, correct)
     '''
 
     def test_is_misc(self):
         """Test Indic Syllabifier is_misc method"""
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         v = syllabifier.get_phonetic_feature_vector('न', 'hi')
         self.assertFalse(syllabifier.is_misc(v))
 
     def test_is_consonant(self):
         """Test Indic Syllabifier is_consonant method"""
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         v = syllabifier.get_phonetic_feature_vector('न', 'hi')
         self.assertTrue(syllabifier.is_consonant(v))
 
     def test_is_vowel(self):
         """Test Indic Syllabifier is_vowel method"""
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         v = syllabifier.get_phonetic_feature_vector('न', 'hi')
         self.assertFalse(syllabifier.is_vowel(v))
 
     def test_is_anusvaar(self):
         """Test Indic Syllabifier is_anusvaar method"""
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         v = syllabifier.get_phonetic_feature_vector('न', 'hi')
         self.assertFalse(syllabifier.is_anusvaar(v))
 
     def test_is_plosive(self):
         """Test Indic Syllabifier is_plosive method"""
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         v = syllabifier.get_phonetic_feature_vector('न', 'hi')
         self.assertTrue(syllabifier.is_plosive(v))
 
     def test_is_nukta(self):
         """Test Indic Syllabifier is_nukta method"""
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         v = syllabifier.get_phonetic_feature_vector('न', 'hi')
         self.assertFalse(syllabifier.is_nukta(v))
 
     def test_is_valid(self):
         """Test Indic Syllabifier is_valid method"""
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         v = syllabifier.get_phonetic_feature_vector('न', 'hi')
         self.assertTrue(syllabifier.is_valid(v))
 
     def test_is_dependent_vowel(self):
         """Test Indic Syllabifier is_dependent_vowel method"""
-        syllabifier = Syllabifier('hindi')
+        syllabifier = IndianSyllabifier('hindi')
         v = syllabifier.get_phonetic_feature_vector('न', 'hi')
         self.assertFalse(syllabifier.is_dependent_vowel(v))
 
