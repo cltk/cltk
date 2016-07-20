c027bed99911a04cbac6583c2445b613d1a48199
Sourav Singh <souravsingh@users.noreply.github.com>
2016-07-20 10:29:02 -0700

    Add function for longest substring

--- cltk/text_reuse/comparison.py
+++ cltk/text_reuse/comparison.py
@@ -97,3 +97,19 @@ class Comparison:
             self.language_b = text_ref['language']
 
         return
+    
+    def long_substr(str_a, str_b):
+        """
+        Looks for a longest common string between any two given strings passed
+        :param str_a: str
+        :param str_b: str
+        
+        """
+        data = [str_a, str_b]
+        substr = ''
+        if len(data) > 1 and len(data[0]) > 0:
+            for i in range(len(data[0])):
+                for j in range(len(data[0])-i+1):
+                    if j > len(substr) and all(data[0][i:i+j] in x for x in data):
+                        substr = data[0][i:i+j]
+        return substr.strip()
