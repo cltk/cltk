4142351b2de16a5c3d41d284c19ca1ec0c7deff3
Sourav Singh <souravsingh@users.noreply.github.com>
2016-07-27 19:03:12 +0530

    Update comparison.py

--- cltk/text_reuse/comparison.py
+++ cltk/text_reuse/comparison.py
@@ -98,22 +98,20 @@ class Comparison:
 
         return
     
-    def long_substring(self,str_a, str_b):
-        """
-        Looks for a longest common string between any two given strings passed
-        :param str_a: str
-        :param str_b: str
+def long_substring(str_a, str_b):
+    """
+    Looks for a longest common string between any two given strings passed
+    :param str_a: str
+    :param str_b: str
         
-        Big Thanks to Pulkit Kathuria(@kevincobain2000) for the function 
-        The function is derived from jProcessing toolkit suite
-        """
-        self.str_a= str_a
-        self.str_b= str_b
-        data = [str_a, str_b]
-        substr = ''
-        if len(data) > 1 and len(data[0]) > 0:
-            for i in range(len(data[0])):
-                for j in range(len(data[0])-i+1):
-                    if j > len(substr) and all(data[0][i:i+j] in x for x in data):
-                        substr = data[0][i:i+j]
-        return substr.strip()
+    Big Thanks to Pulkit Kathuria(@kevincobain2000) for the function 
+    The function is derived from jProcessing toolkit suite
+    """
+    data = [str_a, str_b]
+    substr = ''
+    if len(data) > 1 and len(data[0]) > 0:
+        for i in range(len(data[0])):
+            for j in range(len(data[0])-i+1):
+                if j > len(substr) and all(data[0][i:i+j] in x for x in data):
+                    substr = data[0][i:i+j]
+    return substr.strip()
