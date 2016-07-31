3708f91f5b45903464e9a4a21d0cf123381f509f
Sourav Singh <souravsingh@users.noreply.github.com>
2016-07-31 18:02:12 +0530

    Add Old English

--- cltk/corpus/utils/importer.py
+++ cltk/corpus/utils/importer.py
@@ -11,6 +11,7 @@ from cltk.corpus.multilingual.corpora import MULTILINGUAL_CORPORA
 from cltk.corpus.pali.corpora import PALI_CORPORA
 from cltk.corpus.punjabi.corpora import PUNJABI_CORPORA
 from cltk.corpus.tibetan.corpora import TIBETAN_CORPORA
+from cltk.corpus.old_english.corpora import OLD_ENGLISH_CORPORA
 from cltk.utils.cltk_logger import logger
 
 import errno
@@ -28,7 +29,7 @@ __author__ = ['Kyle P. Johnson <kyle@kyle-p-johnson.com>',
 __license__ = 'MIT License. See LICENSE.'
 
 
-AVAILABLE_LANGUAGES = ['chinese', 'coptic', 'greek', 'latin', 'multilingual', 'pali', 'punjabi', 'tibetan', 'sanskrit']
+AVAILABLE_LANGUAGES = ['chinese', 'coptic', 'greek', 'latin', 'multilingual', 'pali', 'punjabi', 'tibetan', 'sanskrit','old_english']
 CLTK_DATA_DIR = '~/cltk_data'
 LANGUAGE_CORPORA = {'chinese': CHINESE_CORPORA,
                     'coptic': COPTIC_CORPORA,
@@ -38,7 +39,8 @@ LANGUAGE_CORPORA = {'chinese': CHINESE_CORPORA,
                     'pali': PALI_CORPORA,
                     'punjabi': PUNJABI_CORPORA,
                     'tibetan': TIBETAN_CORPORA,
-                    'sanskrit': SANSKRIT_CORPORA,}
+                    'sanskrit': SANSKRIT_CORPORA,
+                    'old_english':OLD_ENGLISH_CORPORA}
 
 
 class CorpusImportError(Exception):
