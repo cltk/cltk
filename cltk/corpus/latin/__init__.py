355c5f7cc139fdd53388f0c7b79982b898bfd613
Patrick J. Burns <patrick@diyclassics.org>
2016-08-05 12:41:51 -0400

    Add custom word/sentence tokenizers to Latinlibrary 'book' loader

--- cltk/corpus/latin/__init__.py
+++ cltk/corpus/latin/__init__.py
@@ -6,8 +6,12 @@ __license__ = 'MIT License. See LICENSE.'
 """
 CLTK Latin corpus readers
 """
+
 import os.path
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
+
+from cltk.tokenize.sentence import TokenizeSentence
+from cltk.tokenize.word import WordTokenizer
 
 # Would like to have this search through a CLTK_DATA environment variable
 # Better to use something like make_cltk_path in cltk.utils.file_operations?
@@ -16,7 +20,11 @@ cltk_path = os.path.join(home, 'CLTK_DATA')
 
 # Latin Library
 try:
-    latinlibrary = PlaintextCorpusReader(cltk_path + '/latin/text/latin_text_latin_library', '.*\.txt', encoding='utf-8')
+    latinlibrary = PlaintextCorpusReader(cltk_path + '/latin/text/latin_text_latin_library', 
+    '.*\.txt',
+    word_tokenizer=WordTokenizer('latin'), 
+    sent_tokenizer=TokenizeSentence('latin'), 
+    encoding='utf-8')    
     pass
 except IOError as e:
     print("Corpus not found. Please check that the Latin Library is installed in CLTK_DATA.")
