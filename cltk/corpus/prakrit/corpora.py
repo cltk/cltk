e2808d60eae3236e366c55f1df597b172df48991
ykl7 <ykl7@users.noreply.github.com>
2016-11-30 23:50:37 +0530

    added prakrit gretil corpora (#425)

---
+++ cltk/corpus/prakrit/corpora.py
@@ -0,0 +1,11 @@
+"""Prakrit language corpora available for download or loading locally.
+All remote corpora hosted by github on the cltk organization account, eg:
+'http://github.com/cltk' + name
+"""
+
+PRAKRIT_CORPORA = [
+    {'name': 'prakrit_texts_gretil',
+    'location': 'remote',
+     'type': 'text',
+     'origin': 'https://github.com/cltk/prakrit_texts_gretil.git'}
+]
