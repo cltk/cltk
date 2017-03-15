613965137072c36c1cf44b592cb8ed5044ba778a
Talha Javed Mukhtar <tjaved.bscs15seecs@seecs.edu.pk>
2017-03-15 15:12:02 +0500

    Added alphabet.py for Urdu

---
+++ cltk/corpus/urdu/alphabet.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+
+#Persian digits from 0 to 9
+DIGITS = ['۰', '۱', '۲', '۳', '٤', '۵', '٦', '۷', '۸', '۹']
+
+#The Persian Alphabet
+VOWELS = ['ا', 'و', 'ی', 'ے']
+
+CONSONANTS = ['ء', 'ب', 'پ', 'ت', 'ٹ', 'ث', 'ج', 'چ', 'ح', 'خ', 'د', 'ڈ', 'ذ', 'ر', 'ڑ', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'ه', 'ھ']
+
+
+#Characters that are used to impart accent
+TASHDEED = ['َ', 'ِ', 'ُ']
+
+#Special Characters
+SPECIAL = ['ﺁ', 'ۀ', 'ﻻ']
