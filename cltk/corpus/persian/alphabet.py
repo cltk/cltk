864d490f804c6d4f5081cbeac307d14c9e6074af
Talha Javed Mukhtar <tjaved.bscs15seecs@seecs.edu.pk>
2017-03-15 10:35:16 +0500

    Added alphabet for persian

---
+++ cltk/corpus/persian/alphabet.py
@@ -0,0 +1,20 @@
+"""
+Persian (/ˈpɜːrʒən/ or /ˈpɜːrʃən/), also known by its endonym Farsi, is one of the Western Iranian languages within the Indo-Iranian branch of the Indo-European language family. 
+It is primarily spoken in Iran, Afghanistan (officially known as Dari since 1958), and Tajikistan (officially known as Tajiki since the Soviet era), and some other regions which historically were Persianate societies.
+It is mostly written in the Persian alphabet, a modified variant of the Arabic script.
+"""
+
+#Persian digits from 0 to 9
+DIGITS = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
+
+#The Persian Alphabet
+VOWELS = ['ا', 'و', 'ی']
+
+CONSONANTS = ['ء', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'ه']
+
+
+#Characters that are used to impart accent
+TASHDEED = ['َ', 'ِ', 'ُ']
+
+#Special Characters
+SPECIAL = ['ﺁ', 'ۀ', 'ﻻ']
