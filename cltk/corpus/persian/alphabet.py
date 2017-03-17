2a2c1cbc391fc0055c0c2815ab8820246c4e3855
talhajavedmukhtar <tjaved.bscs15seecs@seecs.edu.pk>
2017-03-17 23:22:56 +0500

    Persian docs (#506)
    * Added alphabet.py for Persian
    
    * Added alphabet.py for Urdu
    
    * Updated urdu/alphabet.py
    
    Added language description
    
    * cleanup persian alpha
    
    * cleanup arabic alpha
    
    * Modified persian/alphabet.py and added docs/persian.rst

--- cltk/corpus/persian/alphabet.py
+++ cltk/corpus/persian/alphabet.py
@@ -4,13 +4,13 @@
 #Persian digits from 0 to 9
 DIGITS = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
 
+#Diacritics
+SHORT_VOWELS = ['َ', 'ِ', 'ُ']
+
 #The Persian Alphabet
-VOWELS = ['ا', 'و', 'ی']
+LONG_VOWELS = ['ا', 'و', 'ی']
 
 CONSONANTS = ['ء', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'ه']
-
-#Characters that are used to impart accent
-TASHDEED = ['َ', 'ِ', 'ُ']
 
 #Special Characters
 SPECIAL = ['ﺁ', 'ۀ', 'ﻻ']
