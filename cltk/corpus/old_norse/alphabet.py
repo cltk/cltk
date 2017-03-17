1ac128e795eb6b62142441adc22a81077f2e3534
Aakash5897 <aakash.dokka@gmail.com>
2017-03-17 12:41:01 -0700

    added alphabet.py

---
+++ cltk/corpus/old_norse/alphabet.py
@@ -0,0 +1,22 @@
+#all the alphabets in old norse
+ALPHABETS=['a','b','c','d','ð','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','þ','æ','ǫ','ø','œ']
+
+
+#All the vowels in old norse
+VOWELS=['a', 'á', 'e', 'é', 'i', 'í', 'o', 'ó', 'u', 'ú', 'y', 'ý', 'æ','ǫ́', 'ǫ', 'ø', 'œ']
+
+
+#All the consonants in old norse
+CONSONANTS=["b","c","d","ð","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z","þ"]
+
+
+#All the Dipthongs in old norse
+DIPTHONGS=["au","ei","ey"]
+
+#Similar to other Germanic languages, the vowel change, or umlaut, operates in Old Norse. There are two kinds: i-umlaut, and u-umlaut.
+#The i-umlaut appears thus:
+UMLAUT_CHART1={"a":"e","á":"æ","o":"ø","ó":"œ","u":"y","ú":"ý"}
+
+
+#In certain circumstances, the presence of a 'u' in the endings of adjectives or nouns causes what is called 'u-umlaut' or 'back mutation' to the stem vowel. This is how that change presents itself:
+UMLAUT_CHART2={"a":"ǫ","á":"ǫ́","e":"ø","é":"œ","i":"y","í":"ý","a":"u"}
