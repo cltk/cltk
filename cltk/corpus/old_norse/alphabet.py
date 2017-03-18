903cfa4889281bb335d18d8323c952a66848e2fc
Aakash Dm <aakash.dokka@gmail.com>
2017-03-18 19:36:09 +0000

    Update alphabet.py

--- cltk/corpus/old_norse/alphabet.py
+++ cltk/corpus/old_norse/alphabet.py
@@ -11,8 +11,14 @@ CONSONANTS = ["b", "c", "d", "ð", "f", "g", "h", "j", "k", "l", "m", "n", "p", 
 DIPTHONGS = ["au","ei","ey"]
 
 #Similar to other Germanic languages, the vowel change, or umlaut, operates in Old Norse. There are two kinds: i-umlaut, and u-umlaut.
+#I-umlaut caused vowels to become fronted, and in that way, drawn towards the ‘i’.
 #The i-umlaut appears thus:
 UMLAUT_I = {"a": "e", "á": "æ", "o": "ø", "ó": "œ", "u": "y", "ú": "ý"}
+#examples of i-umluat:
+#hafa > hef
+#blása > blæs
 
 #In certain circumstances, the presence of a 'u' in the endings of adjectives or nouns causes what is called 'u-umlaut' or 'back mutation' to the stem vowel. This is how that change presents itself:
 UMLAUT_U = {"a": "ǫ", "á": "ǫ́", "e": "ø", "é": "œ", "i": "y", "í": "ý", "a": "u"}
+#example of u-umlaut:
+#saku>sǫk  
