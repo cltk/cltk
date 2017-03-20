65fc47e31f9aecd4c750015ba0ae5d3138896ea3
LBenzahia <lakhdar.benzahia@gmail.com>
2017-03-20 21:47:29 +0100

    add first arabic corpus

---
+++ cltk/corpus/arabic/corpora.py
@@ -0,0 +1,9 @@
+"""Available Arabic corpora available for download."""
+
+ARABIC_CORPORA = [
+    {'name': 'Tashkeela2_Arabic_Vocalized_text',
+     'markup':'xml',
+     'origin': 'git@github.com:LBenzahia/tashkeela2.git',
+     'location': 'remote',
+     'type': 'text'},
+]
