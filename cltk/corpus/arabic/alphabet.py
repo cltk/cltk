10e60a5f6d3ccf3c736a557a15be5c9645f81b30
talhajavedmukhtar <tjaved.bscs15seecs@seecs.edu.pk>
2017-03-23 21:22:56 +0500

    Added alphabet and docs for Arabic (#519)

---
+++ cltk/corpus/arabic/alphabet.py
@@ -0,0 +1,16 @@
+"""Arabic alphabet"""
+
+
+#Arabic digits from 0 to 9
+DIGITS = ['۰', '۱', '۲', '۳', '٤', '۵', '٦', '۷', '۸', '۹']
+
+#Diacritics
+SHORT_VOWELS = ['َ', 'ِ', 'ُ']
+
+#The Persian Alphabet
+LONG_VOWELS = ['ا', 'و', 'ي']
+
+CONSONANTS = ['ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'ل', 'م', 'ن', 'ه']
+
+#Special Characters
+SPECIAL = ['‎ّ', 'ﻻ']
