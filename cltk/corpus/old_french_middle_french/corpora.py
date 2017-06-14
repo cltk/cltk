a345eeb1451fd30d3a32b809a931d92bde7f47fd
nat1881 <nat1881@users.noreply.github.com>
2017-06-14 22:43:21 +0100

    add old french and middle french

--- cltk/corpus/old_french_middle_french/corpora.py
+++ cltk/corpus/old_french_middle_french/corpora.py
@@ -1,7 +1,7 @@
 OLD_FRENCH_MIDDLE_FRENCH_CORPORA = [
   {'encoding': 'utf-8',
-     'origin': 'https://github.com/cltk/old_french_middle_french.git',
-     'name': 'oldfrenchmiddlefrench',
-     'location': 'remote',
-     'type': 'text'}
+   'name': 'oldfrenchmiddlefrench',
+   'origin': 'https://github.com/cltk/old_french_middle_french.git',
+   'location': 'remote',
+   'type': 'text'}
 ]
