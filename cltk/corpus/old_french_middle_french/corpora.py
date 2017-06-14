cea0bfd34928084b69cff808c29407e6a5c0c148
nat1881 <nat1881@users.noreply.github.com>
2017-06-14 22:40:42 +0100

    Create corpora.py

--- cltk/corpus/old_french_middle_french/corpora.py
+++ cltk/corpus/old_french_middle_french/corpora.py
@@ -1,6 +1,6 @@
 OLD_FRENCH_MIDDLE_FRENCH_CORPORA = [
   {'encoding': 'utf-8',
-     'origin': 'https://github.com/cltk/oldfrenchmiddlefrench.git',
+     'origin': 'https://github.com/cltk/old_french_middle_french.git',
      'name': 'oldfrenchmiddlefrench',
      'location': 'remote',
      'type': 'text'}
