b5577b27cda2ed52c13fefef527c92a81d73e983
nat1881 <nat1881@users.noreply.github.com>
2017-06-14 23:41:07 +0100

    Dictionary upload

---
+++ old_french_middle_french/Lexique_cleanup.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+#encoding: utf-8
+
+import re
+import os, sys
+
+
+with open('lexiquedelancienfrancais.txt', 'r') as in_file:
+       stripped = (line.strip() for line in in_file)
+       for stripped in stripped:
+              entry, PoS, definition_sce = stripped.split(',', 2)
+              definition = definition_sce.replace(',','')
+              if not entry[0].isdigit():
+                  entry = " ".join(("1.", entry))
+              entry = entry.replace('1.','1.,')
+              entry = entry.replace('2.', '2.,')
+              entry = entry.replace('3.', '3.,')
+              entry = entry.replace('*', '')
+
+              entries = (entry, PoS, definition)
+              output = ",".join(entries)
+              with open("lex.txt", "a") as text_file:
+                  text_file.write(output +"\n")
+                  text_file.close()
+              print(output)
+
+
+
+
+
+
---
+++ old_french_middle_french/txttocsv.py
@@ -0,0 +1,14 @@
+import csv
+import re
+#encoding: utf-8
+
+
+import csv
+
+with open('lex.txt', 'r') as in_file:
+    stripped = (line.strip() for line in in_file)
+    lines = (line.split(",") for line in stripped if line)
+    with open('lexique.csv', 'w') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerow(('lemma number','entry', 'Pos', 'definition'))
+        writer.writerows(lines)
