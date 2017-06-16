a8359e19d9942274856cd54146b59f73fc6e8a08
nat1881 <nat1881@users.noreply.github.com>
2017-06-16 14:29:59 +0100

    Update corpora.py

--- cltk/corpus/old_french_middle_french/corpora.py
+++ cltk/corpus/old_french_middle_french/corpora.py
@@ -1,7 +1,12 @@
 OLD_FRENCH_MIDDLE_FRENCH_CORPORA = [
   {'encoding': 'utf-8',
-   'name': 'oldfrenchmiddlefrench',
-   'origin': 'https://github.com/cltk/old_french_middle_french.git',
+   'name': 'french',
+   'origin': 'https://github.com/nat1881/french',
+   'location': 'remote',
+   'type': 'text'}
+  {'encoding': 'utf-8',
+   'name': 'french_lexicon',
+   'origin': 'https://github.com/nat1881/french_lexicon_cltk',
    'location': 'remote',
    'type': 'text'}
 ]
