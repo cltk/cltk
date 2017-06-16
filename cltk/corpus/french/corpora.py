9335bc4dc5a99938ecb072ff2f3e995fc1aa9da2
nat1881 <nat1881@users.noreply.github.com>
2017-06-16 18:15:59 +0100

    Update corpora.py

--- cltk/corpus/french/corpora.py
+++ cltk/corpus/french/corpora.py
@@ -1,7 +1,7 @@
 FRENCH_CORPORA = [
   {'encoding': 'utf-8',
-   'name': 'french',
-   'origin': 'https://github.com/nat1881/french.git',
+   'name': 'french_text',
+   'origin': 'https://github.com/nat1881/french-text.git',
    'location': 'remote',
    'type': 'text'}
   {'encoding': 'utf-8',
