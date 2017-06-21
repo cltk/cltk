c1b324265e6c74e326942bdb47482b9dfb700f92
LBenzahia <lakhdar.benzahia@gmail.com>
2017-06-21 23:04:20 +0100

    replace if with elif

--- cltk/tokenize/word.py
+++ cltk/tokenize/word.py
@@ -36,10 +36,9 @@ class WordTokenizer:  # pylint: disable=too-few-public-methods
 
     def tokenize(self, string):
         """Tokenize incoming string."""
-
         if self.language == 'latin':
             tokens = tokenize_latin_words(string)
-        if self.language == 'arabic':
+        elif self.language == 'arabic':
             tokens = tokenize_arabic_words(string)
         else:
             tokens = nltk_tokenize_words(string)
