839c21cab4e4ca1b2aa54e33d4c308f263d9ff3e
todd-cook <todd.g.cook@gmail.com>
2017-07-03 12:07:04 -0700

    corrected documentation & doctest comments that were causing errors. doctests run with an added command line switch: nosetests --no-skip --with-coverage --cover-package=cltk --with-doctest

--- cltk/tests/test_scansion.py
+++ cltk/tests/test_scansion.py
@@ -4,16 +4,14 @@ __license__ = 'MIT License. See LICENSE.'
 
 import unittest
 import doctest
+from cltk.prosody.latin.HexameterScanner import HexameterScanner
+import cltk.prosody.latin.Hexameter
 
-def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite('cltk.prosody.latin.HexameterScanner'))
-    tests.addTests(doctest.DocTestSuite('cltk.prosody.latin.Hexameter'))
-    tests.addTests(doctest.DocTestSuite('cltk.prosody.latin.MetricalValidator'))
-    tests.addTests(doctest.DocTestSuite('cltk.prosody.latin.ScansionConstants'))
-    tests.addTests(doctest.DocTestSuite('cltk.prosody.latin.ScansionFormatter'))
-    tests.addTests(doctest.DocTestSuite('cltk.prosody.latin.Syllabifier'))
-    tests.addTests(doctest.DocTestSuite('cltk.prosody.latin.StringUtils'))
-    return tests
 
 class TestScansionFunctions(unittest.TestCase):  # pylint: disable=R0904
     """Class for unittest"""
+
+    def test_hexameter(self):
+        scanner = HexameterScanner()
+        my_hex = scanner.scan("impulerit. Tantaene animis caelestibus irae?")
+        self.assertEqual(my_hex.scansion, '-  U U -    -   -   U U -    - -  U U  -  - ')
