c7ef9e4d4587af9b48fb1a6596fcccc1a7501c51
chandanakgd <kgdilipchandana@gmail.com>
2017-07-29 01:07:16 +0530

    adding alpabet.py file to pali section

---
+++ cltk/corpus/pali/alphabet.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+''' 
+	Auther Chandana K.G.D  GitHub- chandanakgd 
+'''
+'''
+	Pali language has 41 letters 8 VOWELS and 33 CONSTANTS
+'''
+
+
+INDEPENDENT_VOWELS=['අ','ආ','ඉ','ඊ','උ','එ','ඔ']
+
+CONSTANTS=['ක','ඛ','ග','ඝ','ඞ','ච','ඡ','ජ','ඣ','ඤ','ට','ඨ','ඩ','ඪ','ණ','ත','ථ','ද','ධ','න 			','ප','ඵ','බ','භ','ම','ය','ර','ල','ව','ස','හ','ළ','අං''']
+
+DEPENDENT_VOWELS=[' ා',' ි' ,' ී' ,' ු',' ූ',' ෙ',' ො']
+
