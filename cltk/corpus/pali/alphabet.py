ce00b5347e66f29c87d497c75f8747969239745c
Dilip Chandana <kgdilipchandana@gmail.com>
2017-08-01 22:41:57 +0530

    Update alphabet.py
    added source to alphabet.py file

--- cltk/corpus/pali/alphabet.py
+++ cltk/corpus/pali/alphabet.py
@@ -2,15 +2,19 @@
 
 ''' 
 	Auther Chandana K.G.D  GitHub- chandanakgd 
+	source 1: http://www.shraddha.lk/videos/pali-lessons/
+	source 2: https://en.wikipedia.org/wiki/Sinhala_(Unicode_block)
 '''
 '''
 	Pali language has 41 letters 8 VOWELS and 33 CONSTANTS
 '''
-
+'''
+	When a constant combine with independent vowel, if the constant come first and independent vowel connectet with it later it can be replaced with a dependent vowels
+'''
 
 INDEPENDENT_VOWELS=['අ','ආ','ඉ','ඊ','උ','එ','ඔ']
 
-CONSTANTS=['ක','ඛ','ග','ඝ','ඞ','ච','ඡ','ජ','ඣ','ඤ','ට','ඨ','ඩ','ඪ','ණ','ත','ථ','ද','ධ','න 			','ප','ඵ','බ','භ','ම','ය','ර','ල','ව','ස','හ','ළ','අං''']
+CONSTANTS=['ක','ඛ','ග','ඝ','ඞ','ච','ඡ','ජ','ඣ','ඤ','ට','ඨ','ඩ','ඪ','ණ','ත','ථ','ද','ධ','න','ප','ඵ','බ','භ','ම','ය','ර','ල','ව','ස','හ','ළ','අං''']
 
 DEPENDENT_VOWELS=[' ා',' ි' ,' ී' ,' ු',' ූ',' ෙ',' ො']
 
