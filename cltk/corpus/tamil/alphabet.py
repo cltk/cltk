4efeda60abce9f59e81e71a95f63a7ddf512bb77
KumaraDilshan <maradilshan@gmail.com>
2017-08-02 01:16:49 -0800

    initial commit for tamil alphabet

---
+++ cltk/corpus/tamil/alphabet.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+''' 
+	Auther Dilshan Abeysinghe  GitHub- Dilshan Abeysinghe
+'''
+'''
+	Tamil language has 41 letters 8 VOWELS and 33 CONSTANTS
+'''
+
+
+VOWELS=['அ','ஆ','இ','ஈ','உ','ஊ','எ','ஏ','ஐ','ஒ','ஓ','ஔ']
+
+CONSTANTS=['க்','ங்','ச்','ஞ்','ட்','ண்','த்','ந்','ப்','ம்','ய்','ர்','ல்','வ்','ழ்','ள்','ற்','ன்']
+
+GRANTHA_CONSONANTS=['ஜ்','ஶ்','ஷ்','ஸ்','ஹ்','க்ஷ்']
