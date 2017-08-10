2962d70e48d8902d9143f0a106b97857ab1a3a23
nat1881 <nat1881@users.noreply.github.com>
2017-08-10 18:07:29 +0100

    further lem edits

--- cltk/lemmatize/french/backoff_test.py
+++ cltk/lemmatize/french/backoff_test.py
@@ -8,7 +8,8 @@ from nltk.tag.api import TaggerI
 from nltk.tag.sequential import SequentialBackoffTagger, ContextTagger, DefaultTagger, NgramTagger, UnigramTagger, RegexpTagger
 from cltk.tokenize.word import WordTokenizer
 from cltk.utils.file_operations import open_pickle
-from cltk.lemmatize.french.lex import entries
+##from cltk.lemmatize.french.lex import entries
+from cltk.lemmatize.french.french import estre_replace, avoir_replace, first_conj_rules, i_type_rules, u_type_rules, regime_rules, plural_rules, misc_rules, masc_to_fem_rules, determiner_rules, reduction_rules
 
 
 def backoff_lemmatizer(train_sents, lemmatizer_classes, backoff=None):
@@ -79,6 +80,15 @@ class DictLemmatizer(SequentialBackoffLemmatizer):
     defining as its own class, it is clearer that this lemmatizer is
     based on dictionary lookup and does not use training data."""
 
+    def __init__(self, model, backoff=None):
+        """Setup for TrainLemmatizer().
+
+        :param model: Dictionary with form {TOKEN: LEMMA}
+        :param backoff: Next lemmatizer in backoff chain.
+        """
+        SequentialBackoffLemmatizer.__init__(self, backoff)
+        self.model = model
+
     def choose_lemma(self, tokens, index, history):
         """Returns the given token as the lemma.
 
@@ -90,6 +100,68 @@ class DictLemmatizer(SequentialBackoffLemmatizer):
         for lemma, entry in entries:
             if tokens == lemma:
                 return (tokens, lemma)
+
+class RegexpLemmatizer(SequentialBackoffLemmatizer, RegexpTagger):
+    """"""
+
+    def __init__(self, regexps=None, backoff=None):
+        """Setup for RegexpLemmatizer()
+
+        :param regexps: List of tuples of form (PATTERN, REPLACEMENT)
+        :param backoff: Next lemmatizer in backoff chain.
+        """
+        SequentialBackoffLemmatizer.__init__(self, backoff)
+        RegexpTagger.__init__(self, regexps, backoff)
+        self._regexs = regexps
+
+    def choose_lemma(self, tokens, index, history):
+        """Use regular expressions for rules-based lemmatizing based on word endings;
+        tokens are matched for patterns with the base kept as a group; an word ending
+        replacement is added to the (base) group.
+        :param tokens: List of tokens to be lemmatized
+        :param index: Int with current token
+        :param history: List with tokens that have already been lemmatized
+        :return: Str with concatenated lemma
+        """
+        for pattern, replace in self._regexs:
+            if re.search(pattern, tokens[index]):
+                return re.sub(pattern, replace, tokens[index])
+            break
+                # pragma: no cover
+
+class PPLemmatizer(RegexpLemmatizer):
+   
+
+    def __init__(self, regexps=None, pps=None, backoff=None):
+        """Setup PPLemmatizer().
+
+        :param regexps: List of tuples of form (PATTERN, INT) where INT is
+        the principal part number needed to lookup the correct stem.
+        :param backoff: Next lemmatizer in backoff chain.
+        """
+        RegexpLemmatizer.__init__(self, regexps, backoff)
+        self._regexs = [estre_replace, avoir_replace, first_conj_rules, i_type_rules, u_type_rules, regime_rules, plural_rules, misc_rules, masc_to_fem_rules, determiner_rules, reduction_rules]
+
+    def choose_lemma(self, tokens, index, history):
+        """Use regular expressions for rules-based lemmatizing based on
+        principal parts stems. Tokens are matched for patterns with
+        the ending kept as a group; the stem is looked up in a dictionary
+        by PP number (see above) and ending is discarded.
+
+        :param tokens: List of tokens to be lemmatized
+        :param index: Int with current token
+        :param history: List with tokens that have already been lemmatized
+        :return: Str with index[0] from the dictionary value, see above about '0 index'
+        """
+        for regexp in self._regexs:
+            m = re.match(regexp[0], tokens[index])
+            if m:
+                root = m.group(1)
+                match = [lemma for (lemma, pp) in self.pps.items() if root == pp[regexp[1]]]
+                if not match:
+                    pass
+                else:
+                    return match[0]  # Lemma is indexed at zero in PP dictionary
 
 
 
