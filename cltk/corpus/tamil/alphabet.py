0aa5ba59efc55087880ac53c256d362362a80b2d
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2017-08-12 14:55:23 -0700

    ch formatting

--- cltk/corpus/tamil/alphabet.py
+++ cltk/corpus/tamil/alphabet.py
@@ -1,15 +1,10 @@
-# -*- coding: utf-8 -*-
+"""The Tamil language has 41 letters 8 VOWELS and 33 CONSTANTS"""
 
-''' 
-	Auther Dilshan Abeysinghe  GitHub- Dilshan Abeysinghe
-'''
-'''
-	Tamil language has 41 letters 8 VOWELS and 33 CONSTANTS
-'''
+__author__ = ['Dilshan Abeysinghe']
+__license__ = 'MIT License. See LICENSE.'
 
+VOWELS=['அ', 'ஆ', 'இ', 'ஈ', 'உ', 'ஊ',' எ', 'ஏ', 'ஐ', 'ஒ', 'ஓ', 'ஔ']
 
-VOWELS=['அ','ஆ','இ','ஈ','உ','ஊ','எ','ஏ','ஐ','ஒ','ஓ','ஔ']
+CONSTANTS=['க்', 'ங்', 'ச்', 'ஞ்', 'ட்', 'ண்', 'த்', 'ந்', 'ப்', 'ம்', 'ய்', 'ர்', 'ல்', 'வ்', 'ழ்', 'ள்', 'ற்', 'ன்']
 
-CONSTANTS=['க்','ங்','ச்','ஞ்','ட்','ண்','த்','ந்','ப்','ம்','ய்','ர்','ல்','வ்','ழ்','ள்','ற்','ன்']
-
-GRANTHA_CONSONANTS=['ஜ்','ஶ்','ஷ்','ஸ்','ஹ்','க்ஷ்']
+GRANTHA_CONSONANTS=['ஜ்', 'ஶ்', 'ஷ்', 'ஸ்', 'ஹ்', 'க்ஷ்']
