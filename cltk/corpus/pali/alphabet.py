b2ec5e1c7777029fe293ed3c618c14430aa455dd
Dilip Chandana <kgdilipchandana@gmail.com>
2017-08-16 22:58:30 +0530

    Update alphabet.py
    remove doc strings

--- cltk/corpus/pali/alphabet.py
+++ cltk/corpus/pali/alphabet.py
@@ -1,15 +1,5 @@
-# -*- coding: utf-8 -*-
-
 ''' 
 	Auther Chandana K.G.D  GitHub- chandanakgd 
-	source 1: http://www.shraddha.lk/videos/pali-lessons/
-	source 2: https://en.wikipedia.org/wiki/Sinhala_(Unicode_block)
-'''
-'''
-	Pali language has 41 letters 8 VOWELS and 33 CONSTANTS
-'''
-'''
-	When a constant combine with independent vowel, if the constant come first and independent vowel connectet with it later it can be replaced with a dependent vowels
 '''
 
 INDEPENDENT_VOWELS=['අ','ආ','ඉ','ඊ','උ','එ','ඔ']
