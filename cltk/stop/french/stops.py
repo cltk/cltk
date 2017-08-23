8535474e28b1167f6173b4db5341958791caa022
nat1881 <nat1881@users.noreply.github.com>
2017-08-23 12:54:22 +0100

    updated stops file

--- cltk/stop/french/stops.py
+++ cltk/stop/french/stops.py
@@ -5,6 +5,31 @@
 ##from https://fr.wikisource.org/wiki/Grammaire_élémentaire_de_l’ancien_français (available under a
 ##Attribution-ShareAlike 3.0 Creative Commons license.
 
+##Code used to determine most frequent words in the corpus:
+
+##import nltk
+##import re
+
+##from nltk.probability import FreqDist
+##from cltk.tokenize.word import WordTokenizer
+##determines 100 most common words and number of occurrences in the French corpus
+##ignores punctuation and upper-case
+##file_content = open("~/cltk/cltk/stop/french/frenchtexts.txt").read()
+##from cltk.tokenize.word import WordTokenizer
+
+##word_tokenizer = WordTokenizer('french')
+##words = word_tokenizer.tokenize(file_content)
+##fdist = FreqDist(words)
+##prints 100 most common words
+##common_words=fdist.most_common(125)
+##cw_list = [x[0] for x in common_words]
+##outputs 100 most common words to .txt file
+##with open('french_prov_stops.txt', 'a') as f:
+##    for item in cw_list:
+##        print(item, file=f)
+
+
+
 STOPS_LIST = ["et",
 "a",
 "de",
