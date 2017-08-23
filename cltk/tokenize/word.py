7acb3592b41c5066c3fd184bcd14e3c732c53264
nat1881 <nat1881@users.noreply.github.com>
2017-08-23 19:13:16 +0100

    streamlined tokenizer

--- cltk/tokenize/word.py
+++ cltk/tokenize/word.py
@@ -232,22 +232,7 @@ def tokenize_french_words(string):
 
     ##Dealing with punctuation
     text = re.sub(r"\'", r"' ", text)
-    text = re.sub(r",", r" ,", text)
-    text = re.sub(r"\.", r" .", text)
-    text = re.sub(r";", r" ;", text)
-    text = re.sub(r":", r" :", text)
-    text = re.sub(r"!", r" !", text)
-    text = re.sub(r"\?", r" ?", text)
-    text = re.sub(r"\\\"", r" \\\" ", text)
-    text = re.sub(r"\(", r"\( ", text)
-    text = re.sub(r"\)", r" \)", text)
-   # _treebank_word_tokenizer = TreebankWordTokenizer()
-   # improved_open_quote_regex = re.compile(u'([«“‘])', re.U)
-   # improved_close_quote_regex = re.compile(u'([»”’])', re.U)
-   # improved_punct_regex = re.compile(r'([^\.])(\.)([\]\)}>"\'' u'»”’ ' r']*)\s*$', re.U)
-   # _treebank_word_tokenizer.STARTING_QUOTES.insert(0, (improved_open_quote_regex, r' \1 '))
-   # _treebank_word_tokenizer.ENDING_QUOTES.insert(0, (improved_close_quote_regex, r' \1 '))
-   # _treebank_word_tokenizer.PUNCTUATION.insert(0, (improved_punct_regex, r'\1 \2 \3 '))
+    text = re.sub("(?<=.)(?=[.!?)(\";:,«»\-])", " ", text)
 
     results = str.split(text)
     return (results)
