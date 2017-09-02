de85dad2f93846367c1c566cacf3861297d5163e
MaheshBhosale <maheshbhosale24051996@gmail.com>
2017-09-02 22:18:20 +0530

    Adding marathi corpus from wikisource

---
+++ cltk/corpus/marathi/corpora.py
@@ -0,0 +1,8 @@
+"""Available Hebrew corpora available for download."""
+
+MARATHI_CORPORA = [
+    {'name': 'marathi_text_wikisource',
+     'origin': 'https://github.com/MaheshBhosale/marathi_text_wikisource',
+     'location': 'remote',
+     'type': 'text'},
+]
