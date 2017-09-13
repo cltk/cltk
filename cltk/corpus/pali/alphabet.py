eff47986263679c73aea9b3b61a24ab91a5e99da
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2017-09-13 16:30:02 -0700

    format docstring

--- cltk/corpus/pali/alphabet.py
+++ cltk/corpus/pali/alphabet.py
@@ -1,13 +1,11 @@
-"""
-Chandana K.G.D  GitHub- chandanakgd 
-"""
+"""The Pali alphabet."""
 
 __author__ = ['Chandana K.G.D <GitHub:chandanakgd>']
 __license__ = 'MIT License. See LICENSE.'
 
 
-INDEPENDENT_VOWELS=['අ','ආ','ඉ','ඊ','උ','එ','ඔ']
-
 CONSTANTS=['ක','ඛ','ග','ඝ','ඞ','ච','ඡ','ජ','ඣ','ඤ','ට','ඨ','ඩ','ඪ','ණ','ත','ථ','ද','ධ','න','ප','ඵ','බ','භ','ම','ය','ර','ල','ව','ස','හ','ළ','අං''']
 
 DEPENDENT_VOWELS=[' ා',' ි' ,' ී' ,' ු',' ූ',' ෙ',' ො']
+
+INDEPENDENT_VOWELS=['අ','ආ','ඉ','ඊ','උ','එ','ඔ']
