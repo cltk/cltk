eaee993d42b7f7679c7b95ada1cc98439ca870f7
Dilshan Abeysinghe <maradilshan@gmail.com>
2017-09-28 10:43:56 -0700

    Added corpora, beta_to_unicode, formatter, importer, numerifier, transliterate_mdc

---
+++ cltk/corpus/tamil/corpora.py
@@ -0,0 +1,13 @@
+"""Tamil corpora available for download."""
+
+__author__ = 'Dilshan Abeysinghe <maradilshan[at]gmail[.]com>'
+
+Tamil_CORPORA = [
+    {'name': 'tamil_text_perseus',
+     'markup':'xml',
+     'origin': 'https://github.com/KumaraDilshan/cltk/tree/Tamil_alphabet/cltk/corpus/tamil',
+     'location': 'remote',
+     'type': 'text',
+     'RomanizationType': 'Buckwalter',
+     }
+]
