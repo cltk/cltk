bb709521f0b2dab07b3fa795d547a1c92b6ca530
inishchith <inishchith@gmail.com>
2017-11-10 10:54:20 +0530

    add odia language

---
+++ cltk/corpus/odia/alphabet.py
@@ -0,0 +1,26 @@
+'''Odia is an Indian language, belonging to the Indo-Aryan branch of
+the Indo-European language family.It is mainly spoken in the Indian
+states of Odisha and in parts of West Bengal, Jharkhand, Chhattisgarh
+and Andhra Pradesh.Odia is one of the many official languages in India;
+it is the official language of Odisha and the second official language
+of Jharkhand.Odia is the predominant language of Odisha, where Odia speakers comprise
+around 83.33% of the population according to census surveys.
+'''
+VOWELS = [
+    'ଅ', 'ଆ', 'ଇ', 'ଈ', 'ଉ', 'ଊ', 'ଋ',
+    'ୠ', 'ଌ', 'ୡ', 'ଏ', 'ଐ', 'ଓ', 'ଔ']
+
+STRUCTUREDCONSONENTS = [
+    'କ', 'ଖ', 'ଗ', 'ଘ', 'ଙ',
+    'ଚ', 'ଛ', 'ଜ', 'ଝ', 'ଞ',
+    'ଟ', 'ଠ', 'ଡ', 'ଢ', 'ଣ',
+    'ତ', 'ଥ', 'ଦ', 'ଧ', 'ନ',
+    'ପ', 'ଫ', 'ବ', 'ଭ', 'ମ']
+
+UNSTRUCTUREDCONSONANTS = [
+    'ଯ', 'ୟ', 'ର', 'ଲ', 'ଳ', 'ୱ',
+    'ଶ', 'ଷ', 'ସ', 'ହ', 'କ୍ଷ']
+
+NUMERALS = [
+    '୦', '୧', '୨', '୩', '୪',
+    '୫', '୬', '୭', '୮', '୯']
