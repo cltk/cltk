750bff20fac49e1206de7d014385b671b5151170
clemsciences <clemsciences@aol.com>
2017-11-12 00:26:04 +0100

    Link to old_norse_models_cltk

--- cltk/corpus/old_norse/corpora.py
+++ cltk/corpus/old_norse/corpora.py
@@ -2,4 +2,8 @@ OLD_NORSE_CORPORA = [{"encoding": "utf-8",
                       "name": "old_norse_text_perseus",
                       "location": "remote",
                       "origin": "https://github.com/cltk/old_norse_text_perseus.git",
-                      "type": "text"}]
+                      "type": "text"},
+                     {'name': 'old_norse_models_cltk',
+                     'origin': 'https://github.com/cltk/old_norse_models_cltk.git',
+                     'location': 'remote',
+                     'type': 'model'},]
