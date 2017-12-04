580ca5cc9a8a86941c46f479cceaaa97261137ef
Dhruv Apte <dhruvgirishapte@gmail.com>
2017-12-04 18:42:08 +0530

    Create alphabet.py

---
+++ cltk/corpus/gujarati/alphabet.py
@@ -0,0 +1,19 @@
+""" GUJARATI is an INDO-ARYAN language native to the state of Gujarat. It is spoken in the state of Gujarat in addition to the
+union territories of Daman & Diu and Dadra & Nagar Haveli.
+
+The Gujarati language has 44 letters 11 VOWELS and 33 CONSONANTS.
+"""
+
+__author__ = ['Dhruv Apte']
+__license__ = 'MIT License.See LICENSE.'
+
+DIGITS = ['૧','૨','૩','૪','૫','૬','૭','૮','૯','૧૦']
+
+VOWELS = [ 'અ' , 'આ' , 'ઇ' , 'ઈ' , 'ઉ' , 'ઊ' , 'ઋ' , 'એ' , 'ઐ' , 'ઓ' , 'ઔ' ] 
+
+DEPENDENT_VOWELS = [ 'ા ' ,  'િ' ,  'ી' , 'ો' , 'ૌ' ]
+
+CONSONANTS = [ 'ક' , 'ખ' , 'ગ' , 'ઘ' , 'ચ' , 'છ' , 'જ' , 'ઝ' , 'ઞ' , 'ટ' , 'ઠ' , 'ડ' , 'ઢ' , 'ણ' , 'ત' ,		
+'થ' , 'દ' , 'ધ' , 'ન' , 'પ' , 'ફ' , 'બ' , 'ભ' , 'મ' , 'ય' , 'ર' , 'લ' , 'ળ' , 'વ' , 'શ' , 'ષ' , 'સ' , 'હ' ]		
+
+MODIFIERS = [ ' ्' , ' ॓' , ' ॔' ]
