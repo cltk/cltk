87a0f45efd57bd8a3353b422a30d06256d26992b
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2018-01-17 09:03:35 -0800

    Update alphabet.py

--- cltk/corpus/gujarati/alphabet.py
+++ cltk/corpus/gujarati/alphabet.py
@@ -1,19 +1,18 @@
-"""
-The Gujarati language has 46 letters 13 VOWELS and 33 CONSONANTS.
+"""The Gujarati language has 46 letters 13 VOWELS and 33 CONSONANTS.
 """
 
 __author__ = ['Dhruv Apte']
-__license__ = 'MIT License.See LICENSE.'
+__license__ = 'MIT License. See LICENSE.'
 
-DIGITS = ['૦','૧','૨','૩','૪','૫','૬','૭','૮','૯','૧૦']
+DIGITS = ['૦', '૧', '૨', '૩', '૪', '૫', '૬', '૭', '૮', '૯', '૧૦']
 
-VOWELS = [ 'અ' , 'આ' , 'ઇ' , 'ઈ' , 'ઉ' , 'ઊ' , 'ઋ' , 'એ' , 'ઐ' , 'ઓ' , 'ઔ' , 'અં' , 'અઃ'  ] 
+VOWELS = [ 'અ' , 'આ' , 'ઇ' , 'ઈ' , 'ઉ' , 'ઊ' , 'ઋ' , 'એ' , 'ઐ' , 'ઓ' , 'ઔ' , 'અં' , 'અઃ'  ]
 
 DEPENDENT_VOWELS = [ 'ા ' ,  'િ' ,  'ી' , 'ો' , 'ૌ' ]
 
 CONSONANTS = [ 'ક' , 'ખ' , 'ગ' , 'ઘ' , 'ચ' , 'છ' , 'જ' , 'ઝ' , 'ઞ' , 'ટ' , 'ઠ' , 'ડ' , 'ઢ' , 'ણ' ,
               'ત' ,	'થ' , 'દ' , 'ધ' , 'ન' , 'પ' , 'ફ' , 'બ' , 'ભ' , 'મ' , 'ય' , 'ર' , 'લ' , 'ળ' ,
-              'વ' , 'શ' , 'ષ' , 'સ' , 'હ' ]	
+              'વ' , 'શ' , 'ષ' , 'સ' , 'હ' ]
 
 VELAR_CONSONANTS = [ 'ક' , 'ખ' , 'ગ' , 'ઘ' , 'ઙ' ]
 
