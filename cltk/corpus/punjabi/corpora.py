afeb0f786a833379e7c6a07874ffa58c7f6a3f78
vikrant97 <vikrantgoyal97@gmail.com>
2018-02-15 20:45:21 +0530

    Added corpora for classical punjabi writings by Bhagat Kabir

--- cltk/corpus/punjabi/corpora.py
+++ cltk/corpus/punjabi/corpora.py
@@ -3,5 +3,11 @@ PUNJABI_CORPORA = [
      'origin': 'https://github.com/cltk/punjabi_text_gurban.git',
      'location': 'remote',
      'type': 'text'
+    },
+
+    {'name': 'punjabi_text_wikisource',
+     'origin': 'https://github.com/vikrant97/punjabi_text_wikisource.git',
+     'location': 'remote',
+     'type': 'text'
     }
 ]
