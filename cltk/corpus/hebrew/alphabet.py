161501e19483d1b2e2782a2f17db49e894515acb
osho-agyeya <oshoagyeya123@gmail.com>
2018-02-16 02:22:02 +0530

    Added hebrew alphabets at alphabet.py

---
+++ cltk/corpus/hebrew/alphabet.py
@@ -0,0 +1,20 @@
+
+"""
+Reference: 	https://en.wikipedia.org/wiki/Hebrew_numerals
+			https://www.omniglot.com/writing/hebrew.htm
+			https://www.duolingo.com/comment/7253301/Hebrew-Time-8-The-Hebrew-Vowels
+
+
+
+The Hebrew alphabet (Hebrew: אָלֶף־בֵּית עִבְרִי‬,[a] Alefbet Ivri), known variously by scholars as the Jewish script, 
+square script and block script, is an abjad script used in the writing of the Hebrew language, 
+also adapted as an alphabet script in the writing of other Jewish languages, 
+most notably in Yiddish (lit. "Jewish" for Judeo-German), Djudío (lit. "Jewish" for Judeo-Spanish), and Judeo-Arabic."""
+
+#DIGITS = 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000,2000,5000,10000,100000,1000000
+
+DIGITS = ['א','ב','ג','ד','ה','ו','ז','ח','ט','י','יא‬','יב','יג','יד‬','ט״ו‬','ט״ז‬','יז‬','יח‬','יט‬','כ','ל','מ','נ','ס','ע','פ','צ','ק','ר', 'ש', 'ת','ך‬','ם‬','ן‬','ף‬','ץ‬','א׳‬','ב׳‬','ה׳‬','א׳י‬	','א׳ק‬','א׳א׳‬	']
+
+ALPHABET = ['ז', 'ו', 'ה', 'ד', 'ג', 'בּ/ב', 'א', 'מ', 'ל', 'ך', 'כּ/כ', 'י', 'ט', 'ח', 'ף', 'פּ/פ', 'ע', 'ס', 'ן', 'נ', 'ם', 'ת', 'שׁ/שׂ', 'ר', 'ק', 'ץ', 'צ', "'ת", "'צ", "'ע", "'ח", "'ז", "'ד", "'ג"] 
+
+VOWELS = ['אְ', 'אִ', 'אֵ', 'אֶ', 'אָ', 'אַ', 'ׂא', 'וֹ', 'אֻ', 'וּ', 'תּ', 'שׁ', 'שׂ', 'אֱ', 'אֲ', "אֳ"]
