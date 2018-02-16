283e97038e0c4cbe34b528716de21a3d256c8370
osho-agyeya <oshoagyeya123@gmail.com>
2018-02-16 16:17:25 +0530

    Added Arabic to swadeshi list

--- cltk/tests/test_corpus.py
+++ cltk/tests/test_corpus.py
@@ -762,5 +762,11 @@ class TestScriptInformation(unittest.TestCase):
         match = swadesh.words()[0]
         self.assertEqual(first_word, match)
 
+    def test_swadesh_arabic(self):
+        swadesh = Swadesh('ar')
+        first_word = 'أنا'
+        match = swadesh.words()[0]
+        self.assertEqual(first_word, match)    
+
 if __name__ == '__main__':
     unittest.main()
