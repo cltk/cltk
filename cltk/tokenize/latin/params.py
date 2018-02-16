3de2d25d6f0a7e4801eca371dda68a4e3458c252
Patrick J. Burns <patrick@diyclassics.org>
2018-02-16 15:59:39 -0500

    Add Latin params file

---
+++ cltk/tokenize/latin/params.py
@@ -0,0 +1,18 @@
+""" Params: Latin
+"""
+
+__author__ = ['Patrick J. Burns <patrick@diyclassics.org>']
+__license__ = 'MIT License.'
+
+PRAENOMINA = ['a', 'agr', 'ap', 'c', 'cn', 'd', 'f', 'k', 'l', "m'", 'm', 'mam', 'n', 'oct', 'opet', 'p', 'post', 'pro', 'q', 's', 'ser', 'sert', 'sex', 'st', 't', 'ti', 'v', 'vol', 'vop', 'a', 'ap', 'c', 'cn', 'd', 'f', 'k', 'l', 'm', "m'", 'mam', 'n', 'oct', 'opet', 'p', 'paul', 'post', 'pro', 'q', 'ser', 'sert', 'sex', 'sp', 'st', 'sta', 't', 'ti', 'v', 'vol', 'vop']
+
+CALENDAR = ['ian', 'febr', 'mart', 'apr', 'mai', 'iun', 'iul', 'aug', 'sept', 'oct', 'nov', 'dec'] \
+            + ['kal', 'non', 'id', 'a.d']
+
+MISC = ['coll', 'cos', 'ord', 'pl.', 's.c', 'suff', 'trib']
+
+ABBREVIATIONS = set(
+                   PRAENOMINA +
+                   CALENDAR +
+                   MISC
+                   )
