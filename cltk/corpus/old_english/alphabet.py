08c9664d9f9a13f5371d1d313ced39b22d211d31
TheFlash10 <harshkotta@gmail.com>
2018-02-21 18:04:02 +0530

    Added Digits to the Old English corpus

---
+++ cltk/corpus/old_english/alphabet.py
@@ -0,0 +1,8 @@
+"""
+Old English is the language of the Anglo-Saxons (up to about 1150), an 
+inflected language with a Germanic vocabulary, very different from modern English.
+"""
+
+# Digits [1-10]
+DIGITS = ["Ān","Tƿeġen","Þrēo","fēoƿer","fīf","seox","seofon","eahta","nigon","tīen"]
+
