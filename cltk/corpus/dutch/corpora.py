36fa599e8b7b0a1679a2404efbde99a83e79d585
Shubham Shukla <shubhamshukla1197@gmail.com>
2018-02-21 19:10:58 +0530

    Update corpora.py

--- cltk/corpus/dutch/corpora.py
+++ cltk/corpus/dutch/corpora.py
@@ -1,5 +1,5 @@
 """Dutch corpora available for download: """
-DUTCH_CORPORA = [
+MIDDLE_DUTCH_CORPORA = [
 			   {'name': 'middle_dutch_text',
 			    'origin': 'https://github.com/cltk/middle_dutch_text.git',
 			    'location': 'remote',
