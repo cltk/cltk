6c44ae736933db74dca36259ecca59510be7f71d
TheFlash10 <harshkotta@gmail.com>
2018-02-22 08:08:43 +0530

    Add files via upload

--- cltk/corpus/old_english/alphabet.py
+++ cltk/corpus/old_english/alphabet.py
@@ -1,8 +1,17 @@
+
 """
-Old English is the language of the Anglo-Saxons (up to about 1150), an 
-inflected language with a Germanic vocabulary, very different from modern English.
+Old English (Ænglisc, Anglisc, Englisc), or Anglo-Saxon,
+is the earliest historical form of the English language, 
+spoken in England and southern and eastern Scotland in the early Middle Ages.
 """
 
-# Digits [1-10]
-DIGITS = ["Ān", "Tƿeġen", "Þrēo", "fēoƿer", "fīf", "seox", "seofon", "eahta", "nigon", "tīen"]
+# digits [1-10]
+DIGITS = ['ān','tƿeġen','þrēo','fēoƿer','fīf','seox','seofon','eahta','niġon','tīen']
 
+ALPHABET = ['a','æ','b','c','d','ð','e','f','g','h','i','l','m','n','o','p','r','s','t','u','w','ƿ','x','y','þ']
+
+CONSONANTS = ['b','c','cȝ','cg','d','ð','f','ff','ȝ','g','h','l','n','p','r','s','ss','sc','t','þ','þþ','ƿ','w']
+
+VOWELS = ['a','æ','e','i','o','u','y']
+
+DIPTHONGS = ['ea','eo','ie']
