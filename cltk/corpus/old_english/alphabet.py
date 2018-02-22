3b5f38d5fd90b91efd6f9a2e94ba418f2c315edd
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2018-02-21 19:08:08 -0800

    Update alphabet.py

--- cltk/corpus/old_english/alphabet.py
+++ cltk/corpus/old_english/alphabet.py
@@ -1,17 +1,13 @@
 
-"""
-Old English (Ænglisc, Anglisc, Englisc), or Anglo-Saxon,
-is the earliest historical form of the English language, 
-spoken in England and southern and eastern Scotland in the early Middle Ages.
-"""
+"""Old English alphabet."""
 
 # digits [1-10]
-DIGITS = ['ān','tƿeġen','þrēo','fēoƿer','fīf','seox','seofon','eahta','niġon','tīen']
+DIGITS = ['ān', 'tƿeġen', 'þrēo', 'fēoƿer', 'fīf', 'seox', 'seofon', 'eahta', 'niġon', 'tīen']
 
-ALPHABET = ['a','æ','b','c','d','ð','e','f','g','h','i','l','m','n','o','p','r','s','t','u','w','ƿ','x','y','þ']
+ALPHABET = ['a', 'æ', 'b', 'c', 'd', 'ð', 'e', 'f', 'g', 'h', 'i', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'ƿ', 'x', 'y', 'þ']
 
-CONSONANTS = ['b','c','cȝ','cg','d','ð','f','ff','ȝ','g','h','l','n','p','r','s','ss','sc','t','þ','þþ','ƿ','w']
+CONSONANTS = ['b', 'c', 'cȝ', 'cg', 'd', 'ð', 'f', 'ff', 'ȝ', 'g', 'h', 'l', 'n', 'p', 'r', 's', 'ss', 'sc', 't', 'þ', 'þþ', 'ƿ', 'w']
 
-VOWELS = ['a','æ','e','i','o','u','y']
+VOWELS = ['a', 'æ', 'e', 'i', 'o', 'u', 'y']
 
-DIPTHONGS = ['ea','eo','ie']
+DIPTHONGS = ['ea', 'eo', 'ie']
