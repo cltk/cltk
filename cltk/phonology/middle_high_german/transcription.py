69538024308a766409cc01c6875e08fff70e5d18
Chatziargyriou Eleftheria <36453738+Sedictious@users.noreply.github.com>
2018-03-02 15:49:45 +0200

    Added Phonetic Indexing for MHG

--- cltk/phonology/middle_high_german/transcription.py
+++ cltk/phonology/middle_high_german/transcription.py
@@ -1,4 +1,4 @@
-"""Note: there are no definie MHG  phonological rules, so this module serves as an approximate reconstruction of the original. As of this version, the Transcribe class doesn't support any specific dialects and serves as a superset encompassing various regional accents. 
+"""Note: there are no definite MHG  phonological rules, so this module serves as an approximate reconstruction of the original. As of this version, the Transcribe class doesn't support any specific dialects and serves as a superset encompassing various regional accents. 
  
  To-do: add stress, syllabify module, CV tier
  
@@ -8,8 +8,9 @@
 """
 
 import re
+from cltk.stem.middle_high_german.stem import remove_umlaut
 
-
+#IPA Dictionary
 Dipthongs_IPA = {
 	"ei": "ɛ͡ɪ", #Dipthongs
 	"ie": "i͡ə",
@@ -70,6 +71,38 @@ IPA = {
 	"z": "t͡s",
 	"ȥ": "t͡s"
 }
+
+#Soundex Dictionary
+dict_dipth_SE = {
+	"ng":"2",
+	"ch":"2",
+	"pf":"4",
+	"ts":"4",
+}
+
+dict_SE = {
+	"f":"1",
+	"b":"1",
+	"p":"1",
+	"v":"1",
+	"w":"1",
+	"m":"2",
+	"n":"2",
+	"t":"3",
+	"d":"3",
+	"r":"3",
+	"l":"3",
+	"k":"3",
+	"c":"3",
+	"g":"3",
+	"s":"3",
+	"z":"4",
+	"ȥ":"4",
+	"s":"4",
+	"r":"5",
+	"l":"5",
+	"j":"6"
+}
 	
 class Transcriber:
 	
@@ -96,3 +129,56 @@ class Transcriber:
 			
 		return "[" + text + "]"
 
+"""
+Soundex variant was based on the original American Soundex  developed by Russel and King, altered to better fit Middle High 
+German morphology. The replacement rules were based on matching places and manners of articulation between the two languages 
+(AE and MHG).
+
+Algorithm:
+
+-N ormalize word and convert the first letter to uppercase
+-Remove other vowels 
+
+Replacement Rules:
+- f,v,b,p,w -> 1 Labiodental fricatives [f,v] and bilabial plosives [p,b], approximant [w]
+- m,n,ng -> 2 Nasals
+- t,d,r,l,k,c,g,ch,s -> 3 [non-nasal velars/alveolars]
+- pf, ts, z, s -> 4  Affricates and alveolar fricatives
+- r,l -> 5 Liquids
+- j -> 6 Palatal Approximant
+
+-Remove double numbers
+-Remove remaining letters
+-Retain first 3 numbers (add 0 if less than 3)
+"""
+
+class Word():
+
+	def __init__(self,word):
+		self.word = word
+	
+	def phonetic_indexing(self,p = "SE"):
+		"""Specifies the phonetic indexing method.
+		SE: Soundex variant for MHG"""
+		
+		if p == "SE":
+			return self._Soundex()
+		else:
+			print("Parameter value not supported")
+		
+	def _Soundex(self):
+		t_word = remove_umlaut(self.word[0].lower()).upper() + self.word[1:].lower()
+		
+		for w,val in zip(dict_dipth_SE.keys(), dict_dipth_SE.values()):
+			t_word = t_word.replace(w,val)
+		
+		for w,val in zip(dict_SE.keys(), dict_SE.values()):
+			t_word = t_word.replace(w,val)
+			
+		#Remove adjacent duplicate numbers
+		t_word = re.sub(r"(\d)\1+",r"\1" ,t_word)
+		
+		#Strip remaining letters
+		t_word = re.sub(r"[a-z]+","",t_word)
+		
+		return (t_word + "0"*3) [:4] #Add trailing zeroes
