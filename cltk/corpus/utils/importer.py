580080f6aa365a1dc97bf4124485ef78e68b08b4
rajeevnagarwal <rajeev14084@iiitd.ac.in>
2018-03-06 13:54:05 +0530

    Added Orpha Dataset

--- cltk/corpus/utils/importer.py
+++ cltk/corpus/utils/importer.py
@@ -26,6 +26,7 @@ from cltk.corpus.classical_hindi.corpora import CLASSICAL_HINDI_CORPORA
 from cltk.corpus.french.corpora import FRENCH_CORPORA
 from cltk.corpus.marathi.corpora import MARATHI_CORPORA
 from cltk.corpus.gujarati.corpora import GUJARATI_CORPORA
+from cltk.corpus.orphadata.corpora import ORPHA_DATASET
 
 
 from cltk.utils.cltk_logger import logger
@@ -48,7 +49,7 @@ AVAILABLE_LANGUAGES = ['arabic', 'chinese', 'coptic', 'greek', 'hebrew', 'latin'
                        'pali', 'punjabi', 'tibetan', 'sanskrit', 'old_english',
                        'bengali', 'prakrit', 'hindi', 'old_church_slavonic',
                        'malayalam', 'marathi', 'javanese','old_norse','telugu','classical_hindi',
-                       'french', 'gujarati', 'middle_high_german']
+                       'french', 'gujarati', 'middle_high_german','orpha']
 
 
 CLTK_DATA_DIR = '~/cltk_data'
@@ -76,6 +77,7 @@ LANGUAGE_CORPORA = {'arabic': ARABIC_CORPORA,
                     'classical_hindi':CLASSICAL_HINDI_CORPORA,
                     'french':FRENCH_CORPORA,
                     'gujarati': GUJARATI_CORPORA,
+					'orpha':ORPHA_DATASET
 
                     }
 
