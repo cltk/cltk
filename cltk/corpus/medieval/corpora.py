b3e8197b28a5c7ddfe579283680d00523142ad8a
ASHWANI KUMAR <ashwanidv100@gmail.com>
2018-03-12 09:51:27 +0530

    Add files via upload

---
+++ cltk/corpus/medieval/corpora.py
@@ -0,0 +1,13 @@
+"""Medieval language corpora available for download or loading locally.
+All remote corpora hosted by github on the cltk organization account, eg:
+'http://github.com/cltk' + name
+"""
+
+MEDIEVAL_CORPORA = [
+    {'name': 'medieval_text_wikisource',
+     'origin': 'https://github.com/cltk/medieval_text_wikisource.git',
+     'location': 'remote',
+     'type': 'text'},
+    
+]
+
