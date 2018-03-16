ddb1f800212cf0ed84a23e1cb175e5c3f19be4ab
nada <nada.abdelbaky1996@gmail.com>
2018-03-16 08:23:17 +0200

    adding medieval

---
+++ cltk/corpus/medieval/corpora.py
@@ -0,0 +1,9 @@
+MEDIEVAL_CORPORA=[
+	{'name': 'medieval_text_wikisource',
+     'origin': 'https://github.com/cltk/medieval_text_wikisource.git',
+     'location': 'remote',
+     'type': 'text'
+ 	 },
+
+
+]
