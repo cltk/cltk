1fbe34cde1faafc9209898ac1545536b6ee63e2e
ASHWANI KUMAR <ashwanidv100@gmail.com>
2018-03-17 17:27:37 +0530

    Update corpora.py

--- cltk/corpus/medieval/corpora.py
+++ cltk/corpus/medieval/corpora.py
@@ -2,6 +2,8 @@
 All remote corpora hosted by github on the cltk organization account, eg:
 'http://github.com/cltk' + name
 """
+__author__ = ['Kyle P. Johnson <kyle@kyle-p-johnson.com>', 'Stephen Margheim <stephen.margheim@gmail.com>']
+__license__ = 'MIT License. See LICENSE.'
 
 MEDIEVAL_CORPORA = [
     {'name': 'medieval_text_wikisource',
