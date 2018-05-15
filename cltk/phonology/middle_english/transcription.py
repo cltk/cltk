2e2022973acf8ba8d582720b1e7fb9346bbc0c2c
Chatziargyriou Eleftheria <36453738+Sedictious@users.noreply.github.com>
2018-05-15 17:42:58 +0300

    Added Soundex

--- cltk/phonology/middle_english/transcription.py
+++ cltk/phonology/middle_english/transcription.py
@@ -1,7 +1,7 @@
-
 __author__ = ['Chatziargyriou Eleftheria <ele.hatzy@gmail.com>']
 __license__ = 'MIT License'
 
+import re
 from cltk.stem.middle_english.stem import affix_stemmer
 
 """
@@ -22,6 +22,27 @@ TRIPHTHONGS = ['ght', 'ghl']
 
 CONSONANTS = ['b', 'c', 'd', 'f', 'g', 'h', 'l', 'm', 'n', 'p', 'r', 's', 't', 'x', 'ð', 'þ', 'ƿ']
 
+#Soundex replacement rules
+dict_SE = {
+    'p': '1',
+    'b': '1',
+    'f': '1',
+    'v': '1',
+    'gh':'1',
+    'sh':'2',
+    't': '2',
+    'd': '2',
+    's': '2',
+    'z': '2',
+    'r': '2',
+    'k': '2',
+    'g': '2',
+    'w': '2',
+    'l': '3',
+    'm': '4',
+    'n': '4',
+    'r': '5'
+}
 
 class Word:
 
@@ -46,7 +67,7 @@ class Word:
 
          Returns:
              list: string list containing the syllables of the given word
-        
+
         Examples:
             >>> Word('heldis').syllabify()
             ['hel', 'dis']
@@ -107,7 +128,7 @@ class Word:
 
             i += 1
 
-        #Check whether the last syllable should be merged with the previous one
+        # Check whether the last syllable should be merged with the previous one
         try:
             if ind[-1] in [len(self.word) - 2, len(self.word) - 1]:
                 ind = ind[:-(1 + (ind[-2] == len(self.word) - 2))]
@@ -135,22 +156,22 @@ class Word:
         """
         Returns:
              str: Syllabified word in string format
-        
+
         Examples:
             >>> Word('conseil').syllabified_str()
             'con.seil'
-            
+
             You can also specify the separator('.' by default)
-            
+
             >>> Word('sikerly').syllabified_str(separator = '-')
             'sik-er-ly'
         """
         return separator.join(self.syllabified if self.syllabified else self.syllabify())
 
-    def stresser(self, stress_rule = 'FSR'):
+    def stresser(self, stress_rule='FSR'):
         """
         Args:
-        
+
             :param stress_rule: Stress Rule, valid options:
 
                 'FSR': French Stress Rule, stress falls on the ultima, unless
@@ -161,12 +182,12 @@ class Word:
                 of the stemm. Note that the accuracy of the function directly
                 depends on that of the stemmer.
 
-                'LSR': Latin Stress Rule, stress falls on the penult if its 
-                heavy, else, if it has more than two syllables on the 
+                'LSR': Latin Stress Rule, stress falls on the penult if its
+                heavy, else, if it has more than two syllables on the
                 antepenult, else on the ultima.
 
         Returns:
-        
+
             list: A list containing the separate syllable, where the stressed
             syllable is prefixed by ' . Monosyllabic words are left unchanged,
             since stress indicates relative emphasis.
@@ -192,16 +213,16 @@ class Word:
             ['ver', "'bum"]
         """
 
-        #Syllabify word
+        # Syllabify word
         if not self.syllabified:
             self.syllabify()
 
-        #Check whether word is monosyllabic
+        # Check whether word is monosyllabic
         if len(self.syllabified) == 1:
             return self.syllabified
 
         if stress_rule == 'FSR':
-            #Check whether ultima ends in e
+            # Check whether ultima ends in e
             if self.syllabified[-1][-1] == 'e':
                 return self.syllabified[:-2] + ['\'{0}'.format(self.syllabified[-2])] + self.syllabified[-1:]
 
@@ -209,15 +230,15 @@ class Word:
                 return self.syllabified[:-1] + ['\'{0}'.format(self.syllabified[-1])]
 
         elif stress_rule == 'GSR':
-            #The word striped of suffixes
-            st_word = affix_stemmer([self.word], strip_suf = False)
+            # The word striped of suffixes
+            st_word = affix_stemmer([self.word], strip_suf=False)
             affix = self.word[:len(self.word) - len(st_word)]
 
-            #Syllabify stripped word and affix
+            # Syllabify stripped word and affix
 
             syl_word = Word(st_word).syllabify()
 
-            #Add stress
+            # Add stress
             syl_word = ['\'{0}'.format(syl_word[0])] + syl_word[1:]
 
             if affix:
@@ -227,7 +248,7 @@ class Word:
             return syl_word
 
         elif stress_rule == 'LSR':
-            #Check whether penult is heavy (contains more than one mora)
+            # Check whether penult is heavy (contains more than one mora)
             if sum(map(lambda x: x in SHORT_VOWELS, self.syllabified[-1])) > 1:
                 return self.syllabified[:-2] + ['\'{0}'.format(self.syllabified[-2])] + self.syllabified[-1:]
 
@@ -236,5 +257,81 @@ class Word:
 
             else:
                 return self.syllabified[:-1] + ['\'{0}'.format(self.syllabified[-1])]
-            
- 
+
+    def phonetic_indexing(self, p = "SE"):
+        """
+        Args:
+
+            :param p: Specifies the phonetic indexing method
+                SE: Soundex variant for MHG
+
+        Returns:
+            str: Encoded string corresponding to the word's phonetic
+            representation
+
+        """
+
+        if p == "SE":
+            return self._Soundex()
+
+    def _Soundex(self):
+        """
+        The Soundex phonetic indexing algorithm adapted to ME phonology.
+
+        Algorithm:
+
+        Let w the original word and W the resulting one
+
+        1) Capitalize the first letter of w and append it to W
+
+        2) Apply the following replacement rules
+
+            p, b, f, v, gh (non-nasal fricatives) -> 1
+
+            t, d, s, sh, z, r, k, g, w (non-nasal alveolars and velars) -> 2
+
+            l (alveolar lateral) -> 3
+
+            m, n (nasals) -> 4
+
+            r (alveolar approximant) -> 5
+
+        3) Concetate multiple occurrences of numbers into one
+
+        4) Remove non-numerical characters
+
+        Notes:
+            /h/ was thought to be either a voiceless or velar fricative
+        when occurring in the coda with its most used grapheme being <gh>.
+        Those phonemes either disappeared, resulting in the lengthening
+        of preceding vowel clusters, or were developed into /f/ as evident
+        by modern spelling (e.g. 'enough': /ɪˈnʌf/ and 'though': /ðəʊ/)
+
+        Examples:
+            >>> Word("midel").phonetic_indexing(p="SE")
+            'M230'
+
+            >>> Word("myddle").phonetic_indexing(p="SE")
+            'M230'
+
+            >>> Word("might").phonetic_indexing(p="SE")
+            'M120'
+
+            >>> Word("myghtely").phonetic_indexing(p="SE")
+            'M123'
+        """
+        
+        word = self.word[1:]
+
+        for w, val in zip(dict_SE.keys(), dict_SE.values()):
+            word = word.replace(w, val)
+
+        #Remove multiple adjacent occurences of digit
+        word = re.sub(r"(\d)\1+", r"\1", word)
+
+        # Strip remaining letters
+        word = re.sub(r"[a-zðþƿ]+", "", word)
+
+        # Add trailing zeroes and return
+        return (self.word[0].upper() + word + "0" * 3)[:4]
+    
