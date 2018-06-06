31b74dd3c18a51e068bf57c21f94a2aeeb5fc1e4
Iman Nazari <32216131+ishto7@users.noreply.github.com>
2018-06-06 22:11:27 +0430

    Update Persian alphabet
    Just starting...

--- cltk/corpus/persian/alphabet.py
+++ cltk/corpus/persian/alphabet.py
@@ -1,22 +1,44 @@
 """Persian alphabet"""
 
+__author__ = "Iman Nazari"
 
-#Persian digits from 0 to 9
-DIGITS = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
+# HAMZEH Family
+ALEF_WITH_HAMZEH_ABOVE = '\u0623'
+ALEF_WITH_HAMZEH_BELOW = '\u0625'
 
-#Diacritics
-SHORT_VOWELS = ['َ', 'ِ', 'ُ', 'ْ']
+# Extensions
+ALEF_WITH_MAD = '\u0622'
 
-#The Persian Alphabet
-LONG_VOWELS = ['ا', 'و', 'ی']
-
-CONSONANTS = ['ء', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'ه']
-
-#Special Characters
-SPECIAL = ['آ', 'ۀ']
-
-#Tanvins
-TANVIN = [ 'ٌ' , 'ٍ' , 'ً']
-
-#Tashdid
-TASHDID = 'ّ'
+# Alphabet
+ALEF = '\u0627'
+BE = '\u0628'
+PE = '\u067e'
+TE = '\u062a'
+SE = '\u062b'
+JIM = '\u062c'
+CHE = '\u0686'
+HE = '\u062d'
+KHE = '\u062e'
+DAL = '\u062f'
+ZAL = '\u0630'
+RE = '\u0631'
+ZE = '\u0632'
+ZHE = '\u0698'
+SIN = '\u0633'
+SHIN = '\u0634'
+SAD = '\u0635'
+ZAD = '\u0636'
+TA = '\u0637'
+ZA = '\u0638'
+EYN = '\u0639'
+GHEYN = '\u063a'
+FE = '\u0641'
+GHAF = '\u0642'
+KAF = '\u06a9'
+GAF = '\u0642'
+LAM = '\u0644'
+MIM = '\u0645'
+NOON = '\u0646'
+VAV = '\u0648'
+HE2 = '\u0647'
+YE = '\u06cc'
