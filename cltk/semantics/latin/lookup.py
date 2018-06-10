d464dfb17ad3bef925fb4ba7f4869a988173d40c
Travis CI <travis@travis-ci.org>
2018-06-09 18:17:54 -0700

    Clean up lookup.py
    I used the `pylint` library to help streamline your classes. A few things
    to point out: 1) If a class inherts from another, I believe it ought to call `super().__init__()` in order to reuse variables and functions. Not sure of the best way for you to do that here, however what I have done works. 2) I removed `load_replacement_patterns()` from `Synonyms`, because that in `Lemmata` is identical.

--- cltk/semantics/latin/lookup.py
+++ cltk/semantics/latin/lookup.py
@@ -1,46 +1,70 @@
-from cltk.utils.cltk_logger import logger
+"""Module for discovering same-language synonyms and Latin-to-Greek
+translations of individual words.
+
+@James improve this description. Give important info about the data source
+"""
+
 import importlib.machinery
 import os
+import types
+
+from cltk.utils.cltk_logger import logger
+
 
 class Lemmata:
-    """Parent class for all semantic 'lemmatizers'. Initialize with the dictionary type (lemmata, synonym, translation) and the source language)."""
+    """Parent class for all semantic 'lemmatizers'. Initialize with the
+    dictionary type (lemmata, synonym, translation) and the source language).
+    """
+
     def __init__(self, dictionary, language):
         self.dictionary = dictionary
         self.language = language
-        self.load_replacement_patterns()
+        self.lemmata = self.load_replacement_patterns()
+
     def load_replacement_patterns(self):
         """Check for availability of the specified dictionary."""
         filename = self.dictionary + '.py'
         models = self.language + '_models_cltk'
-        rel_path = os.path.join('~','cltk_data', self.language,'model', models,'semantics',filename)
+        rel_path = os.path.join('~/cltk_data',
+                                self.language,
+                                'model',
+                                models,
+                                'semantics',
+                                filename)
         path = os.path.expanduser(rel_path)
-        #logger.info('Loading lemmata. This may take a minute.')
+        logger.info('Loading lemmata or synonyms. This may take a minute.')
         loader = importlib.machinery.SourceFileLoader(filename, path)
-        module = loader.load_module()
-        self.lemmata = module.DICTIONARY    
+        module = types.ModuleType(loader.name)
+        loader.exec_module(module)
+        return module.DICTIONARY
+
     def lookup(self, tokens):
         """Return a list of possible lemmata and their probabilities for each token"""
         lemmatized_tokens = []
         for token in tokens:
             # look for token in lemma dict keys
             if token.lower() in self.lemmata.keys():
-                #lemmas is a list of possible lemmata. Probability values must be assigned.
-                #lemmalist is a list of the form [(LEMMA, PROBABILITY), (LEMMA, PROBABILITY)]
-                #lemmaobj is a tuple with the form (LEMMA, LIST)
+                # `lemmas` is a list of possible lemmata. Probability values must be assigned.
+                # `lemmalist` is a list of the form [(LEMMA, PROBABILITY), (LEMMA, PROBABILITY)]
+                # `lemmaobj` is a tuple with the form (LEMMA, LIST)
                 lemmas = self.lemmata[token.lower()]
                 lemmalist = []
                 for lemma in lemmas:
                     lemmalist.append((lemma, 1/len(lemmas)))
-                lemmaobj = (token, lemmalist)                
+                lemmaobj = (token, lemmalist)
             else:
             # if token not found in lemma-headword list, return the token itself
                 lemmalist = []
                 lemmalist.append((token, 1))
                 lemmaobj = (token, lemmalist)
-            lemmatized_tokens.append(lemmaobj)                        
+            lemmatized_tokens.append(lemmaobj)
         return lemmatized_tokens
-    def isolate(self, obj):
-        """Feed a standard semantic object in and receive a simple list of lemmata"""
+
+    @staticmethod
+    def isolate(obj):
+        """Feed a standard semantic object in and receive a simple list of
+        lemmata
+        """
         answers = []
         for token in obj:
             lemmata = token[1]
@@ -48,23 +72,19 @@ class Lemmata:
                 answers.append(pair[0])
         return answers
 
+
 class Synonyms(Lemmata):
-    def __init__(self, dictionary, language):
-        self.dictionary = dictionary
-        self.language = language
-        self.load_replacement_patterns()
-    def load_replacement_patterns(self):
-        """Check for availability of the specified dictionary."""
-        filename = self.dictionary + '.py'
-        models = self.language + '_models_cltk'
-        rel_path = os.path.join('~','cltk_data', self.language,'model', models,'semantics',filename)
-        path = os.path.expanduser(rel_path)
-        #logger.info('Loading synonyms. This may take a minute.')
-        loader = importlib.machinery.SourceFileLoader(filename, path)
-        module = loader.load_module()
-        self.synonyms = module.DICTIONARY
-    def lookup(self, lems):
-        """Requires a list of lemmata, not tokens. Feed tokens through a Lemmata object's lookup() and isolate() methods first."""
+    """@James put a description in here."""
+
+    def __init__(self, *args, **kwargs):
+        """Setup class."""
+        super().__init__(*args, **kwargs)
+        self.synonyms = self.load_replacement_patterns()
+
+    def lookup_synonyms(self, lems):
+        """Requires a list of lemmata, not tokens. Feed tokens through a
+        Lemmata object's lookup() and isolate() methods first.
+        """
         final_synonyms = []
         for lemma in lems:
             if lemma.lower() in self.synonyms.keys():
@@ -77,44 +97,3 @@ class Synonyms(Lemmata):
             else:
                 pass
         return final_synonyms
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
