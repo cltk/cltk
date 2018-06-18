6391ccb4d8dda75055a1381161855ef9e8fbb569
andrew.deloucas <andrewdeloucas@gmail.com>
2018-06-18 15:23:05 -0700

    added corpus/akkadian/corpora -- cdli_corpus

---
+++ cltk/corpus/akkadian/corpora.py
@@ -0,0 +1,11 @@
+"""Akkadian corpora available for download."""
+
+__author__ = ['Andrew Deloucas <ADeloucas@g.harvard.com>']
+
+AKKADIAN_CORPORA = [
+
+ {'name': 'cdli_corpus',
+ 'origin': 'https://github.com/cdli-gh/data',
+ 'location': 'remote',
+ 'type': 'atf'}
+]
