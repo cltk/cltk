c00a2ce6ab5f9b97394503809f6eae0af453fc74
TheFlash10 <harshkotta@gmail.com>
2018-06-29 12:31:35 +0530

    Update alphabet.py

--- cltk/corpus/pali/alphabet.py
+++ cltk/corpus/pali/alphabet.py
@@ -1,10 +1,21 @@
 """The Pali alphabet"""
 
-ALPHABET = ['a','ā','i','ī','u','ū','e','o','ṃ','k','kh','g','gh','ṅ','c','ch','j','jh','ñ','ṭ','ṭh','ḍ','ḍh','ṇ','t','th','d','dh','n','p','ph','b','bh','m','y','r','l','ḷ','v','s','h']
+__author__ = ['Chandana K.G.D <GitHub:chandanakgd>']
+__license__ = 'MIT License. See LICENSE.'
 
-CONSONANTS = ['k','kh','g','gh','ṅ','c','ch','j','jh','ñ','ṭ','ṭh','ḍ','ḍh','ṇ','t','th','d','dh','n','p','ph','b','bh','m','y','r','l','v','s','h','ḷ','ṃ']
 
-VOWELS = ['a','ā','i','ī','u','ū','e','o']
+CONSONANTS = ['ක', 'ඛ', 'ග', 'ඝ', 'ඞ', 'ච', 'ඡ', 'ජ', 'ඣ', 'ඤ', 'ට', 'ඨ', 'ඩ', 'ඪ', 'ණ', 'ත', 'ථ', 'ද', 'ධ', 'න', 'ප', 'ඵ', 'බ', 'භ', 'ම', 'ය', 'ර', 'ල', 'ව', 'ස', 'හ', 'ළ', 'අං''']
+
+DEPENDENT_VOWELS = ['ා', 'ි', 'ී', 'ු', 'ූ', 'ෙ', 'ො']
+
+INDEPENDENT_VOWELS = ['අ', 'ආ', 'ඉ', 'ඊ', 'උ', 'එ', 'ඔ']
+
+# Romanized
+ALPHABET_ROMANIZED = ['a','ā','i','ī','u','ū','e','o','ṃ','k','kh','g','gh','ṅ','c','ch','j','jh','ñ','ṭ','ṭh','ḍ','ḍh','ṇ','t','th','d','dh','n','p','ph','b','bh','m','y','r','l','ḷ','v','s','h']
+
+CONSONANTS_ROMANIZED = ['k','kh','g','gh','ṅ','c','ch','j','jh','ñ','ṭ','ṭh','ḍ','ḍh','ṇ','t','th','d','dh','n','p','ph','b','bh','m','y','r','l','v','s','h','ḷ','ṃ']
+
+VOWELS_ROMANIZED = ['a','ā','i','ī','u','ū','e','o']
 
 # digits[1-10]
-DIGITS = ['eka','dvi','ti','catu','pañca','cha','satta','aṭṭha','nava','dasa']
+DIGITS_ROMANIZED = ['eka','dvi','ti','catu','pañca','cha','satta','aṭṭha','nava','dasa']
