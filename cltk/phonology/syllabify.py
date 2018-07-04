d08e9489bd7853e850d39e63e559d2d3677f9e23
Chatziargyriou Eleftheria <36453738+Sedictious@users.noreply.github.com>
2018-07-04 20:35:08 +0300

    Update syllabify.py

--- cltk/phonology/syllabify.py
+++ cltk/phonology/syllabify.py
@@ -1,4 +1,3 @@
-
 __author__ = ['Eleftheria Chatziargyriou <ele.hatzy@gmail.com>']
 __license__ = 'MIT License. See LICENSE.'
 
@@ -8,9 +7,11 @@ from cltk.exceptions import InputError
 LOG = logging.getLogger(__name__)
 LOG.addHandler(logging.NullHandler())
 
+
 class Syllabifier:
 
-    def __init__(self, low_vowels=[], mid_vowels=[], high_vowels=[], flaps=[], laterals=[], nasals=[], fricatives=[], plosives=[]):
+    def __init__(self, low_vowels=[], mid_vowels=[], high_vowels=[], flaps=[], laterals=[], nasals=[], fricatives=[],
+                 plosives=[]):
 
         self.low_vowels = low_vowels
         self.mid_vowels = mid_vowels
@@ -24,8 +25,8 @@ class Syllabifier:
         self.plosives = plosives
         self.consonants = flaps + laterals + fricatives + plosives
 
-        #Dictionary indicating sonority hierarchy
-        self.hierarchy = {key:0 for key in self.low_vowels}
+        # Dictionary indicating sonority hierarchy
+        self.hierarchy = {key: 0 for key in self.low_vowels}
         self.hierarchy.update({key: 1 for key in self.mid_vowels})
         self.hierarchy.update({key: 2 for key in self.high_vowels})
         self.hierarchy.update({key: 3 for key in self.flaps})
@@ -34,7 +35,39 @@ class Syllabifier:
         self.hierarchy.update({key: 6 for key in self.fricatives})
         self.hierarchy.update({key: 7 for key in self.plosives})
 
-    def syllabify(self, word, mode = 'SSP'):
+    def set_hierarchy(self, hierarchy):
+        """
+        Sets an alternative sonority hierarchy, note that you will also need
+        to specify the vowelset with the set_vowels, in order for the module
+        to correctly identify each nucleus.
+
+        The order of the phonemes defined is by decreased consonantality
+
+        Example:
+            >>> s = Syllabifier()
+
+            >>> s.set_hierarchy([['i', 'u'], ['e'], ['a'], ['r'], ['m', 'n'], ['f']])
+
+            >>> s.set_vowels(['i', 'u', 'e', 'a'])
+
+            >>> s.syllabify('feminarum')
+            ['fe', 'mi', 'na', 'rum']
+        """
+        self.hierarchy = dict([(k, i) for i, j in enumerate(hierarchy) for k in j])
+
+    def set_vowels(self, vowels):
+        """
+        Define the vowel set of the syllabifier module
+
+        Example:
+            >>> s = Syllabifier()
+
+            >>> s.set_vowels(['i', 'u', 'e', 'a'])
+            ['i', 'u', 'e', 'a']
+        """
+        self.vowels = vowels
+
+    def syllabify(self, word, mode='SSP'):
         if mode == 'SSP':
             return self.syllabify_SSP(word)
 
@@ -63,16 +96,16 @@ class Syllabifier:
 
             >>> s.syllabify("feminarum")
             ['fe', 'mi', 'na', 'rum']
-            
+
             Not specifying your alphabet results in an error:
-            
+
             >>> s.syllabify("foemina")
             Traceback (most recent call last):
                 ...
             cltk.exceptions.InputError
         """
 
-        #List indicating the syllable indices
+        # List indicating the syllable indices
         syllables = []
 
         find_nucleus = True
@@ -80,33 +113,34 @@ class Syllabifier:
         i = 1
 
         try:
-            #Replace each letter occurence with its corresponding number
-            #indicating its position in the sonority hierarchy
+            # Replace each letter occurence with its corresponding number
+            # indicating its position in the sonority hierarchy
             encoded = list(map(lambda x: self.hierarchy[x], word))
 
         except KeyError:
-            LOG.error("The given string contains invalid characters. Make sure to define the mater of articulation for each phoneme.")
+            LOG.error(
+                "The given string contains invalid characters. Make sure to define the mater of articulation for each phoneme.")
             raise InputError
 
         while i < len(word) - 1:
-            #Search for nucleus
+            # Search for nucleus
             while word[i] not in self.vowels and i < len(word) - 1 and find_nucleus:
-                    i += 1
+                i += 1
 
             if i >= len(word) - 1:
                 break
 
             else:
-                #If a cluster of three phonemes with the same values exist, break syllable
-                if encoded[i-1] == encoded[i] == encoded[i+1]:
+                # If a cluster of three phonemes with the same values exist, break syllable
+                if encoded[i - 1] == encoded[i] == encoded[i + 1]:
                     syllables.append(i)
                     find_nucleus = True
 
-                elif encoded[i] > encoded[i-1] and encoded[i] > encoded[i+1]:
+                elif encoded[i] > encoded[i - 1] and encoded[i] > encoded[i + 1]:
                     syllables.append(i)
                     find_nucleus = True
 
-                elif encoded[i] < encoded[i-1] and encoded[i] < encoded[i+1]:
+                elif encoded[i] < encoded[i - 1] and encoded[i] < encoded[i + 1]:
                     syllables.append(i)
                     find_nucleus = True
 
@@ -120,11 +154,13 @@ class Syllabifier:
 
         word = word.split('.')
 
-        #Check if last syllable has a nucleus
+        # Check if last syllable has a nucleus
 
         if sum(map(lambda x: x in self.vowels, word[-1])) == 0:
-             word[-2] += word[-1]
-             word = word[:-1]
+            word[-2] += word[-1]
+            word = word[:-1]
 
         return word
+
+
 
