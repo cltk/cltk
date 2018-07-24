f361071c6a69b84aa5dd5af44d8c19e8fd0d19e3
Chatziargyriou Eleftheria <36453738+Sedictious@users.noreply.github.com>
2018-07-24 12:46:33 +0300

    Update treebanks.py

--- cltk/tag/treebanks.py
+++ cltk/tag/treebanks.py
@@ -1,6 +1,28 @@
 
 
 def set_path(dicts, keys, v):
+    """ Helper function for modifying nested dictionaries
+
+    :param dicts: dict: the given dictionary
+    :param keys: list str: path to added value
+    :param v: str: value to be added
+
+    Example:
+        >>> d = dicts()
+
+        >>> set_path(d, ['a', 'b', 'c'],  'd')
+
+        >>> d
+        {'a': {'b': {'c': ['d']}}}
+
+        In case of duplicate paths, the additional value will
+        be added to the leaf node rather than simply replace it:
+
+        >>> set_path(d, ['a', 'b', 'c'],  'e')
+        
+        >>> d
+        {'a': {'b': {'c': ['d', 'e']}}}
+    """
     for key in keys[:-1]:
         dicts = dicts.setdefault(key, dict())
     dicts = dicts.setdefault(keys[-1], list())
@@ -8,9 +30,18 @@ def set_path(dicts, keys, v):
 
 
 def get_paths(src):
-    """Generates root-to-leaf paths, given a treebank in string format
+    """ Generates root-to-leaf paths, given a treebank in string format. Note that
+    get_path is an iterator and does not return all the paths simultaneously.
 
     :param src: str: treebank
+
+    Examples:
+        >>> st = "((IP-MAT-SPE (' ') (INTJ Yes) (, ,) (' ') (IP-MAT-PRN (NP-SBJ (PRO he)) (VBD seyde)) (, ,) (' ') (NP-SBJ (PRO I)) (MD shall)	(VB promyse) (NP-OB2 (PRO you)) (IP-INF (TO to)	(VB fullfylle) (NP-OB1 (PRO$ youre) (N desyre))) (. .) (' '))"
+
+        Get the sixth generated path:
+
+        >>> list(get_paths(st))[5]
+        ['IP-MAT-SPE', 'IP-MAT-PRN', 'VBD', 'seyde']
     """
     st = list()
     tmp = ''
