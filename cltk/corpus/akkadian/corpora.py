bde638e9cdf7bfa9b950526b0110fb464356294d
Andrew Deloucas <38022737+adeloucas@users.noreply.github.com>
2018-07-24 10:23:33 -0700

    Update corpora.py

--- cltk/corpus/akkadian/corpora.py
+++ cltk/corpus/akkadian/corpora.py
@@ -3,13 +3,6 @@
 __author__ = ['Andrew Deloucas <ADeloucas@g.harvard.com>']
 
 AKKADIAN_CORPORA = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> upstream/master
-=======
->>>>>>> bb71cb6527b480b1927f3cd9de44cad8b7fcd659
  {'name': 'cdli_corpus',
  'origin': 'https://github.com/cdli-gh/data.git',
  'location': 'remote',
