7d12ea35dda64bf50fe919df44393a4db99ca975
clemsciences <clemsciences@aol.com>
2018-08-07 00:05:50 +0200

    Declensions of more pronouns

--- cltk/declension/old_norse/utils.py
+++ cltk/declension/old_norse/utils.py
@@ -1,13 +1,6 @@
-"""
-
-"""
+"""Declensions of pronouns, nouns, adjectives"""
 
 from enum import Enum, auto
-
-
-# class AutoName(Enum):
-#     def _generate_next_value_(name, a, b, d):
-#         return name
 
 
 class Number(Enum):
@@ -42,14 +35,13 @@ class Declinable:
         else:
             return self.declension[number.value-1][case.value-1]
 
-
-class Noun(Declinable):
-    def __init__(self, name: str, gender: Gender):
-        Declinable.__init__(self, name)
-        self.gender = gender
-
-    def get_declined(self, case: Case, number: Number, gender=None):
-        return self.declension[self.gender.value-1][number.value-1][case.value-1]
+    def print_declension(self):
+        for a_gender in Gender:
+            for a_number in Number:
+                for a_case in Case:
+                    print(a_case.name, a_number.name, a_gender.name, ":\t",
+                          self.get_declined(a_case, a_number, a_gender))
+        print("\n")
 
 
 class Pronoun(Declinable):
@@ -59,6 +51,53 @@ class Pronoun(Declinable):
 
 class Adjective:
     pass
+
+
+class DeclinableNoGender:
+    def __init__(self, name: str):
+        self.declension = []
+        self.name = name
+
+    def set_declension(self, declension):
+        self.declension = declension
+
+    def get_declined(self, case: Case, number: Number):
+        return self.declension[number.value-1][case.value-1]
+
+    def print_declension(self):
+        for a_number in Number:
+            for a_case in Case:
+                print(a_case.name, a_number.name, ":\t",
+                      self.get_declined(a_case, a_number))
+        print("\n")
+
+
+class DeclinableOneGender:
+    def __init__(self, name: str, gender: Gender):
+        self.declension = []
+        self.name = name
+        self.gender = gender
+
+    def set_declension(self, declension):
+        self.declension = declension
+
+    def get_declined(self, case: Case, number: Number):
+        return self.declension[number.value-1][case.value-1]
+
+    def print_declension(self):
+        for a_number in Number:
+            for a_case in Case:
+                print(a_case.name, a_number.name, ":\t",
+                      self.get_declined(a_case, a_number))
+        print("\n")
+
+
+class Noun(DeclinableOneGender):
+    def __init__(self, name: str, gender: Gender):
+        DeclinableOneGender.__init__(self, name, gender)
+        self.gender = gender
+        self.name = name
+        self.declension = []
 
 
 class DeclensionPattern(Declinable):
@@ -76,11 +115,31 @@ demonstrative_pronouns_this = [
     [["þetta", "þetta", "þessu", "þessa"], ["þessi", "þessi", "þessum", "þessa"]]
 ]
 
-if __name__ == "__main__":
-    pronouns = Pronoun("demonstrative_pronouns_this")
-    pronouns.set_declension(demonstrative_pronouns_this)
-    print(pronouns.get_declined(Case.dative, Number.singular, Gender.neuter))
-    for gender in Gender:
-        for number in Number:
-            for case in Case:
-                print(pronouns.get_declined(case, number, gender))
+demonstrative_pronouns_that = [
+    [["sá", "þann", "þeim", "þess"], ["þeir", "þá", "þeim", "þeirra"]],
+    [["sú", "þá", "þeirri", "þeirrar"], ["þær", "þær", "þeim", "þeirra"]],
+    [["þat", "þat", "því, þí", "þess"], ["þau", "þau", "þeim", "þeirra"]]
+]
+
+personal_pronouns_ek = [["ek", "mik", "mér", "mín"], ["vér", "oss", "oss", "vár"]]
+personal_pronouns_thu = [["þú", "þik", "þér", "þín"], ["ér", "yðr", "yðr", "yðar"]]
+personal_pronouns_hann_hon_that = [
+    [["hann", "hann", "hánum", "hans"], ["þeir", "þá", "þeim", "þeirra"]],
+    [["hon", "hana", "henni", "hennar"], ["þær", "þær", "þeim", "þeirra"]],
+    [["þat", "þat", "því", "þess"], ["þau", "þau", "þeim", "þeirra"]]
+]
+
+pro_demonstrative_pronouns_this = Pronoun("demonstrative_pronouns_this")
+pro_demonstrative_pronouns_this.set_declension(demonstrative_pronouns_this)
+
+pro_demonstrative_pronouns_that = Pronoun("demonstrative_pronouns_that")
+pro_demonstrative_pronouns_that.set_declension(demonstrative_pronouns_this)
+
+pro_personal_pronouns_ek = DeclinableNoGender("personal_pronouns_ek")
+pro_personal_pronouns_ek.set_declension(personal_pronouns_ek)
+
+pro_personal_pronouns_thu = DeclinableNoGender("personal_pronouns_thu")
+pro_personal_pronouns_thu.set_declension(personal_pronouns_thu)
+
+pro_personal_pronouns_hann_hon_that = Declinable("personal_pronouns_hann_hon_that")
+pro_personal_pronouns_hann_hon_that.set_declension(personal_pronouns_hann_hon_that)
