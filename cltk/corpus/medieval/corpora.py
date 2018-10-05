46345a7f887dc15cb489403f83fc9978d3fe1a24
Mayank Dhiman <32799055+mynkdhmn@users.noreply.github.com>
2018-10-05 09:57:10 +0530

    Update corpora.py

--- cltk/corpus/medieval/corpora.py
+++ cltk/corpus/medieval/corpora.py
@@ -6,7 +6,7 @@ All remote corpora hosted by github on the cltk organization account, eg:
 
 MEDIEVAL_CORPORA = [
 	{
-  		'name': 'gujarati_text_wikisource',
+  		'name': 'medieval_text_wikisource',
 		'origin': 'https://github.com/mynkdhmn/medieval_text_wikisource.git',
 		'location': 'remote',
 		'type': 'text';
