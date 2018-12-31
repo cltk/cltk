fcd7edf2e0006e1dbda3b58491ae758bdad36bf8
seeni <seeni0424@gmail.com>
2018-12-31 14:39:43 +0530

    added tamil corpora in corpus

---
+++ cltk/corpus/tamil/corpora.py
@@ -0,0 +1,7 @@
+TAMIL_CORPORA = [
+    {'name': 'cltk_tamil_corpus',
+     'origin': 'https://github.com/Seenivasanseeni/cltk_tamil_corpus.git',
+     'location': 'remote',
+     'type': 'text'
+    }
+]
