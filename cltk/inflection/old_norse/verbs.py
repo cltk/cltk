bfb916927800a95e1705204448c6ef9067ba224d
clement_besnier <clemsciences@aol.com>
2019-02-08 18:58:15 +0100

    More attributes

--- cltk/inflection/old_norse/verbs.py
+++ cltk/inflection/old_norse/verbs.py
@@ -3,17 +3,20 @@
 from enum import Enum, auto
 from typing import List
 
-from cltk.phonology.old_norse.transcription import measure_old_norse_syllable
+from cltk.phonology.old_norse.transcription import measure_old_norse_syllable, DIPHTHONGS_IPA, DIPHTHONGS_IPA_class, \
+    IPA_class, old_norse_rules
 
 from cltk.phonology.syllabify import Syllabifier, Syllable
 from cltk.corpus.old_norse.syllabifier import invalid_onsets, VOWELS, CONSONANTS, LONG_VOWELS, BACK_TO_FRONT_VOWELS
 from cltk.inflection.utils import Number
-from cltk.phonology.utils import Length
+from cltk.phonology.utils import Length, Transcriber
 
 __author__ = ["Clément Besnier <clemsciences@aol.com>", ]
 
 s = Syllabifier(language="old_norse", break_geminants=True)
 s.set_invalid_onsets(invalid_onsets)
+
+transcriber = Transcriber(DIPHTHONGS_IPA, DIPHTHONGS_IPA_class, IPA_class, old_norse_rules)
 
 
 class Person(Enum):
@@ -186,14 +189,18 @@ class WeakOldNorseVerb(OldNorseVerb):
 
     def __init__(self):
         super().__init__()
+
         self.sng = ""
         self.s_sng = None
+        self.sp_sng = None
 
         self.sfg3et = ""
         self.s_sfg3et = None
+        self.sp_sfg3et = None
 
         self.stgken = ""
         self.s_stgken = None
+        self.sp_stgken = None
 
         self.subclass = 0
         self.syllabified = []
@@ -201,25 +208,28 @@ class WeakOldNorseVerb(OldNorseVerb):
     def set_canonic_forms(self, canonic_forms: List[str]):
         """
 
-        >>> verb = WeakOldNorseVerb()
 
         Weak verbs
         I
+        >>> verb = WeakOldNorseVerb()
         >>> verb.set_canonic_forms(["kalla", "kallaði", "kallaðinn"])
         >>> verb.subclass
         1
 
         II
+        >>> verb = WeakOldNorseVerb()
         >>> verb.set_canonic_forms(["mæla", "mælti", "mæltr"])
         >>> verb.subclass
         2
 
         III
-        >>> verb.set_canonic_forms(["teja", "taldi", "talinn"])
+        >>> verb = WeakOldNorseVerb()
+        >>> verb.set_canonic_forms(["telja", "taldi", "talinn"])
         >>> verb.subclass
         3
 
         IV
+        >>> verb = WeakOldNorseVerb()
         >>> verb.set_canonic_forms(["vaka", "vakta", "vakat"])
         >>> verb.subclass
         4
@@ -232,9 +242,15 @@ class WeakOldNorseVerb(OldNorseVerb):
             self.sng, self.sfg3et, self.stgken = canonic_forms
             self.category = VerbCategory.weak
             self.name = self.sng
+
             self.s_sng = s.syllabify_ssp(self.sng)
+            self.sp_sng = s.syllabify_phonemes(transcriber.text_to_phonemes(self.sng))
+
             self.s_sfg3et = s.syllabify_ssp(self.sfg3et)
+            self.sp_sfg3et = s.syllabify_phonemes(transcriber.text_to_phonemes(self.sfg3et))
+
             self.s_stgken = s.syllabify_ssp(self.stgken)
+            self.sp_stgken = s.syllabify_phonemes(transcriber.text_to_phonemes(self.stgken))
             self.classify()
         else:
             raise ValueError("Not a correct argument")
@@ -301,21 +317,22 @@ class WeakOldNorseVerb(OldNorseVerb):
         elif self.sng in ["spá"]:
             self.subclass = 2
         elif self.sng and self.sfg3et and self.stgken:
+            # print(self.s_sng)
             if self.sfg3et.endswith("aði"):
                 self.subclass = 1
-            elif not "".join(Syllable(self.s_sng[0], CONSONANTS, VOWELS).nucleus) in BACK_TO_FRONT_VOWELS.values():
+
+            elif not "".join(Syllable(self.s_sng[0], VOWELS, CONSONANTS).nucleus) in BACK_TO_FRONT_VOWELS.values():
                 self.subclass = 4
             else:
+                print(self.s_sng[0])
                 stem_length = measure_old_norse_syllable(self.s_sng[0])
                 if stem_length == Length.long or stem_length == Length.overlong:
                     self.subclass = 2
                 elif stem_length == Length.short:
                     self.subclass = 3
-
-
-
-
-
+                else:
+                    print(stem_length)
+                    self.subclass = 5
 
 
 def add_t_ending_to_syllable(last_syllable):
