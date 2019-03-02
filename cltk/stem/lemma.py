2bfc49812fa086c3c70382fa9da3cfc5a153696c
diyclassics <patrick@diyclassics.org>
2019-03-02 08:27:25 -0500

    Add deprecation warning to lemmareplacer

--- cltk/stem/lemma.py
+++ cltk/stem/lemma.py
@@ -3,6 +3,7 @@
 __author__ = ['Kyle P. Johnson <kyle@kyle-p-johnson.com>']
 __license__ = 'MIT License. See LICENSE.'
 
+import warnings
 from cltk.utils.cltk_logger import logger
 import importlib.machinery
 from nltk.tokenize.punkt import PunktLanguageVars
@@ -10,7 +11,7 @@ import os
 
 AVAILABLE_LANGUAGES = ['greek', 'latin']
 
-
+# Deprecated; remove from future release?
 class LemmaReplacer(object):  # pylint: disable=too-few-public-methods
     """Lemmatize Latin words by replacing input words with corresponding
     values from a replacement list.
@@ -20,12 +21,16 @@ class LemmaReplacer(object):  # pylint: disable=too-few-public-methods
         """Import replacement patterns into a list."""
         self.language = language.lower()
         assert self.language in AVAILABLE_LANGUAGES, \
-            "Lemmatizer not available for language '{0}'.".format(self.language)
+            "\n\nLemmatizer not available for language '{0}'.".format(self.language)
         self.lemmata = self._load_replacement_patterns()
 
     def _load_replacement_patterns(self):
         """Check for availability of lemmatizer for a language."""
         if self.language == 'latin':
+            warnings.warn(
+                        "LemmaReplacer is deprecated and will soon be removed from CLTK. Please use the BackoffLatinLemmatizer at cltk.lemmatize.latin.backoff.",
+                        DeprecationWarning,
+                        stacklevel=2)
             rel_path = os.path.join('~','cltk_data',
                                     self.language,
                                     'model','latin_models_cltk',
@@ -99,11 +104,12 @@ class LemmaReplacer(object):  # pylint: disable=too-few-public-methods
 
 
 if __name__ == '__main__':
-    REPLACER = LemmaReplacer('greek')
+    REPLACER = LemmaReplacer('latin')
     PUNKT = PunktLanguageVars()
     #STRING = 'Est interdum praestare mercaturis rem quaerere, nisi tam periculosum sit, et item foenerari, si tam honestum. Maiores nostri sic habuerunt et ita in legibus posiuerunt: furem dupli condemnari, foeneratorem quadrupli. Quanto peiorem ciuem existimarint foeneratorem quam furem, hinc licet existimare. Et uirum bonum quom laudabant, ita laudabant: bonum agricolam bonumque colonum; amplissime laudari existimabatur qui ita laudabatur. Mercatorem autem strenuum studiosumque rei quaerendae existimo, uerum, ut supra dixi, periculosum et calamitosum. At ex agricolis et uiri fortissimi et milites strenuissimi gignuntur, maximeque pius quaestus stabilissimusque consequitur minimeque inuidiosus, minimeque male cogitantes sunt qui in eo studio occupati sunt. Nunc, ut ad rem redeam, quod promisi institutum principium hoc erit.'  # pylint: disable=line-too-long
-    #STRING = 'hominum divomque voluptas'
+    STRING = 'hominum divomque voluptas'
     #EX_TOKENS = PUNKT.word_tokenize(UMLEMMATIZED)
-    UMLEMMATIZED = ['τὴν', 'διάγνωσιν', 'αὐτῶν', 'ἔρχεσθαι']
-    LEMMATIZED = REPLACER.lemmatize(UMLEMMATIZED, return_raw=False, return_string=True)
+    # UMLEMMATIZED = ['τὴν', 'διάγνωσιν', 'αὐτῶν', 'ἔρχεσθαι']
+    # LEMMATIZED = REPLACER.lemmatize(UMLEMMATIZED, return_raw=False, return_string=True)
+    LEMMATIZED = REPLACER.lemmatize(STRING.split(), return_raw=False, return_string=True)
     print(LEMMATIZED)
