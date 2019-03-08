5170bd185668e51e8d25636761864664117cf942
diyclassics <patrick@diyclassics.org>
2019-03-08 15:52:31 -0500

    Test inheritance

--- cltk/tokenize/utils.py
+++ cltk/tokenize/utils.py
@@ -30,24 +30,26 @@ class BaseSentenceTokenizerTrainer(object):
         """
         Train sentence tokenizer.
         """
+        print(self.punctuation)
+        return None
 
-        # Set punctuation
-        language_punkt_vars = PunktLanguageVars
-
-        if self.punctuation or self.strict:
-            language_punkt_vars.sent_end_chars = self.punctuation+self.strict
-
-        # Set abbreviations
-        trainer = PunktTrainer(text, language_punkt_vars)
-        trainer.INCLUDE_ALL_COLLOCS = True
-        trainer.INCLUDE_ABBREV_COLLOCS = True
-
-        tokenizer = PunktSentenceTokenizer(trainer.get_params())
-
-        for abbreviation in self.abbreviations:
-            tokenizer._params.abbrev_types.add(abbreviation)
-
-        return tokenizer
+        # # Set punctuation
+        # language_punkt_vars = PunktLanguageVars
+        #
+        # if self.punctuation or self.strict:
+        #     language_punkt_vars.sent_end_chars = self.punctuation+self.strict
+        #
+        # # Set abbreviations
+        # trainer = PunktTrainer(text, language_punkt_vars)
+        # trainer.INCLUDE_ALL_COLLOCS = True
+        # trainer.INCLUDE_ABBREV_COLLOCS = True
+        #
+        # tokenizer = PunktSentenceTokenizer(trainer.get_params())
+        #
+        # for abbreviation in self.abbreviations:
+        #     tokenizer._params.abbrev_types.add(abbreviation)
+        #
+        # return tokenizer
 
     def pickle_sentence_tokenizer(self, filename, tokenizer):
         # Dump pickled tokenizer
