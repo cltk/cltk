3f90fa8066b59b1c5826773eb7c2f4e355af377e
Patrick J. Burns <patricks@diyclassics.org>
2019-03-09 18:44:39 -0500

    Add path check for corpus install

--- install_corpora.py
+++ install_corpora.py
@@ -1,8 +1,15 @@
+import os.path
 from cltk.corpus.utils.importer import CorpusImporter
 
-corpus_importer = CorpusImporter('latin')
-corpus_importer.import_corpus('latin_text_latin_library')
-corpus_importer.import_corpus('latin_models_cltk')
+corpora = [
+    ('latin', 'model', 'latin_models_cltk'),
+    ('latin', 'text', 'latin_text_latin_library'),
+    ('greek', 'model', 'greek_models_cltk'),
+]
 
-corpus_importer = CorpusImporter('greek')
-corpus_importer.import_corpus('greek_models_cltk')
+for lang, type, corpus in corpora:
+    if os.path.isdir(os.path.expanduser(f'~/cltk_data/{lang}/{type}/{corpus}/')):
+        print(f'{corpus} is already installed.')
+    else:
+        corpus_importer = CorpusImporter(lang)
+        print(f'Installing {corpus}.')
