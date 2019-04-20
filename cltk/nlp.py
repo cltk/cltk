8357f9e6ccc4ec808666327166420af396765ee6
Travis CI <travis@travis-ci.org>
2019-04-20 12:42:19 -0700

    fix remaining tests

--- cltk/nlp.py
+++ cltk/nlp.py
@@ -9,54 +9,66 @@ from cltk.wrappers.stanford import StanfordNLPWrapper
 class NLP:
     """Primary class for NLP pipeline.
 
+    TODO: Give illustrative examples of expected use of this class.
+
     >>> cltk_nlp = NLP(language='greek')
-    Use ...
     >>> cltk_nlp.language
     'greek'
-
-    #
-    # >>> xen_anab = "Δαρείου καὶ Παρυσάτιδος γίγνονται παῖδες δύο, πρεσβύτερος μὲν Ἀρταξέρξης, νεώτερος δὲ Κῦρος: ἐπεὶ δὲ ἠσθένει Δαρεῖος καὶ ὑπώπτευε τελευτὴν τοῦ βίου, ἐβούλετο τὼ παῖδε ἀμφοτέρω παρεῖναι."
-    # >>> nlp_xen_anab = cltk_nlp.parse_stanford(xen_anab)
-    # >>> import stanfordnlp
-    # >>> isinstance(nlp_xen_anab , stanfordnlp.pipeline.doc.Document)
-    # True
-
-    # >>> nlp_xen_anab.words
-    # ['At', 'ille', 'ut', 'Carthaginem', 'venit,', 'multo', 'aliter,', 'ac', 'sperarat,', 'rem', 'publicam', 'se', 'habentem', 'cognovit.', 'Namque', 'diuturnitate', 'externi', 'mali', 'tantum', 'exarsit', 'intestinum', 'bellum,', 'ut', 'numquam', 'in', 'pari', 'periculo', 'fuerit', 'Carthago,', 'nisi', 'cum', 'deleta', 'est.', 'Primo', 'mercennarii', 'milites,', 'qui', 'adversus', 'Romanos', 'fuerant,', 'desciverunt;', 'quorum', 'numerus', 'erat', 'XX', 'milium.']
-    # >>> nlp_xen_anab.sentences
-    # ['At ille ut Carthaginem venit, multo aliter, ac sperarat, rem publicam se habentem cognovit', 'Namque diuturnitate externi mali tantum exarsit intestinum bellum, ut numquam in pari periculo fuerit Carthago, nisi cum deleta est', 'Primo mercennarii milites, qui adversus Romanos fuerant, desciverunt; quorum numerus erat XX milium.']
     """
 
     def __init__(self, language: str) -> None:
         """Constructor for CLTK class.
 
-        # >>> cltk_nlp = NLP('Itaque metu.', language='la')
-        # >>> isinstance(cltk_nlp, NLP)
-        # True
+        >>> cltk_nlp = NLP(language='greek')
+        >>> isinstance(cltk_nlp, NLP)
+        True
         """
 
         self.language = language
         self.tokenizer_word = self._get_word_tokenizer()  # type: TokenizeWord
         self.stanford_obj = self._get_stanford_model()
 
-    def parse_stanford(self, text: str):
-        """Do the actual parsing of input text."""
+    def _parse_stanford(self, text: str):
+        """Do the actual parsing of input text.
+
+        >>> cltk_nlp = NLP(language='greek')
+        >>> xen_anab = "Δαρείου καὶ Παρυσάτιδος γίγνονται παῖδες δύο, πρεσβύτερος μὲν Ἀρταξέρξης, νεώτερος δὲ Κῦρος: ἐπεὶ δὲ ἠσθένει Δαρεῖος καὶ ὑπώπτευε τελευτὴν τοῦ βίου, ἐβούλετο τὼ παῖδε ἀμφοτέρω παρεῖναι."
+        >>> nlp_xen_anab = cltk_nlp._parse_stanford(xen_anab)
+        >>> import stanfordnlp
+        >>> isinstance(nlp_xen_anab , stanfordnlp.pipeline.doc.Document)
+        True
+        >>> hasattr(nlp_xen_anab, 'conll_file')
+        True
+        >>> hasattr(nlp_xen_anab, 'load_annotations')
+        True
+        >>> hasattr(nlp_xen_anab, 'sentences')
+        True
+        >>> hasattr(nlp_xen_anab, 'text')
+        True
+        >>> hasattr(nlp_xen_anab, 'write_conll_to_file')
+        True
+        """
         return self.stanford_obj.parse(text=text)
 
     def _get_stanford_model(self):
         """If available for a given language, get the entire
         object returned by the `stanfordnlp` project.
+
+        >>> cltk_nlp = NLP(language='greek')
+        >>> plain_stanford_obj = cltk_nlp._get_stanford_model()
+        >>> import cltk
+        >>> isinstance(plain_stanford_obj, cltk.wrappers.stanford.StanfordNLPWrapper)
+        True
         """
-        # return 'STAND IN FOR STANFORD OBJECT'
         return StanfordNLPWrapper(language=self.language)
 
     def _get_word_tokenizer(self) -> TokenizeWord:
         """Fetches and returns callable tokenizer for
         appropriate language.
 
-        # >>> cltk_nlp = NLP('Itaque metu.', language='latin')
-        # >>> isinstance(cltk_nlp.tokenizer_word, TokenizeWord)
-        # True
+        >>> cltk_nlp = NLP(language='greek')
+        >>> isinstance(cltk_nlp.tokenizer_word, TokenizeWord)
+        True
         """
         return TokenizeWord(language=self.language)
 
@@ -97,8 +109,8 @@ if __name__ == '__main__':
     cltk_nlp = NLP(language='greek')
     xen_anab = "Δαρείου καὶ Παρυσάτιδος γίγνονται παῖδες δύο, πρεσβύτερος μὲν Ἀρταξέρξης, νεώτερος δὲ Κῦρος: ἐπεὶ δὲ ἠσθένει Δαρεῖος καὶ ὑπώπτευε τελευτὴν τοῦ βίου, ἐβούλετο τὼ παῖδε ἀμφοτέρω παρεῖναι."
 
-    nlp_xen_anab = cltk_nlp.parse_stanford(text=xen_anab)
-    import stanfordnlp
+    nlp_xen_anab = cltk_nlp._parse_stanford(text=xen_anab)
+    import stanfordnlp  # type: ignore
     print(isinstance(nlp_xen_anab, stanfordnlp.pipeline.doc.Document) == True)  # True
     # print(dir(nlp_xen_anab))
     print(nlp_xen_anab.text == xen_anab)
