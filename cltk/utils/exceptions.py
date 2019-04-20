e3514c0a84a032c4b05a7f5bc93dc272436cae32
Travis CI <travis@travis-ci.org>
2019-04-20 12:42:58 -0700

    add exception note

--- cltk/utils/exceptions.py
+++ cltk/utils/exceptions.py
@@ -3,4 +3,6 @@
 class UnknownLanguageError(Exception):
     """Exception for when a user requests an NLP method that is not
     supported.
+
+    TODO: Make a `CLTKException(Exception)` and have this inherit from that.
     """
