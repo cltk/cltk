58eedf0bfde7699cd6c5af88246b6bbae7a14653
Travis CI <travis@travis-ci.org>
2019-08-22 18:06:10 -0700

    sys path

--- docs/conf.py
+++ docs/conf.py
@@ -10,8 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
+sys.path.insert('src/cltkv1')
 # sys.path.insert(0, os.path.abspath('.'))
 
 
