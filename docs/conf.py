a627f80bcd1072b55ad9bdb30d1bacdd71d7ee14
Travis CI <travis@travis-ci.org>
2019-08-22 18:12:08 -0700

    conf: print(os.path.abspath("."))

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, 'src/cltkv1')
+print('* ' * 88)
+print(os.path.abspath("."))
 # sys.path.insert(0, os.path.abspath('.'))
+print('* ' * 88)
 
 
 # -- Project information -----------------------------------------------------
