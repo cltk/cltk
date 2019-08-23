6e498c2ce295b282c7a81a2eaf6aac877d9fea06
Travis CI <travis@travis-ci.org>
2019-08-22 18:23:09 -0700

    comment autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ release = "1.0.0a1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+# extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
