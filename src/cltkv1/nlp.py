b0e56e87b0de90feb149327c87f1b88fcac453c8
Travis CI <travis@travis-ci.org>
2019-08-24 20:21:57 -0700

    add raw tokens fn to Text

--- src/cltkv1/nlp.py
+++ src/cltkv1/nlp.py
@@ -11,9 +11,13 @@ from cltkv1.wrappers import StanfordNLPWrapper
 # It will be held in a list. From this,
 # TODO: Fill out more attributes to this
 
+# TODO: Look into ``dataclasses.field(default_factory=some_function)`` to auto populate a field based upon another: https://realpython.com/python-data-classes/
+
 
 @dataclass
 class Word:
+    """Contains attributes of each processed word in a list of tokens. To be used most often in the ``Text.tokens`` dataclass."""
+
     index_char_start: int = None
     index_char_stop: int = None
     index_token: int = None
@@ -25,12 +29,26 @@ class Word:
 
 @dataclass
 class Text:
+    """The object returned to the user from the ``NLP()`` class. Contains overall attributes of submitted texts, plus (probably) most importantly the processed tokenized text ``tokens``, being a list of ``Word`` types.."""
+
     indices_sentences: List[List[int]] = None
     indices_tokens: List[List[int]] = None
     language: str = None
     tokens: List[Word] = None
     pipeline: List[str] = None
     raw: str = None
+
+    def get_raw_tokens(self):
+        """Return list of string tokens.
+
+        >>> cltk_nlp = NLP(language='greek')
+        >>> john_damascus_corinth = "Τοῦτο εἰπὼν, ᾐνίξατο αἰτίους ὄντας τοῦ τὰ ἐλάσσονα λαμβάνειν, καὶ κυρίους, εἰ βούλοιντο, τοῦ τὰ μείζονα. Ἔστι δὲ πολὺ μείζων ἡ ἀγάπη πάντων τῶν χαρισμάτων."
+        >>> john_text_analyzed = cltk_nlp.analyze(john_damascus_corinth)
+        >>> str_tokens = john_text_analyzed.get_raw_tokens()
+        >>> str_tokens[0:3]
+        ['Τοῦτο', 'εἰπὼν', 'ᾐνίξατο']
+        """
+        return [word.string for word in self.tokens]
 
 
 class NLP:
