e5e48b90ca897faaf3d1f41707072083ea44f726
William Michael Short <w.short@exeter.ac.uk>
2019-10-27 15:34:18 +0000

    move IC to models/semantics

--- cltk/corpus/latin/wordnet.py
+++ cltk/corpus/latin/wordnet.py
@@ -2080,9 +2080,7 @@ class WordNetICCorpusReader(CorpusReader):
     >>> LWNIC = WordNetICCorpusReader(fileids=['ic-lasla.dat'])
 
     """
-
-    # root=os.path.join(get_cltk_data_dir(), 'latin/model/latin_models_cltk/semantics/')
-    def __init__(self, root="cltk/corpus/latin", fileids=None):
+    def __init__(self, root=os.path.join(get_cltk_data_dir(), 'latin/model/latin_models_cltk/semantics/wordnet/'), fileids=None):
         CorpusReader.__init__(self, root, fileids, encoding="utf8")
         if fileids is not None:
             self.load_ic(fileids[0])
