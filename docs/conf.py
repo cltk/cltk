9d4cecc3588feba5d5991a4327aae5dd8a89af39
Kyle P. Johnson <kyle.p.johnson>
2019-11-01 01:23:24 -0700

    add torch to mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,9 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
+autodoc_mock_imports = ["torch", "stanfordnlp"]
+
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
@@ -65,4 +68,4 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
