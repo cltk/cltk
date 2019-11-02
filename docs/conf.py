162db64f98019f2104c836e9cbbbe72c49f78951
Kyle P. Johnson <kyle.p.johnson>
2019-11-02 09:31:02 -0700

    do not mock sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -62,6 +62,13 @@ autodoc_mock_imports = list(autodoc_mock_imports) + [
 ]
 
 autodoc_mock_imports.remove("sphinx_autodoc_typehints")
+autodoc_mock_imports.remove("sphinx-autodoc-typehints")
+autodoc_mock_imports.remove("sphinxcontrib-applehelp")
+autodoc_mock_imports.remove("sphinxcontrib-devhelp")
+autodoc_mock_imports.remove("sphinxcontrib-htmlhelp")
+autodoc_mock_imports.remove("sphinxcontrib-jsmath")
+autodoc_mock_imports.remove("sphinxcontrib-qthelp")
+autodoc_mock_imports.remove("sphinxcontrib-serializinghtml")
 
 
 # Add any paths that contain templates here, relative to this directory.
