e2a29092d46919608ef8bd7f27f37852048bf9f5
Kyle P. Johnson <kyle.p.johnson>
2020-03-15 10:36:48 -0700

    run format

--- src/cltkv1/alphabet/urd.py
+++ src/cltkv1/alphabet/urd.py
@@ -4,16 +4,51 @@ TODO: Add tests.
 """
 
 
-#Persian digits from 0 to 9
-DIGITS = ['۰', '۱', '۲', '۳', '٤', '۵', '٦', '۷', '۸', '۹']
+# Persian digits from 0 to 9
+DIGITS = ["۰", "۱", "۲", "۳", "٤", "۵", "٦", "۷", "۸", "۹"]
 
-#Characters that are used to impart accent
-SHORT_VOWELS = ['َ', 'ِ', 'ُ']
+# Characters that are used to impart accent
+SHORT_VOWELS = ["َ", "ِ", "ُ"]
 
-#The Persian Alphabet
-LONG_VOWELS = ['ا', 'و', 'ی', 'ے']
+# The Persian Alphabet
+LONG_VOWELS = ["ا", "و", "ی", "ے"]
 
-CONSONANTS = ['ء', 'ب', 'پ', 'ت', 'ٹ', 'ث', 'ج', 'چ', 'ح', 'خ', 'د', 'ڈ', 'ذ', 'ر', 'ڑ', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'ه', 'ھ']
+CONSONANTS = [
+    "ء",
+    "ب",
+    "پ",
+    "ت",
+    "ٹ",
+    "ث",
+    "ج",
+    "چ",
+    "ح",
+    "خ",
+    "د",
+    "ڈ",
+    "ذ",
+    "ر",
+    "ڑ",
+    "ز",
+    "ژ",
+    "س",
+    "ش",
+    "ص",
+    "ض",
+    "ط",
+    "ظ",
+    "ع",
+    "غ",
+    "ف",
+    "ق",
+    "ک",
+    "گ",
+    "ل",
+    "م",
+    "ن",
+    "ه",
+    "ھ",
+]
 
-#Special Characters
-SPECIAL = ['ﺁ', 'ۀ', 'ﻻ']
+# Special Characters
+SPECIAL = ["ﺁ", "ۀ", "ﻻ"]
