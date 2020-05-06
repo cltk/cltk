f73c2a4a0fe01ff30012d051db49d4daae40b441
Clément Besnier <clemsciences@aol.com>
2020-05-06 19:59:38 +0200

    Removed useless statements

--- cltk/lemmatize/middle_high_german/backoff.py
+++ cltk/lemmatize/middle_high_german/backoff.py
@@ -20,12 +20,11 @@ class BackoffMHGLemmatizer:
     models_path = os.path.normpath(get_cltk_data_dir() +
                                    '/middle_high_german/model/middle_high_german_models_cltk/lemmata/backoff')
 
-    def __init__(self, train: List[list] = None, seed: int = 3, verbose: bool = False):
+    def __init__(self, seed: int = 3, verbose: bool = False):
         self.models_path = BackoffMHGLemmatizer.models_path
 
         missing_models_message = "BackoffMHGLemmatizer requires the ```middle_high_german_models_cltk``` " \
                                  "to be in cltk_data. Please load this corpus."
-        self.train = train
         self.seed = seed
         self.verbose = verbose
 
@@ -33,7 +32,6 @@ class BackoffMHGLemmatizer:
         self.lemma_to_tokens = []
 
         try:
-            self.lemma_to_tokens = open_pickle(os.path.join(self.models_path, "lemma_to_tokens.pickle"))
             self.token_to_lemmata = open_pickle(os.path.join(self.models_path, "token_to_lemma.pickle"))
         except FileNotFoundError as err:
             raise type(err)(missing_models_message)
@@ -41,7 +39,6 @@ class BackoffMHGLemmatizer:
         self._define_lemmatizer()
 
     def _define_lemmatizer(self):
-        # Suggested backoff chain--should be tested for optimal order
         self.backoff0 = None
         self.backoff1 = IdentityLemmatizer(verbose=self.verbose)
         self.backoff2 = DictLemmatizer(lemmas=self.token_to_lemmata, source='ReferenzKorpus Mittelhochdeutsch Lemmata',
