32fb4abf0f8afb4e317f5206ef35104e44bdf408
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2020-06-18 21:29:38 -0700

    format and update tests

--- cltk/corpus/utils/formatter.py
+++ cltk/corpus/utils/formatter.py
@@ -3,9 +3,12 @@ Some formatting can happen here, or invoke language-specific formatters in
 other files.
 """
 
-__author__ = ['Kyle P. Johnson <kyle@kyle-p-johnson.com>', 'Stephen Margheim <stephen.margheim@gmail.com>',
-              'Natasha Voake <natashavoake@gmail.com>']
-__license__ = 'MIT License. See LICENSE.'
+__author__ = [
+    "Kyle P. Johnson <kyle@kyle-p-johnson.com>",
+    "Stephen Margheim <stephen.margheim@gmail.com>",
+    "Natasha Voake <natashavoake@gmail.com>",
+]
+__license__ = "MIT License. See LICENSE."
 
 from builtins import bytes
 from cltk.corpus.greek.tlg_index import TLG_INDEX
@@ -22,16 +25,16 @@ import regex
 
 # research how to build regex values for regex.compile() from this dict
 TLG_PHI_REPLACEMENTS = {
-    'newline_hyphen': '-\n',
-    'newline': '\n',  # you probably want to substitute this with an empty space ' '
-    'within_pointed_brackets': '{.+?}',
-    'chevrons': '«|»',
-    'ellipsis': ' ... ',
-    'latin_09': '[a-zA-Z0-9]',
-    'within_parentheses': '\(.+?\)',
-    'pointed_brackets': '\<|\>',
-    'curled_single_quotes': '‘|’',
-    'underscore': '_',
+    "newline_hyphen": "-\n",
+    "newline": "\n",  # you probably want to substitute this with an empty space ' '
+    "within_pointed_brackets": "{.+?}",
+    "chevrons": "«|»",
+    "ellipsis": " ... ",
+    "latin_09": "[a-zA-Z0-9]",
+    "within_parentheses": "\(.+?\)",
+    "pointed_brackets": "\<|\>",
+    "curled_single_quotes": "‘|’",
+    "underscore": "_",
 }
 
 
@@ -51,7 +54,7 @@ def tonos_oxia_converter(text, reverse=False):
       tonos (meant for Modern Greek) into the oxia equivalent. Without this
       normalization, string comparisons will fail."""
     for char_tonos, char_oxia in TONOS_OXIA.items():
-        if not reverse:        
+        if not reverse:
             text = text.replace(char_tonos, char_oxia)
         else:
             text = text.replace(char_oxia, char_tonos)
@@ -72,12 +75,12 @@ def remove_non_latin(input_string, also_keep=None):
     that will not be filtered.
     """
     if also_keep:
-        also_keep += [' ']
+        also_keep += [" "]
     else:
-        also_keep = [' ']
-    latin_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        also_keep = [" "]
+    latin_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     latin_chars += latin_chars.lower()
-    latin_chars += ''.join(also_keep)
+    latin_chars += "".join(also_keep)
     no_latin = "".join([char for char in input_string if char in latin_chars])
     return no_latin
 
@@ -86,31 +89,35 @@ def tlg_plaintext_cleanup(text, rm_punctuation=False, rm_periods=False):
     """Remove and substitute post-processing for Greek TLG text.
     TODO: Surely more junk to pull out. Please submit bugs!
     """
-    remove_comp = regex.compile(r'-\n|[«»<>\(\)‘’_—:!\?\'\"\*]|{[[:print:][:space:]]+?}|\[[[:print:][:space:]]+?\]|[a-zA-Z0-9]', flags=regex.VERSION1)
-    text = remove_comp.sub('', text)
+    remove_comp = regex.compile(
+        r"-\n|[«»<>\(\)‘’_—:!\?\'\"\*]|{[[:print:][:space:]]+?}|\[[[:print:][:space:]]+?\]|[a-zA-Z0-9]",
+        flags=regex.VERSION1,
+    )
+    text = remove_comp.sub("", text)
 
     if rm_punctuation:
-        punct_comp = regex.compile(r',|·')
-        text = punct_comp.sub('', text)        
-        
+        punct_comp = regex.compile(r",|·")
+        text = punct_comp.sub("", text)
+
     if rm_periods:
-        period_comp = regex.compile(r'\.|;')
-        text = period_comp.sub('', text)
+        period_comp = regex.compile(r"\.|;")
+        text = period_comp.sub("", text)
 
     # replace line breaks w/ space
-    replace_comp = regex.compile(r'\n')
-    text = replace_comp.sub(' ', text)
+    replace_comp = regex.compile(r"\n")
+    text = replace_comp.sub(" ", text)
 
-    comp_space = regex.compile(r'\s+')
-    text = comp_space.sub(' ', text)
+    comp_space = regex.compile(r"\s+")
+    text = comp_space.sub(" ", text)
 
     return text
 
+
 def cltk_normalize(text, compatibility=True):
     if compatibility:
-        return normalize('NFKC', text)
+        return normalize("NFKC", text)
     else:
-        return normalize('NFC', text)
+        return normalize("NFC", text)
 
 
 def phi5_plaintext_cleanup(text, rm_punctuation=False, rm_periods=False):
@@ -120,19 +127,21 @@ def phi5_plaintext_cleanup(text, rm_punctuation=False, rm_periods=False):
     """
     # This works OK, doesn't get some
     # Note: rming all characters between {} and ()
-    remove_comp = regex.compile(r'-\n|«|»|\<|\>|\.\.\.|‘|’|_|{.+?}|\(.+?\)|\(|\)|“|#|%|⚔|&|=|/|\\|〚|†|『|⚖|–|˘|⚕|☾|◌|◄|►|⌐|⌊|⌋|≈|∷|≈|∞|”|[0-9]')
-    text = remove_comp.sub('', text)
+    remove_comp = regex.compile(
+        r"-\n|«|»|\<|\>|\.\.\.|‘|’|_|{.+?}|\(.+?\)|\(|\)|“|#|%|⚔|&|=|/|\\|〚|†|『|⚖|–|˘|⚕|☾|◌|◄|►|⌐|⌊|⌋|≈|∷|≈|∞|”|[0-9]"
+    )
+    text = remove_comp.sub("", text)
 
     new_text = None
     if rm_punctuation:
-        new_text = ''
-        punctuation = [',', ';', ':', '"', "'", '?', '-', '!', '*', '[', ']', '{', '}']
+        new_text = ""
+        punctuation = [",", ";", ":", '"', "'", "?", "-", "!", "*", "[", "]", "{", "}"]
         if rm_periods:
-            punctuation += ['.']
+            punctuation += ["."]
         for char in text:
             # rm acute combining acute accents made by TLGU
             # Could be caught by regex, tried and failed, not sure why
-            if bytes(char, 'utf-8') == b'\xcc\x81':
+            if bytes(char, "utf-8") == b"\xcc\x81":
                 pass
             # second try at rming some punctuation; merge with above regex
             elif char in punctuation:
@@ -143,58 +152,59 @@ def phi5_plaintext_cleanup(text, rm_punctuation=False, rm_periods=False):
         text = new_text
 
     # replace line breaks w/ space
-    replace_comp = regex.compile(r'\n')
-    text = replace_comp.sub(' ', text)
+    replace_comp = regex.compile(r"\n")
+    text = replace_comp.sub(" ", text)
 
-    comp_space = regex.compile(r'\s+')
-    text = comp_space.sub(' ', text)
+    comp_space = regex.compile(r"\s+")
+    text = comp_space.sub(" ", text)
 
     return text
 
 
 def assemble_tlg_author_filepaths():
     """Reads TLG index and builds a list of absolute filepaths."""
-    plaintext_dir_rel = get_cltk_data_dir() + '/greek/text/tlg/plaintext/'
+    plaintext_dir_rel = get_cltk_data_dir() + "/greek/text/tlg/plaintext/"
     plaintext_dir = os.path.expanduser(plaintext_dir_rel)
-    filepaths = [os.path.join(plaintext_dir, x + '.TXT') for x in TLG_INDEX]
+    filepaths = [os.path.join(plaintext_dir, x + ".TXT") for x in TLG_INDEX]
     return filepaths
 
 
 def assemble_phi5_author_filepaths():
     """Reads PHI5 index and builds a list of absolute filepaths.
     """
-    plaintext_dir_rel = get_cltk_data_dir() + '/latin/text/phi5/plaintext/'
+    plaintext_dir_rel = get_cltk_data_dir() + "/latin/text/phi5/plaintext/"
     plaintext_dir = os.path.expanduser(plaintext_dir_rel)
-    filepaths = [os.path.join(plaintext_dir, x + '.TXT') for x in PHI5_INDEX]
+    filepaths = [os.path.join(plaintext_dir, x + ".TXT") for x in PHI5_INDEX]
     return filepaths
 
 
 def assemble_tlg_works_filepaths():
     """Reads TLG index and builds a list of absolute filepaths."""
-    plaintext_dir_rel = get_cltk_data_dir() + '/greek/text/tlg/individual_works/'
+    plaintext_dir_rel = get_cltk_data_dir() + "/greek/text/tlg/individual_works/"
     plaintext_dir = os.path.expanduser(plaintext_dir_rel)
     all_filepaths = []
     for author_code in TLG_WORKS_INDEX:
         author_data = TLG_WORKS_INDEX[author_code]
-        works = author_data['works']
+        works = author_data["works"]
         for work in works:
-            f = os.path.join(plaintext_dir, author_code + '.TXT' + '-' + work + '.txt')
+            f = os.path.join(plaintext_dir, author_code + ".TXT" + "-" + work + ".txt")
             all_filepaths.append(f)
     return all_filepaths
 
 
 def assemble_phi5_works_filepaths():
     """Reads PHI5 index and builds a list of absolute filepaths."""
-    plaintext_dir_rel = get_cltk_data_dir() + '/latin/text/phi5/individual_works/'
+    plaintext_dir_rel = get_cltk_data_dir() + "/latin/text/phi5/individual_works/"
     plaintext_dir = os.path.expanduser(plaintext_dir_rel)
     all_filepaths = []
     for author_code in PHI5_WORKS_INDEX:
         author_data = PHI5_WORKS_INDEX[author_code]
-        works = author_data['works']
+        works = author_data["works"]
         for work in works:
-            f = os.path.join(plaintext_dir, author_code + '.TXT' + '-' + work + '.txt')
+            f = os.path.join(plaintext_dir, author_code + ".TXT" + "-" + work + ".txt")
             all_filepaths.append(f)
     return all_filepaths
+
 
 """The normalizer aims to maximally reduce the variation between the orthography of texts written in the Anglo-Norman dialect
 to bring it in line with “orthographe commune”. It is heavily inspired by Pope (1956).
@@ -232,34 +242,41 @@ Anglo-French spelling variants normalized to "orthographe commune", from M. K. P
 
 """
 
-patterns = [("eaus$", "eus"),
-            ("ceaus$", "ceus"),
-            ("iu", "ieu"),
-            ("((?<!^)|(?<!(e)))u(?!$)", "eu"),
-            ("ie$", "iee"),
-            ("ue$", "uee"),
-            ("ure$", "eure"),
-            ("eo$", "o"),
-            ("iw$", "ieux"),
-            ("ew$", "ieux"),
-            ("a$", "e"),
-            ("^en", "an"),
-            ("d$", "")]
+patterns = [
+    ("eaus$", "eus"),
+    ("ceaus$", "ceus"),
+    ("iu", "ieu"),
+    ("((?<!^)|(?<!(e)))u(?!$)", "eu"),
+    ("ie$", "iee"),
+    ("ue$", "uee"),
+    ("ure$", "eure"),
+    ("eo$", "o"),
+    ("iw$", "ieux"),
+    ("ew$", "ieux"),
+    ("a$", "e"),
+    ("^en", "an"),
+    ("d$", ""),
+]
+
 
 def build_match_and_apply_functions(pattern, replace):
     def matches_rule(word):
         return re.search(pattern, word)
+
     def apply_rule(word):
         return re.sub(pattern, replace, word)
+
     return (matches_rule, apply_rule)
 
 
-rules = [build_match_and_apply_functions(pattern, replace)
-         for (pattern, replace) in patterns]
+rules = [
+    build_match_and_apply_functions(pattern, replace) for (pattern, replace) in patterns
+]
+
 
 def normalize_fr(string):
     string = string.lower()
-    word_tokenizer = WordTokenizer('french')
+    word_tokenizer = WordTokenizer("french")
     tokens = word_tokenizer.tokenize(string)
     normalized_text = []
     for token in tokens:
@@ -268,4 +285,3 @@ def normalize_fr(string):
                 normalized = apply_rule(token)
                 normalized_text.append(normalized)
     return normalized_text
-
