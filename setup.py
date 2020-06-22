a8543c453bc4d880fcf2a0b9f33f97e5e9eda389
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2020-06-21 22:37:18 -0700

    bump vers for addition of pointy bracket to tlg cleanup

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     name='cltk',
     packages=find_packages(),
     url='https://github.com/cltk/cltk',
-    version='0.1.120',
+    version='0.1.121',
     zip_safe=True,
     test_suite='cltk.tests.test_cltk',
 )
