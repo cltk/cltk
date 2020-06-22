2b76afd59082bcc658554b25e62715450fd28e7e
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2020-06-21 22:05:33 -0700

    update with new angle braket

--- cltk/corpus/utils/formatter.py
+++ cltk/corpus/utils/formatter.py
@@ -91,7 +91,7 @@ def tlg_plaintext_cleanup(text, rm_punctuation=False, rm_periods=False):
     TODO: Surely more junk to pull out. Please submit bugs!
     """
     remove_comp = regex.compile(
-        r"-\n|[«»<>\(\)‘’_—:!\?\'\"\*]|{[[:print:][:space:]]+?}|\[[[:print:][:space:]]+?\]|[a-zA-Z0-9]",
+        r"-\n|[«»<>〈〉\(\)‘’_—:!\?\'\"\*]|{[[:print:][:space:]]+?}|\[[[:print:][:space:]]+?\]|[a-zA-Z0-9]",
         flags=regex.VERSION1,
     )
     text = remove_comp.sub("", text)
