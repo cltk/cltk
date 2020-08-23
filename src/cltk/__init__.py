d2fe480d1dbe1cf9099283d1a25ea6bd9491a45c
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2020-08-23 14:57:10 -0700

    add version to init

--- src/cltk/__init__.py
+++ src/cltk/__init__.py
@@ -1,6 +1,10 @@
 """Init module for importing the CLTK class.
 
-TODO: Add ``__version__`` here with `curr_version = pkg_resources.get_distribution("cltk")  # type: pkg_resources.EggInfoDistribution`` and ``release = curr_version.version  # type: str``
+TODO: Add ``__version__`` here with ``curr_version = pkg_resources.get_distribution("cltk")  # type: pkg_resources.EggInfoDistribution`` and ``release = curr_version.version  # type: str``
 """
+
+import pkg_resources
+
+__version__ = curr_version = pkg_resources.get_distribution("cltk")  # type: pkg_resources.EggInfoDistribution
 
 from .nlp import NLP
