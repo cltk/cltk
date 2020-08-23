9c85ec9f83f1e447a8d9be60ed9014047c50e0fc
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2020-08-23 14:57:35 -0700

    rm todo note

--- src/cltk/__init__.py
+++ src/cltk/__init__.py
@@ -1,7 +1,4 @@
-"""Init module for importing the CLTK class.
-
-TODO: Add ``__version__`` here with ``curr_version = pkg_resources.get_distribution("cltk")  # type: pkg_resources.EggInfoDistribution`` and ``release = curr_version.version  # type: str``
-"""
+"""Init module for importing the CLTK class."""
 
 import pkg_resources
 
