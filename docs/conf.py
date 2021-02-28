338c6cec14e233bc99bad256eff5595e0c4ecde3
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2021-02-28 09:49:39 -0800

    add favicon to docs

--- docs/conf.py
+++ docs/conf.py
@@ -142,5 +142,6 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
+html_favicon = 'favicon-32x32.png'
 # https://alabaster.readthedocs.io/en/latest/installation.html
