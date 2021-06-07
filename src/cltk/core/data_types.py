cdcaadb49d692f3284f8c655f10e512219a6a4ff
todd-cook <665389+todd-cook@users.noreply.github.com>
2021-06-07 00:59:23 -0700

    Added the generation of SIF sentence embeddings. Added Sentence object to contain the sentence embedding, and did minor refactoring so that it be indexed as a list of a list of words--as before. Added bring-your-own word idf file for computing the embeddings. Added TFIDF mapping for cltk models. Added scikit-learn library for the PCA reduction of the embeddings.

--- src/cltk/core/data_types.py
+++ src/cltk/core/data_types.py
@@ -11,6 +11,7 @@ of the NLP pipeline.
 
 import importlib
 from abc import ABC, abstractmethod
+from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Dict, List, Type, Union
 
@@ -106,6 +107,25 @@ category={}, stop=None, named_entity=None, syllables=None, phonetic_transcriptio
 
 
 @dataclass
+class Sentence:
+    """
+    The Data Container for sentences.
+    """
+
+    words: List[Word] = None
+    index: int = None
+    embedding: np.ndarray = field(repr=False, default=None)
+
+    def __getitem__(self, item):
+        """This indexing operation descends into the word list structure."""
+        return self.words[item]
+
+    def __len__(self):
+        """Returns the number of tokens in the sentence"""
+        return len(self.words)
+
+
+@dataclass
 class Doc:
     """The object returned to the user from the ``NLP()`` class.
     Contains overall attributes of submitted texts, plus most
@@ -169,28 +189,34 @@ class Doc:
     raw: str = None
     normalized_text: str = None
     embeddings_model = None
+    sent_embeddings: Dict[int, np.ndarray] = field(
+        repr=False, default=None
+    )  #: slot for sentences
 
     @property
-    def sentences(self) -> List[List[Word]]:
-        """Returns a list of lists, with the inner list being a
-        list of ``Word`` objects.
-        """
-        sentences = {}
+    def sentences(self) -> List[Sentence]:
+        """Returns a list of Sentences, with each sentence being a container for a
+        list of ``Word`` objects."""
+        sents = defaultdict(list)
         for word in self.words:
-            sentence = sentences.get(word.index_sentence, {})
-            sentence[word.index_token] = word
-            sentences[word.index_sentence] = sentence
-
-        sorted_values = lambda dict: [x[1] for x in sorted(dict.items())]
-
-        return [sorted_values(sentence) for sentence in sorted_values(sentences)]
+            sents[word.index_sentence].append(word)
+        for key in sents:
+            sents[key].sort(key=lambda x: x.index_token)
+        if (
+            not self.sent_embeddings
+        ):  # sometimes not available, nor initialized; e.g. stanza
+            self.sent_embeddings = {}
+        return [
+            Sentence(words=val, index=key, embedding=self.sent_embeddings.get(key))
+            for key, val in sorted(sents.items(), key=lambda x: x[0])
+        ]
 
     @property
     def sentences_tokens(self) -> List[List[str]]:
         """Returns a list of lists, with the inner list being a
         list of word token strings.
         """
-        sentences_list = self.sentences  # type: List[List[Word]]
+        sentences_list = self.sentences  # type: List[Sentence]
         sentences_tokens = list()  # type: List[List[str]]
         for sentence in sentences_list:
             sentence_tokens = [word.string for word in sentence]  # type: List[str]
@@ -202,7 +228,7 @@ class Doc:
         """Returns a list of strings, with each string being
         a sentence reconstructed from the word tokens.
         """
-        sentences_list = self.sentences_tokens  # type: List[List[str]]
+        sentences_list = self.sentences_tokens  # type: List[Sentence]
         sentences_str = list()  # type: List[str]
         for sentence_tokens in sentences_list:  # type: List[str]
             if self.language == "akk":
--- src/cltk/embeddings/processes.py
+++ src/cltk/embeddings/processes.py
@@ -1,14 +1,18 @@
 """This module holds the embeddings ``Process``es."""
-
+import os
+import pickle
 from copy import deepcopy
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
 
 import numpy as np
 from boltons.cacheutils import cachedproperty
+from sklearn.decomposition import TruncatedSVD
 
-from cltk.core.data_types import Doc, Process
+from cltk.core.data_types import Doc, Process, Sentence
 from cltk.core.exceptions import CLTKException
 from cltk.embeddings.embeddings import FastTextEmbeddings, Word2VecEmbeddings
+from cltk.utils import CLTK_DATA_DIR
 
 
 @dataclass
@@ -30,6 +34,10 @@ class EmbeddingsProcess(Process):
 
     language: str = None
     variant: str = "fasttext"
+    embedding_length: int = None
+    word_idf: Optional[Dict[str, float]] = field(repr=False, default=None)
+    min_idf = None
+    max_idf = None
 
     @cachedproperty
     def algorithm(self):
@@ -45,20 +53,50 @@ class EmbeddingsProcess(Process):
             )
 
     def run(self, input_doc: Doc) -> Doc:
+        """Compute the embeddings."""
         output_doc = deepcopy(input_doc)
-
-        embedding_length = None
         embeddings_obj = self.algorithm
-
         for index, word_obj in enumerate(output_doc.words):
-            if not embedding_length:
-                embedding_length = embeddings_obj.get_embedding_length()
+            if not self.embedding_length:
+                self.embedding_length = embeddings_obj.get_embedding_length()
             word_embedding = embeddings_obj.get_word_vector(word=word_obj.string)
             if not isinstance(word_embedding, np.ndarray):
-                word_embedding = np.zeros([embedding_length])
+                word_embedding = np.zeros([self.embedding_length])
             word_obj.embedding = word_embedding
             output_doc.words[index] = word_obj
 
+        if not self.word_idf:
+            # You can bring your own model
+            if os.environ.get("WORD_IDF_FILE"):
+                with open(os.environ.get("WORD_IDF_FILE"), "rb") as fin:
+                    self.word_idf = pickle.load(fin)
+            elif TFIDF_MAP.get(self.language):
+                model_path = TFIDF_MAP[self.language]
+                if not os.path.isdir(model_path):
+                    msg = f"TFIDF model path '{model_path}' not found. Going to try to download it ..."
+                    logging.warning(msg)
+                    dl_msg = f"This part of the CLTK depends upon models from the CLTK project."
+                    model_url = f"https://github.com/cltk/{self.language}_models_cltk"
+                    download_prompt(
+                        iso_code=self.language, message=dl_msg, model_url=model_url
+                    )
+                else:
+                    with open(f"{model_path}word_idf.pkl", "rb") as fin:
+                        self.word_idf = pickle.load(fin)
+        # These values are needed while generating sentence embeddings
+        if self.word_idf and not self.min_idf:
+            self.min_idf = np.min(np.array(list(self.word_idf.values())))
+            self.max_idf = np.max(np.array(list(self.word_idf.values())))
+        if self.word_idf:
+            output_doc.sent_embeddings = {}  # type: Dict[int, np.ndarray]
+            for index, sent_obj in enumerate(output_doc.sentences):
+                output_doc.sent_embeddings[index] = get_sent_embeddings(
+                    sent_obj,
+                    self.word_idf,
+                    self.min_idf,
+                    self.max_idf,
+                    self.embedding_length,
+                )
         return output_doc
 
 
@@ -125,3 +163,98 @@ class SanskritEmbeddingsProcess(EmbeddingsProcess):
 
     description: str = "Default embeddings for Sanskrit."
     language: str = "san"
+
+
+TFIDF_MAP = {
+    "lat": os.path.join(
+        CLTK_DATA_DIR,
+        "lat/model/lat_models_cltk/tfidf/",
+    ),
+}
+
+
+def rescale_idf(val: float, min_idf: float, max_idf: float) -> float:
+    """
+    rescale idf values
+    """
+    return (val - min_idf) / (max_idf - min_idf)
+
+
+def compute_pc(X: np.ndarray, npc: int = 1) -> np.ndarray:
+    """
+    Compute the principal components. DO NOT MAKE THE DATA ZERO MEAN!
+    :param X: X[i,:] is a data point
+    :param npc: number of principal components to remove
+    :return: component_[i,:] is the i-th pc
+
+    This has been adapted from the SIF paper code: https://openreview.net/pdf?id=SyK00v5xx
+    """
+    svd = TruncatedSVD(n_components=npc, n_iter=7, random_state=0)
+    svd.fit(X)
+    return svd.components_
+
+
+def remove_pc(X: np.ndarray, npc: int = 1):
+    """
+    Remove the projection on the principal components
+    :param X: X[i,:] is a data point
+    :param npc: number of principal components to remove
+    :return: XX[i, :] is the data point after removing its projection
+
+    This has been adapted from the SIF paper code: https://openreview.net/pdf?id=SyK00v5xx
+    """
+    pc = compute_pc(X, npc)
+    if npc == 1:
+        XX = X - X.dot(pc.transpose()) * pc
+    else:
+        XX = X - X.dot(pc.transpose()).dot(pc)
+    return XX
+
+
+def get_sent_embeddings(
+    sent: Sentence,
+    word_idf: Dict[str, float],
+    min_idf: float,
+    max_idf: float,
+    dimensions: int = 300,
+) -> np.ndarray:
+    """
+    Provides the weighted average of a sentence's word vectors
+    with the principle component removed.
+
+    Expectations:
+    Word can only appear once in a sentence, multiple occurrences are collapsed.
+    Must have 2 or more embeddings, otherwise Principle Component cannot be found and removed.
+
+    :param sent: Sentence
+    :param word_idf: a dictionary of tokens and idf values
+    :param min_idf: the min idf score to use for scaling
+    :param max_idf: the max idf score to use for scaling
+    :param dimensions: the number of dimensions of the embedding
+
+    :return ndarray: values of the sentence embedding, or returns an array of zeroes
+    if no sentence embedding could be computed.
+    """
+    embed_map = {
+        tmp.string: (
+            rescale_idf(word_idf.get(tmp.string.lower(), min_idf), min_idf, max_idf),
+            tmp.embedding,
+        )
+        for tmp in sent.words
+        if not np.all((tmp.embedding == 0))  # skip processing empty embeddings
+    }
+    words = embed_map.keys()
+    weights_embedds = embed_map.values()
+    if (
+        len(weights_embedds) < 2
+    ):  # we can't create a sentence embedding for just one word
+        return np.zeros(dimensions)
+    weights, embedds = zip(*weights_embedds)
+    if sum(weights) == 0:
+        return np.zeros(dimensions)
+    embedds = remove_pc(np.array(embedds))
+    scale_factor = 1 / sum(weights)
+    scaled_vals = np.array([tmp * scale_factor for tmp in weights])
+    # apply our weighted terms to the adjusted embeddings
+    weighted_embeds = embedds * scaled_vals[:, None]
+    return np.sum(weighted_embeds, axis=0)
