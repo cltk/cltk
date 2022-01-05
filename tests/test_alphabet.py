35bc8f2b87acd08f9b4716f8a51a7228ad2e0942
Patrick J. Burns <diyclassics@users.noreply.github.com>
2022-01-05 13:51:45 -0500

    Fix normalization process as described in #1143; add test (#1144)

---
+++ tests/test_alphabet.py
@@ -0,0 +1,23 @@
+"""Test ``cltk.alphabet``."""
+
+__license__ = "MIT License. See LICENSE."
+
+import unittest
+import unicodedata
+
+from cltk.alphabet.grc.grc import normalize_grc
+
+
+class TestSequenceFunctions(unittest.TestCase):  # pylint: disable=R0904
+    """Class for unittest"""
+
+    def test_normalize_grc(self):
+        """Test for normalizing grc input"""
+        source = "Ἡροδότου Ἁλικαρνησσέος ἱστορίης ἀπόδεξις ἥδε"
+        source_nfd = unicodedata.normalize("NFD", source)
+        target = normalize_grc(source_nfd)
+        self.assertEqual(source, target)
+
+
+if __name__ == "__main__":
+    unittest.main()
