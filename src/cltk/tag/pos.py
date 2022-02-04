4ed8c0a2450e6e2dbac4252bca421dca8af5272d
Clément Besnier <clemsciences@aol.com>
2022-02-05 00:36:47 +0100

    Added Middle High German POS tagger into CLTK v1 (#1136)
    * Added Middle High German POS tagger
    
    * Fixed gmh tests
    
    * fro_models_cltk is a model

--- src/cltk/tag/pos.py
+++ src/cltk/tag/pos.py
@@ -39,6 +39,12 @@ TAGGERS = {
         "crf": "crf.pickle",
         "perceptron": "perceptron.pickle",
     },
+    "gmh": {
+        "unigram": "unigram.pickle",
+        "bigram": "bigram.pickle",
+        "trigram": "trigram.pickle",
+        "tnt": "tnt.pickle",
+    },
 }
 
 
