e7a5fa8fa9e295c66c4b39592f1fe0f5a50bcea6
Ben Deschamps <ben@t8pe.com>
2022-04-26 02:09:34 -0400

    Changed path from fro/texts/fro_models_cltk to fro/model/fro_models_cltk (#1162)
    Co-authored-by: Kyle P. Johnson <kyle@kyle-p-johnson.com>

--- src/cltk/ner/ner.py
+++ src/cltk/ner/ner.py
@@ -22,7 +22,7 @@ __author__ = ["Natasha Voake <natashavoake@gmail.com>"]
 
 NER_DICT = {
     # "ang": os.path.join(CLTK_DATA_DIR, "ang/model/ang_models_cltk/ner/spacy_model/"),
-    "fro": os.path.join(CLTK_DATA_DIR, "fro/text/fro_models_cltk/named_entities_fr.py"),
+    "fro": os.path.join(CLTK_DATA_DIR, "fro/model/fro_models_cltk/named_entities_fr.py"),
     # "grc": os.path.join(
     #     CLTK_DATA_DIR,
     #     "grc/model/grc_models_cltk/ner/spacy_model/",
