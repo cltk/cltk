e79786f77ee9a3c30a337aeaa832b8fc03578af9
Peter Aronoff <peter@aronoff.org>
2022-07-17 12:51:45 -0400

    fix: replace distutils.strtobool with str_to_bool

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,11 +3,31 @@
 import unittest
 from unittest.mock import patch
 
-from cltk.utils.utils import query_yes_no
+from cltk.utils.utils import query_yes_no, str_to_bool
 
 
 class TestUtils(unittest.TestCase):
     """Test utils."""
+
+    def test_str_to_bool_yes(self):
+        """Test that "yes" returns True."""
+        self.assertEqual(str_to_bool("yes"), True)
+
+    def test_str_to_bool_no(self):
+        """Test that "no" returns False."""
+        self.assertEqual(str_to_bool("no"), False)
+
+    def test_str_to_bool_case_insensitivity(self):
+        """Test that the function is case insensitive."""
+        self.assertEqual(str_to_bool("yES"), True)
+
+    def test_str_to_bool_custom_truths_true(self):
+        """Test that we can pass a custom list of true values and get True."""
+        self.assertEqual(str_to_bool("hello", truths=["Hello", "World"]), True)
+
+    def test_str_to_bool_custom_truths_false(self):
+        """Test that we can pass a custom list of true values and get False."""
+        self.assertEqual(str_to_bool("yes", truths=["Hello", "World"]), False)
 
     @patch("builtins.input", lambda *args: "yes")
     def test_query_yes(self):
