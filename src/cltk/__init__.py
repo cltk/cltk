fb73a6d490bf3b150b26d6a0a6235c60ee46abe4
William Holland <parkerholland04@gmail.com>
2024-07-30 10:51:21 -0400

    Fixed first python3.7 import error
    Source I got this from in the comments of the modified file

--- src/cltk/__init__.py
+++ src/cltk/__init__.py
@@ -1,6 +1,11 @@
 """Init module for importing the CLTK class."""
 
-from importlib import metadata
+# Fix for importing when using different Python versions
+# https://stackoverflow.com/questions/59216175/importerror-cannot-import-name-metadata-from-importlib
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
 
 from .nlp import NLP
 
